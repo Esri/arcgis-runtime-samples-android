470788c11df16bb443a66fba3907374452ffd4a4
doneill <doneill@esri.com>
2014-05-16 09:02:09 -0800

    finished MapRotation main activity

--- MapRotation/src/main/java/com/arcgis/android/samples/maps/maprotation/MainActivity.java
+++ MapRotation/src/main/java/com/arcgis/android/samples/maps/maprotation/MainActivity.java
@@ -18,6 +18,9 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.esri.android.map.MapView;
+import com.esri.android.map.event.OnSingleTapListener;
+
 /**
  * The Map Rotation sample app shows how to allow a user to rotate a map, and also shows a compass that displays the
  * current map rotation angle. The setAllowRotationByPinch method allows rotation of the map using a pinch gesture; the
@@ -27,12 +30,44 @@ import android.view.MenuItem;
  */
 public class MainActivity extends Activity {
 
+    MapView mMapView;
+    Compass mCompass;
+
+    int mProgress;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
 
+        // Get a reference to the MapView.
+        mMapView = (MapView) findViewById(R.id.map);
+
+        // Set the MapView to allow the user to rotate the map when as part of a pinch gesture.
+        mMapView.setAllowRotationByPinch(true);
+
+        // Enabled wrap around map.
+        mMapView.enableWrapAround(true);
+
+        // Create the Compass custom view, and add it onto the MapView.
+        mCompass = new Compass(this, null, mMapView);
+        mMapView.addView(mCompass);
+
+        // Set a single tap listener on the MapView.
+        mMapView.setOnSingleTapListener(new OnSingleTapListener() {
+
+            public void onSingleTap(float x, float y) {
+
+                // When a single tap gesture is received, reset the map to its default rotation angle,
+                // where North is shown at the top of the device.
+                mMapView.setRotationAngle(0);
+
+                // Also reset the compass angle.
+                mCompass.setRotationAngle(0);
+            }
+        });
+
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -52,4 +87,19 @@ public class MainActivity extends Activity {
         }
         return super.onOptionsItemSelected(item);
     }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        // Call MapView.pause to suspend map rendering while the activity is paused
+        mMapView.pause();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        // Call MapView.unpause to resume map rendering when the activity returns to the foreground.
+        mMapView.unpause();
+    }
+
 }
