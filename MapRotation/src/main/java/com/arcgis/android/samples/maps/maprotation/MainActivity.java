6d7e43de5ebbcf524287c4e49f614a987ed0691c
doneill <doneill@esri.com>
2014-05-16 08:48:49 -0800

    added class comment

--- MapRotation/src/main/java/com/arcgis/android/samples/maps/maprotation/MainActivity.java
+++ MapRotation/src/main/java/com/arcgis/android/samples/maps/maprotation/MainActivity.java
@@ -18,7 +18,13 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
-
+/**
+ * The Map Rotation sample app shows how to allow a user to rotate a map, and also shows a compass that displays the
+ * current map rotation angle. The setAllowRotationByPinch method allows rotation of the map using a pinch gesture; the
+ * current angle of rotation is then retrieved from the MapView using getRotationAngle. A custom View showing a compass
+ * image is added to the map, which rotates itself in response to the OnPinchListener set on the MapView. An
+ * OnSingleTapListener allows the map rotation angle to be reset to 0 by tapping on the map.
+ */
 public class MainActivity extends Activity {
 
     @Override
