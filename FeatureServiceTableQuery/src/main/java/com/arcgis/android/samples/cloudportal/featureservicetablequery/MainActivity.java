72a33cb16260ea1a6722a5d983c8a477d36610c0
doneill <doneill@esri.com>
2014-05-27 11:37:30 -0800

    added license header text

--- FeatureServiceTableQuery/src/main/java/com/arcgis/android/samples/cloudportal/featureservicetablequery/MainActivity.java
+++ FeatureServiceTableQuery/src/main/java/com/arcgis/android/samples/cloudportal/featureservicetablequery/MainActivity.java
@@ -1,3 +1,16 @@
+/* Copyright 2014 ESRI
+ *
+ * All rights reserved under the copyright laws of the United States
+ * and applicable international laws, treaties, and conventions.
+ *
+ * You may freely redistribute and use this sample code, with or
+ * without modification, provided you include the original copyright
+ * notice and use restrictions.
+ *
+ * See the Sample code usage restrictions document for further information.
+ *
+ */
+
 package com.arcgis.android.samples.cloudportal.featureservicetablequery;
 
 import android.app.Activity;
