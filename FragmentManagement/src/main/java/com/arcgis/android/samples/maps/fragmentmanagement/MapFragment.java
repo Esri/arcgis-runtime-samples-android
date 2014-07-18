796b61e6a2d21d18979a57b89df9f609b26afeff
doneill <doneill@esri.com>
2014-07-17 20:22:49 -0800

    added license.  ensured fragment instance retained when activity is destroyed

--- FragmentManagement/src/main/java/com/arcgis/android/samples/maps/fragmentmanagement/MapFragment.java
+++ FragmentManagement/src/main/java/com/arcgis/android/samples/maps/fragmentmanagement/MapFragment.java
@@ -1,3 +1,16 @@
+/* Copyright 2014 ESRI
+ *
+ * All rights reserved under the copyright laws of the United States
+ * and applicable international laws, treaties, and conventions.
+ *
+ * You may freely redistribute and use this sample code, with or
+ * without modification, provided you include the original copyright
+ * notice and use restrictions.
+ *
+ * See the Sample code usage restrictions document for further information.
+ *
+ */
+
 package com.arcgis.android.samples.maps.fragmentmanagement;
 
 import android.os.Bundle;
@@ -50,6 +63,11 @@ public class MapFragment extends Fragment {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        /** Calling setRetainInstance() causes the Fragment instance to be retained
+         * when its Activity is destroyed and recreated. This allows map Layer
+         * objects to be retained so data will not need to be fetched from the network again.
+         */
+        setRetainInstance(true);
     }
 
     @Override
