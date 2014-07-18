09cd450c6ca69094864f66fe79eaa7e8f6e1ede0
doneill <doneill@esri.com>
2014-07-17 20:36:16 -0800

    initial add

---
+++ FragmentManagement/src/main/java/com/arcgis/android/samples/maps/fragmentmanagement/BasemapListFragmentFragment.java
@@ -0,0 +1,98 @@
+/* Copyright 2014 ESRI
+ *
+ * All rights reserved under the copyright laws of the United States
+ * and applicable international laws, treaties, and conventions.
+ *
+ * You may freely redistribute and use this sample code, with or
+ * without modification, provided you include the original copyright
+ * notice and use restrictions.
+ *
+ * See the Sample code usage restrictions document for further information.
+ *
+ */
+
+package com.arcgis.android.samples.maps.fragmentmanagement;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.app.ListFragment;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+
+import com.arcgis.android.samples.maps.fragmentmanagement.dummy.DummyContent;
+
+/**
+ * A fragment representing a list of basemaps. This fragment also supports a two-pane view by
+ * allowing list items to be given an 'activated' state upon selection. This helps indicate
+ * which item is currently being viewed in a {@link MapFragment}.
+ * Activities containing this fragment MUST implement the {@code BasemapListListener} interface.
+ */
+public class BasemapListFragmentFragment extends ListFragment {
+
+    private OnFragmentInteractionListener mListener;
+
+    /**
+     * Mandatory empty constructor for the fragment manager to instantiate the
+     * fragment (e.g. upon screen orientation changes).
+     */
+    public BasemapListFragmentFragment() {
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+
+        // TODO: Change Adapter to display your content
+        setListAdapter(new ArrayAdapter<DummyContent.DummyItem>(getActivity(),
+                android.R.layout.simple_list_item_1, android.R.id.text1, DummyContent.ITEMS));
+    }
+
+
+    @Override
+    public void onAttach(Activity activity) {
+        super.onAttach(activity);
+        try {
+            mListener = (OnFragmentInteractionListener) activity;
+        } catch (ClassCastException e) {
+            throw new ClassCastException(activity.toString()
+                + " must implement OnFragmentInteractionListener");
+        }
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        mListener = null;
+    }
+
+
+    @Override
+    public void onListItemClick(ListView l, View v, int position, long id) {
+        super.onListItemClick(l, v, position, id);
+
+        if (null != mListener) {
+            // Notify the active callbacks interface (the activity, if the
+            // fragment is attached to one) that an item has been selected.
+            mListener.onFragmentInteraction(DummyContent.ITEMS.get(position).id);
+        }
+    }
+
+    /**
+    * This interface must be implemented by activities that contain this
+    * fragment to allow an interaction in this fragment to be communicated
+    * to the activity and potentially other fragments contained in that
+    * activity.
+    * <p>
+    * See the Android Training lesson <a href=
+    * "http://developer.android.com/training/basics/fragments/communicating.html"
+    * >Communicating with Other Fragments</a> for more information.
+    */
+    public interface OnFragmentInteractionListener {
+        // TODO: Update argument type and name
+        public void onFragmentInteraction(String id);
+    }
+
+}
