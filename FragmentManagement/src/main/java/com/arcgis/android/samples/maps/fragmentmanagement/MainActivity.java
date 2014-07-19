192e11e5c9f3c9f68704fa2bd7dad1f456343514
doneill <doneill@esri.com>
2014-07-18 16:15:26 -0800

    Refactored layout files to same name

--- FragmentManagement/src/main/java/com/arcgis/android/samples/maps/fragmentmanagement/MainActivity.java
+++ FragmentManagement/src/main/java/com/arcgis/android/samples/maps/fragmentmanagement/MainActivity.java
@@ -39,7 +39,7 @@ public class MainActivity extends Activity implements BasemapListFragment.Basema
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.main);
 
         FragmentManager fragMgr = getFragmentManager();
         getActionBar().setDisplayHomeAsUpEnabled(false);
