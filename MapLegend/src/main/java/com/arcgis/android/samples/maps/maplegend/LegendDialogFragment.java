54be4026eb55f068f7eea3276913fe99a19c73a5
doneill <doneill@esri.com>
2014-08-27 13:04:04 -0800

    added layouts

--- MapLegend/src/main/java/com/arcgis/android/samples/maps/maplegend/LegendDialogFragment.java
+++ MapLegend/src/main/java/com/arcgis/android/samples/maps/maplegend/LegendDialogFragment.java
@@ -13,13 +13,26 @@
 
 package com.arcgis.android.samples.maps.maplegend;
 
+import android.app.Activity;
 import android.app.DialogFragment;
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.LayoutInflater;
+import android.view.View;
 import android.view.ViewGroup;
+import android.widget.BaseAdapter;
 import android.widget.LinearLayout;
+import android.widget.ListView;
+import android.widget.TextView;
 
 import com.esri.android.map.ags.ArcGISDynamicMapServiceLayer;
+import com.esri.android.map.ags.ArcGISLayerInfo;
+import com.esri.core.map.Legend;
+
+import java.util.Collections;
+import java.util.List;
 
 /**
  * A dialog that shows the legend of a ArcGISDynamicMapServiceLayer.
@@ -41,6 +54,91 @@ public class LegendDialogFragment extends DialogFragment {
         new FetchLegendTask().execute();
 
         return mLinearLayout;
+    }
+
+    public void onAttach(Activity activity) {
+        super.onAttach(activity);
+
+        if (!(activity instanceof MainActivity)) {
+            throw new IllegalStateException("Hosting activity needs to be of type MainActivity");
+        }
+    }
+
+    /**
+     * Populates the list of legend items for each sub-layer of an ArcGISDynamicMapServiceLayer.
+     */
+    private class LayerLegendAdapter extends BaseAdapter {
+
+        private final List<Legend> mLegends;
+
+        public LayerLegendAdapter(List<Legend> legends) {
+            mLegends = legends != null ? legends : Collections.EMPTY_LIST;
+        }
+
+        public int getCount() {
+            return mLegends.size();
+        }
+
+        public Object getItem(int position) {
+            return mLegends.get(position);
+        }
+
+        public long getItemId(int position) {
+            return position;
+        }
+
+        public View getView(int position, View convertView, ViewGroup parent) {
+            View view = convertView;
+            if (view == null) {
+                view = getActivity().getLayoutInflater().inflate(R.layout.layer_legend_item_layout, null);
+            }
+
+            Legend legend = mLegends.get(position);
+            TextView textView = (TextView) view;
+            textView.setText(legend.getLabel());
+
+            Bitmap bitmap = legend.getImage();
+            BitmapDrawable drawable = new BitmapDrawable(getResources(), bitmap);
+            drawable.setBounds(0, 0, bitmap.getWidth(), bitmap.getHeight());
+            textView.setCompoundDrawables(drawable, null, null, null);
+
+            return view;
+        }
+    }
+
+    /**
+     * Retrieves the legend information asynchronously from the ArcGISDynamicMapServiceLayer.
+     */
+    private class FetchLegendTask extends AsyncTask<Void, Void, Void> {
+
+        @Override
+        protected Void doInBackground(Void... params) {
+            mLayer.retrieveLegendInfo();
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void result) {
+            super.onPostExecute(result);
+
+            for (ArcGISLayerInfo layerInfo : mLayer.getLayers()) {
+                View view = getActivity().getLayoutInflater().inflate(R.layout.layer_legend_layout, null);
+                populateLegendView(view, layerInfo);
+
+                mLinearLayout.addView(view);
+            }
+        }
+
+        private View populateLegendView(View view, ArcGISLayerInfo layerInfo) {
+            if (layerInfo != null) {
+                TextView textView = (TextView) view.findViewById(R.id.layer_legend_title_textview);
+                ListView listView = (ListView) view.findViewById(R.id.layer_legend_symbols_listview);
+
+                textView.setText(layerInfo.getName());
+                listView.setAdapter(new LayerLegendAdapter(layerInfo.getLegend()));
+            }
+            return view;
+        }
     }
 
 }
