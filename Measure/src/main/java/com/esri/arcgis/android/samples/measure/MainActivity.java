8ec98db4a3204e0bdda237c8fac42fb7c052d396
doneill <doneill@esri.com>
2015-01-02 16:52:04 -0900

    added arcgis aar dependency, added mapoptions

--- Measure/src/main/java/com/esri/arcgis/android/samples/measure/MainActivity.java
+++ Measure/src/main/java/com/esri/arcgis/android/samples/measure/MainActivity.java
@@ -8,6 +8,7 @@ import android.view.MenuItem;
 
 public class MainActivity extends ActionBarActivity {
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
