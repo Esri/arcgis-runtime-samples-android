eedca8c26f2f5f02c7e5ba95f9d8711223a651ce
doneill <doneill@esri.com>
2015-01-05 19:12:54 -0900

    implemented MeasureTool

--- Measure/src/main/java/com/esri/arcgis/android/samples/measure/MainActivity.java
+++ Measure/src/main/java/com/esri/arcgis/android/samples/measure/MainActivity.java
@@ -1,11 +1,18 @@
 package com.esri.arcgis.android.samples.measure;
 
+import android.graphics.Color;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
 import com.esri.android.map.MapView;
+import com.esri.android.toolkit.analysis.MeasuringTool;
+import com.esri.core.geometry.LinearUnit;
+import com.esri.core.geometry.Unit;
+import com.esri.core.symbol.SimpleFillSymbol;
+import com.esri.core.symbol.SimpleLineSymbol;
+import com.esri.core.symbol.SimpleMarkerSymbol;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -32,13 +39,44 @@ public class MainActivity extends ActionBarActivity {
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
+
+        SimpleFillSymbol fillSymbol;
+
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
+        if (id == R.id.action_measure) {
+
+            Unit[] linearUnits = new Unit[] {
+                    Unit.create(LinearUnit.Code.CENTIMETER),
+                    Unit.create(LinearUnit.Code.METER),
+                    Unit.create(LinearUnit.Code.KILOMETER),
+                    Unit.create(LinearUnit.Code.INCH),
+                    Unit.create(LinearUnit.Code.FOOT),
+                    Unit.create(LinearUnit.Code.YARD),
+                    Unit.create(LinearUnit.Code.MILE_STATUTE)
+            };
+
+            SimpleMarkerSymbol markerSymbol = new SimpleMarkerSymbol(Color.BLUE, 10, SimpleMarkerSymbol.STYLE.DIAMOND);
+            SimpleLineSymbol lineSymbol = new SimpleLineSymbol(Color.YELLOW, 3);
+            fillSymbol = new SimpleFillSymbol(Color.argb(100, 0, 225, 255));
+            fillSymbol.setOutline(new SimpleLineSymbol(Color.TRANSPARENT, 0));
+
+            // create the tool, required.
+            MeasuringTool measuringTool = new MeasuringTool(mMapView);
+
+            // customize the tool, optional.
+            measuringTool.setLinearUnits(linearUnits);
+            measuringTool.setMarkerSymbol(markerSymbol);
+            measuringTool.setLineSymbol(lineSymbol);
+            measuringTool.setFillSymbol(fillSymbol);
+
+            // fire up the tool, required.
+            startActionMode(measuringTool);
+
             return true;
         }
 
