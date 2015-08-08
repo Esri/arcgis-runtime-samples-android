db343e3008b3096857fb6f3f17536d8d601184cc
doneill <doneill@esri.com>
2015-08-07 16:35:14 -0800

    updated license header

--- GeoJSONEarthquakeMap/src/main/java/com/esri/arcgis/android/samples/GeoJSONEarthquakeMap/GeoJSONEarthquakeMapActivity.java
+++ GeoJSONEarthquakeMap/src/main/java/com/esri/arcgis/android/samples/GeoJSONEarthquakeMap/GeoJSONEarthquakeMapActivity.java
@@ -1,34 +1,20 @@
-/* Copyright 2014 ESRI
+/* Copyright 2015 Esri
  *
- * All rights reserved under the copyright laws of the United States
- * and applicable international laws, treaties, and conventions.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
  *
- * You may freely redistribute and use this sample code, with or
- * without modification, provided you include the original copyright
- * notice and use restrictions.
+ *    http://www.apache.org/licenses/LICENSE-2.0
  *
- * See the sample code usage restrictions document for further information.
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  *
  */
 
 package com.esri.arcgis.android.samples.GeoJSONEarthquakeMap;
-
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.util.HashMap;
-import java.util.Map;
-
-import org.apache.http.HttpEntity;
-import org.apache.http.HttpResponse;
-import org.apache.http.client.ClientProtocolException;
-import org.apache.http.client.HttpClient;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.impl.client.DefaultHttpClient;
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
 
 import android.app.Activity;
 import android.app.ProgressDialog;
@@ -49,6 +35,23 @@ import com.esri.core.geometry.Point;
 import com.esri.core.geometry.SpatialReference;
 import com.esri.core.map.Graphic;
 import com.esri.core.symbol.SimpleMarkerSymbol;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * This sample demonstrates how to use information from a GEOJson feed. For this
--- GeometryEditor/src/main/java/com/esri/arcgis/android/samples/geometryeditor/FeatureTypeData.java
+++ GeometryEditor/src/main/java/com/esri/arcgis/android/samples/geometryeditor/FeatureTypeData.java
@@ -1,13 +1,16 @@
-/* Copyright 2010 ESRI
+/* Copyright 2015 Esri
  *
- * All rights reserved under the copyright laws of the United States
- * and applicable international laws, treaties, and conventions.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
  *
- * You may freely redistribute and use this sample code, with or
- * without modification, provided you include the original copyright
- * notice and use restrictions.
+ *    http://www.apache.org/licenses/LICENSE-2.0
  *
- * See the sample code usage restrictions document for further information.
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  *
  */
 
