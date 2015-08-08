44eed2e7f9b05dd2ca45bc7f14cc911be3d377b0
doneill <doneill@esri.com>
2015-08-07 16:41:04 -0800

    updated license header

--- IdentifyTask/src/main/java/com/esri/arcgis/android/samples/identifytask/Identify.java
+++ IdentifyTask/src/main/java/com/esri/arcgis/android/samples/identifytask/Identify.java
@@ -1,21 +1,20 @@
-/* Copyright 2014 ESRI
+/* Copyright 2015 Esri
  *
- * All rights reserved under the copyright laws of the United States
- * and applicable international laws, treaties, and conventions.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
  *
- * You may freely redistribute and use this sample code, with or
- * without modification, provided you include the original copyright
- * notice and use restrictions.
+ *    http://www.apache.org/licenses/LICENSE-2.0
  *
- * See the Sample code usage restrictions document for further information.
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  *
  */
 
 package com.esri.arcgis.android.samples.identifytask;
-
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
 
 import android.app.Activity;
 import android.app.ProgressDialog;
@@ -43,6 +42,10 @@ import com.esri.core.geometry.Point;
 import com.esri.core.tasks.identify.IdentifyParameters;
 import com.esri.core.tasks.identify.IdentifyResult;
 import com.esri.core.tasks.identify.IdentifyTask;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 
 /**
  * This sample allows the user to identify data based on single tap and view the
--- LocalRasterData/src/main/java/com/arcgis/android/samples/localdata/localrasterdata/FileBrowserFragment.java
+++ LocalRasterData/src/main/java/com/arcgis/android/samples/localdata/localrasterdata/FileBrowserFragment.java
@@ -1,19 +1,25 @@
-/* Portions of this code is based on code from 
- * https://github.com/mburman/Android-File-Explore and contributions from 
+/*
+ * Portions of this code is based on code from
+ * https://github.com/mburman/Android-File-Explore and contributions from
  * Sugan Krishnan (https://github.com/rgksugan)
- * under Apache2 license > http://www.apache.org/licenses/LICENSE-2.0
- * Modifications have made to use fragment instead of activity
- * and added more features to the file browser.
+ * under Apache2 license.  Modifications have made to use fragment
+ * instead of activity and added more features to the file browser.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
  */
 
 package com.arcgis.android.samples.localdata.localrasterdata;
-
-import java.io.File;
-import java.io.FilenameFilter;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Comparator;
-import java.util.List;
 
 import android.graphics.Color;
 import android.os.Bundle;
@@ -33,6 +39,13 @@ import android.widget.PopupMenu;
 import android.widget.PopupMenu.OnMenuItemClickListener;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import java.io.File;
+import java.io.FilenameFilter;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
 
 public class FileBrowserFragment extends Fragment {
 
