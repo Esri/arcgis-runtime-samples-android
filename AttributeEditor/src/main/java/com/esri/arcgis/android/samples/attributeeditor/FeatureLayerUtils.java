6893144280f8b38858ea4426275da38094836294
doneill <doneill@esri.com>
2015-08-07 16:24:30 -0800

    updated license header

--- AttributeEditor/src/main/java/com/esri/arcgis/android/samples/attributeeditor/FeatureLayerUtils.java
+++ AttributeEditor/src/main/java/com/esri/arcgis/android/samples/attributeeditor/FeatureLayerUtils.java
@@ -1,17 +1,24 @@
-/* Copyright 2014 ESRI
- *
- * All rights reserved under the copyright laws of the United States
- * and applicable international laws, treaties, and conventions.
- *
- * You may freely redistribute and use this sample code, with or
- * without modification, provided you include the original copyright
- * notice and use restrictions.
- *
- * See the sample code usage restrictions document for further information.
- *
- */
+/* Copyright 2015 Esri
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*    http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*
+*/
 
 package com.esri.arcgis.android.samples.attributeeditor;
+
+import com.esri.core.map.FeatureType;
+import com.esri.core.map.Field;
+import com.esri.core.map.Graphic;
 
 import java.text.DateFormat;
 import java.text.ParseException;
@@ -19,10 +26,6 @@ import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.HashMap;
 import java.util.Map;
-
-import com.esri.core.map.FeatureType;
-import com.esri.core.map.Field;
-import com.esri.core.map.Graphic;
 
 public class FeatureLayerUtils {
 
