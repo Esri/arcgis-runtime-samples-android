20438482b337d5eca8f1c886384ff62164dfaab1
doneill <doneill@esri.com>
2015-08-31 07:56:17 -0800

    initial commit change basemaps sample

---
+++ change-basemaps/README.md
@@ -0,0 +1,14 @@
+# Change Basemaps
+
+![Change Basemaps App](change-basemaps.png)
+
+The Change Basemaps sample app shows how you can change basemaps from the ```ActionBar``` overflow button.  You can easily add more basemaps by extending the menu items and implementing them in the ```onCreateOptionsMenu``` and ```onOptionsItemSelected``` methods.
+
+## Features
+* Map
+* MapView
+* Basemap
+* ViewPoint
+
+## Sample Design
+The ```ActionBar``` overflow button lists a group of basemaps to choose from.  The basemaps types are created from the ```Basemap``` class and passed to the ```Map```.
---
+++ change-basemaps/src/main/java/com/esri/arcgisruntime/sample/switchbasemaps/MainActivity.java
@@ -0,0 +1,116 @@
+/* Copyright 2015 Esri
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package com.esri.arcgisruntime.sample.switchbasemaps;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.esri.arcgisruntime.mapping.Basemap;
+import com.esri.arcgisruntime.mapping.Map;
+import com.esri.arcgisruntime.mapping.view.MapView;
+import com.esri.arcgisruntime.mapping.view.Viewpoint;
+import com.esri.arcgisruntime.mapping.view.ViewpointType;
+import com.esri.arcgisruntime.mapping.view.VisibleAreaChangedEvent;
+import com.esri.arcgisruntime.mapping.view.VisibleAreaChangedListener;
+
+public class MainActivity extends AppCompatActivity {
+
+    private MapView mMapView;
+    private Map mMap;
+    private Viewpoint mCurrentViewPoint;
+
+    // The basemap switching menu items.
+    private MenuItem mStreetsMenuItem = null;
+    private MenuItem mTopoMenuItem = null;
+    private MenuItem mGrayMenuItem = null;
+    private MenuItem mOceansMenuItem = null;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // inflate MapView from layout
+        mMapView = (MapView) findViewById(R.id.mapView);
+        // create a map with Topographic Basemap
+        mMap = new Map(Basemap.createTopographic());
+        // set the map to be displayed in this view
+        mMapView.setMap(mMap);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+
+        // Get the basemap switching menu items.
+        mStreetsMenuItem = menu.getItem(0);
+        mTopoMenuItem = menu.getItem(1);
+        mGrayMenuItem = menu.getItem(2);
+        mOceansMenuItem = menu.getItem(3);
+
+        // set the topo menu item checked by default
+        mTopoMenuItem.setChecked(true);
+
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        // handle menu item selection
+        switch(item.getItemId()){
+            case R.id.World_Street_Map:
+                // create a map with Streets Basemap
+                mMap.setBasemap(Basemap.createStreets());
+                mStreetsMenuItem.setChecked(true);
+                return true;
+            case R.id.World_Topo:
+                // create a map with Topographic Basemap
+                mMap.setBasemap(Basemap.createTopographic());
+                mTopoMenuItem.setChecked(true);
+                return true;
+            case R.id.Gray:
+                // create a map with Gray Basemap
+                mMap.setBasemap(Basemap.createLightGrayCanvas());
+                mGrayMenuItem.setChecked(true);
+                return true;
+            case R.id.Ocean_Basemap:
+                // create a map with Oceans Basemap
+                mMap.setBasemap(Basemap.createOceans());
+                mOceansMenuItem.setChecked(true);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    @Override
+    protected void onResume(){
+        super.onResume();
+        mMapView.resume();
+    }
+
+    @Override
+    protected void onPause(){
+        super.onPause();
+        mMapView.pause();
+
+    }
+}
