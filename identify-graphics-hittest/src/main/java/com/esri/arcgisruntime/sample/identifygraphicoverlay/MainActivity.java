561cf2f62a46a36b4237137327b8974f6957746d
Dan O'Neill <doneill@esri.com>
2016-03-21 20:41:36 -0800

    Updated to beta 2 workflow

--- identify-graphics-hittest/src/main/java/com/esri/arcgisruntime/sample/identifygraphicoverlay/MainActivity.java
+++ identify-graphics-hittest/src/main/java/com/esri/arcgisruntime/sample/identifygraphicoverlay/MainActivity.java
@@ -1,4 +1,4 @@
-/* Copyright 2015 Esri
+/* Copyright 2016 Esri
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -21,26 +21,19 @@ import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.MotionEvent;
 import android.widget.Toast;
 
 import com.esri.arcgisruntime.concurrent.ListenableFuture;
-import com.esri.arcgisruntime.geometry.Envelope;
-import com.esri.arcgisruntime.geometry.Point;
-import com.esri.arcgisruntime.geometry.PointCollection;
-import com.esri.arcgisruntime.geometry.Polygon;
+import com.esri.arcgisruntime.geometry.PolygonBuilder;
+import com.esri.arcgisruntime.geometry.SpatialReferences;
 import com.esri.arcgisruntime.mapping.Basemap;
 import com.esri.arcgisruntime.mapping.Map;
 import com.esri.arcgisruntime.mapping.view.DefaultMapViewOnTouchListener;
-import com.esri.arcgisruntime.mapping.view.DrawStatus;
-import com.esri.arcgisruntime.mapping.view.DrawStatusChangedEvent;
-import com.esri.arcgisruntime.mapping.view.DrawStatusChangedListener;
 import com.esri.arcgisruntime.mapping.view.Graphic;
 import com.esri.arcgisruntime.mapping.view.GraphicsOverlay;
 import com.esri.arcgisruntime.mapping.view.MapView;
-import com.esri.arcgisruntime.symbology.SimpleLineSymbol;
+import com.esri.arcgisruntime.symbology.SimpleFillSymbol;
 import com.esri.arcgisruntime.util.ListenableList;
 
 
@@ -62,7 +55,7 @@ public class MainActivity extends AppCompatActivity {
         mMapView = (MapView) findViewById(R.id.mapView);
 
         // create a map with the BasemapType topographic
-        Map mMap = new Map(Basemap.Type.TOPOGRAPHIC, 34.056295, -117.195800, 14);
+        Map mMap = new Map(Basemap.Type.TOPOGRAPHIC, 3.184710, -4.734690, 2);
         // set the map to be displayed in this view
         mMapView.setMap(mMap);
 
@@ -70,45 +63,22 @@ public class MainActivity extends AppCompatActivity {
         MapViewTouchListener mMapViewTouchListener = new MapViewTouchListener(this, mMapView);
         mMapView.setOnTouchListener(mMapViewTouchListener);
 
-        // work with the MapView after it has loaded
-        mMapView.addDrawStatusChangedListener(new DrawStatusChangedListener() {
-
-            @Override
-            public void drawStatusChanged(DrawStatusChangedEvent drawStatusChangedEvent) {
-                Log.i(TAG, "drawStatusChanged: " + drawStatusChangedEvent.getDrawStatus().toString());
-
-                if (drawStatusChangedEvent.getDrawStatus() == DrawStatus.COMPLETED) {
-                    // get center of MapView
-                    Polygon visibleArea = mMapView.getVisibleArea();
-                    Envelope polygonExtent = visibleArea.getExtent();
-                    Point center = polygonExtent.getCenter();
-                    // pass center of MapView to add graphic overlay
-                    addGraphicsOverlay(center);
-                }
-
-            }
-        });
+        addGraphicsOverlay();
     }
 
-    private void addGraphicsOverlay(Point center) {
-        // create values inside the visible area extent for creating graphic
-        double xValue = mMapView.getVisibleArea().getExtent().getWidth() / 5;
-        double yValue = mMapView.getVisibleArea().getExtent().getHeight() / 10;
-
-        // create point collection
-        PointCollection polyPoints = new PointCollection(mMapView.getSpatialReference());
-        polyPoints.add(new Point(center.getX() - xValue * 2, center.getY() - yValue * 2));
-        polyPoints.add(new Point(center.getX() - xValue * 2, center.getY() + yValue * 2));
-        polyPoints.add(new Point(center.getX() + xValue * 2, center.getY() + yValue * 2));
-        polyPoints.add(new Point(center.getX() + xValue * 2, center.getY() - yValue * 2));
-        // create polygon
-        Polygon polygon = new Polygon(polyPoints);
+    private void addGraphicsOverlay() {
+        // create the polygon
+        PolygonBuilder polygonGeometry = new PolygonBuilder(SpatialReferences.getWebMercator());
+        polygonGeometry.addPoint(-20e5, 20e5);
+        polygonGeometry.addPoint(20e5, 20.e5);
+        polygonGeometry.addPoint(20e5, -20e5);
+        polygonGeometry.addPoint(-20e5, -20e5);
 
         // create solid line symbol
-        int blue = Color.rgb(0, 0, 230);
-        SimpleLineSymbol solidLine = new SimpleLineSymbol(SimpleLineSymbol.Style.SOLID, blue, 4);
-        // create graphic from polygon and symbol
-        Graphic graphic = new Graphic(polygon, solidLine);
+        int yellow = Color.argb(4, 255, 255, 0);
+        SimpleFillSymbol polygonSymbol = new SimpleFillSymbol(yellow, SimpleFillSymbol.Style.SOLID, null);
+        // create graphic from polygon geometry and symbol
+        Graphic graphic = new Graphic(polygonGeometry.toGeometry(), polygonSymbol);
 
         // create graphics overlay
         grOverlay = new GraphicsOverlay();
@@ -118,28 +88,7 @@ public class MainActivity extends AppCompatActivity {
         graphics.add(graphic);
         // add graphics overlay to the MapView
         mMapView.getGraphicsOverlays().add(grOverlay);
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
+        Log.i(TAG, "Graphic Overlay added to MapView");
     }
 
     @Override
