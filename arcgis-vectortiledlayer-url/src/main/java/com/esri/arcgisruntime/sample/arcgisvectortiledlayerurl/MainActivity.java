4cb857b64b81de138d82e03f28316cdda40f72bc
Dan O'Neill <doneill@esri.com>
2016-03-29 19:32:03 -0800

    added lifecycle methods and simplifed ArrayAdapter

--- arcgis-vectortiledlayer-url/src/main/java/com/esri/arcgisruntime/sample/arcgisvectortiledlayerurl/MainActivity.java
+++ arcgis-vectortiledlayer-url/src/main/java/com/esri/arcgisruntime/sample/arcgisvectortiledlayerurl/MainActivity.java
@@ -63,13 +63,25 @@ public class MainActivity extends AppCompatActivity {
     mDrawerList = (ListView) findViewById(R.id.left_drawer);
 
     // Set the adapter for the list view
-    mDrawerList.setAdapter(new ArrayAdapter<String>(this,
-        R.layout.drawer_list_item, mNavigationDrawerItemTitles));
+    mDrawerList.setAdapter(new ArrayAdapter<>(this,
+            R.layout.drawer_list_item, mNavigationDrawerItemTitles));
     // Set the list's click listener
     mDrawerList.setOnItemClickListener(new DrawerItemClickListener());
 
     // set the navigation vector tiled layer item in the navigation drawer to selected
     mDrawerList.setItemChecked(0, true);
+  }
+
+  @Override
+  protected void onPause(){
+    super.onPause();
+    mMapView.pause();
+  }
+
+  @Override
+  protected void onResume(){
+    super.onResume();
+    mMapView.resume();
   }
 
   /**
