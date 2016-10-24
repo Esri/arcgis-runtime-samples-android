66e61edd372b067def1346893e9a24a940ab6037
will6489 <wcrick@esri.com>
2016-10-24 15:38:45 -0700

    removed duplicate setcontentview

--- picture-marker-symbols/src/main/java/com/esri/arcgisruntime/sample/picturemarkersymbols/MainActivity.java
+++ picture-marker-symbols/src/main/java/com/esri/arcgisruntime/sample/picturemarkersymbols/MainActivity.java
@@ -58,7 +58,6 @@ public class MainActivity extends AppCompatActivity {
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
-    setContentView(R.layout.activity_main);
 
     setContentView(R.layout.activity_main);
 
