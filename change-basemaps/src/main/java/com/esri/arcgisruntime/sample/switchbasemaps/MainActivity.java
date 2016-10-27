c622b51ab4482ae939beb786ed4a7962e6465cf0
Dan O'Neill <doneill@esri.com>
2016-10-26 18:02:52 -0700

    refactor ActionBar overflow to navigation drawer, add vector basemap

--- change-basemaps/src/main/java/com/esri/arcgisruntime/sample/switchbasemaps/MainActivity.java
+++ change-basemaps/src/main/java/com/esri/arcgisruntime/sample/switchbasemaps/MainActivity.java
@@ -16,10 +16,16 @@
 
 package com.esri.arcgisruntime.sample.switchbasemaps;
 
+import android.content.res.Configuration;
 import android.os.Bundle;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import com.esri.arcgisruntime.mapping.ArcGISMap;
 import com.esri.arcgisruntime.mapping.Basemap;
@@ -30,16 +36,34 @@ public class MainActivity extends AppCompatActivity {
     private MapView mMapView;
     private ArcGISMap mMap;
 
-    // The basemap switching menu items.
-    private MenuItem mStreetsMenuItem = null;
-    private MenuItem mTopoMenuItem = null;
-    private MenuItem mGrayMenuItem = null;
-    private MenuItem mOceansMenuItem = null;
+    private String[] mNavigationDrawerItemTitles;
+
+    private ListView mDrawerList;
+
+    private ActionBarDrawerToggle mDrawerToggle;
+    private DrawerLayout mDrawerLayout;
+    private String mActivityTitle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // inflate navigation drawer
+        mNavigationDrawerItemTitles = getResources().getStringArray(R.array.vector_tiled_types);
+        mDrawerList = (ListView)findViewById(R.id.navList);
+        mDrawerLayout = (DrawerLayout)findViewById(R.id.drawer_layout);
+        // get app title
+        mActivityTitle = getTitle().toString();
+
+        addDrawerItems();
+        setupDrawer();
+
+        if(getSupportActionBar() != null){
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+            getSupportActionBar().setHomeButtonEnabled(true);
+            getSupportActionBar().setTitle("");
+        }
 
         // inflate MapView from layout
         mMapView = (MapView) findViewById(R.id.mapView);
@@ -49,50 +73,100 @@ public class MainActivity extends AppCompatActivity {
         mMapView.setMap(mMap);
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
+    /**
+     * Add navigation drawer items
+     */
+    private void addDrawerItems(){
+        ArrayAdapter<String> mAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, mNavigationDrawerItemTitles);
+        mDrawerList.setAdapter(mAdapter);
 
-        // Get the basemap switching menu items.
-        mStreetsMenuItem = menu.getItem(0);
-        mTopoMenuItem = menu.getItem(1);
-        mGrayMenuItem = menu.getItem(2);
-        mOceansMenuItem = menu.getItem(3);
+        mDrawerList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int position, long id) {
+                selectBasemap(position);
+            }
+        });
+    }
 
-        // set the topo menu item checked by default
-        mTopoMenuItem.setChecked(true);
+    /**
+     * Set up the navigation drawer
+     */
+    private void setupDrawer(){
 
-        return true;
+        mDrawerToggle = new ActionBarDrawerToggle(this, mDrawerLayout, R.string.drawer_open, R.string.drawer_close){
+
+            /** Called when a drawer has settled in a completely open state. */
+            public void onDrawerOpened(View drawerView) {
+                super.onDrawerOpened(drawerView);
+                getSupportActionBar().setTitle(mActivityTitle);
+                invalidateOptionsMenu(); // creates call to onPrepareOptionsMenu()
+            }
+
+            /** Called when a drawer has settled in a completely closed state. */
+            public void onDrawerClosed(View view) {
+                super.onDrawerClosed(view);
+                invalidateOptionsMenu(); // creates call to onPrepareOptionsMenu()
+            }
+        };
+
+        mDrawerToggle.setDrawerIndicatorEnabled(true);
+        mDrawerLayout.addDrawerListener(mDrawerToggle);
+    }
+
+    /**
+     * Select the Basemap item based on position in the navigation drawer
+     *
+     * @param position order int in navigation drawer
+     */
+    private void selectBasemap(int position){
+        // update selected item and title, then close the drawer
+        mDrawerList.setItemChecked(position, true);
+        mDrawerLayout.closeDrawer(mDrawerList);
+
+        // if-else is used because this sample is used elsewhere as a Library module
+        if(position == 0){
+            // position 0 = Streets
+            mMap.setBasemap(Basemap.createStreets());
+            getSupportActionBar().setTitle(mNavigationDrawerItemTitles[position]);
+        } else if(position == 1){
+            // position 1 = Navigation Vector
+            mMap.setBasemap(Basemap.createNavigationVector());
+            getSupportActionBar().setTitle(mNavigationDrawerItemTitles[position]);
+        } else if(position == 2){
+            // position 2 = Topographic
+            mMap.setBasemap(Basemap.createTopographic());
+            getSupportActionBar().setTitle(mNavigationDrawerItemTitles[position]);
+        } else if(position == 3){
+            // position 3 = Topographic Vector
+            mMap.setBasemap(Basemap.createTopographicVector());
+            getSupportActionBar().setTitle(mNavigationDrawerItemTitles[position]);
+        } else if (position == 4){
+            // position 3 = Gray Canvas
+            mMap.setBasemap(Basemap.createLightGrayCanvas());
+            getSupportActionBar().setTitle(mNavigationDrawerItemTitles[position]);
+        } else if (position == 5){
+            // position 3 = Gray Canvas Vector
+            mMap.setBasemap(Basemap.createLightGrayCanvasVector());
+            getSupportActionBar().setTitle(mNavigationDrawerItemTitles[position]);
+        }
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // handle menu item selection
-        //if-else is used because this sample is used elsewhere as a Library module
-        int itemId = item.getItemId();
-        if (itemId == R.id.World_Street_Map) {
-            mMap.setBasemap(Basemap.createStreets());
-            mStreetsMenuItem.setChecked(true);
-            return true;
-        }else if (itemId == R.id.World_Topo) {
-            // create a map with Topographic Basemap
-            mMap.setBasemap(Basemap.createTopographic());
-            mTopoMenuItem.setChecked(true);
-            return true;
-        }else if (itemId == R.id.Gray) {
-            // create a map with Gray Basemap
-            mMap.setBasemap(Basemap.createLightGrayCanvas());
-            mGrayMenuItem.setChecked(true);
-            return true;
-        }else if (itemId == R.id.Ocean_Basemap) {
-            // create a map with Oceans Basemap
-            mMap.setBasemap(Basemap.createOceans());
-            mOceansMenuItem.setChecked(true);
-            return true;
-        }else {
-            return super.onOptionsItemSelected(item);
-        }
+        // Activate the navigation drawer toggle
+        return (mDrawerToggle.onOptionsItemSelected(item)) || super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        mDrawerToggle.syncState();
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        mDrawerToggle.onConfigurationChanged(newConfig);
     }
 
     @Override
