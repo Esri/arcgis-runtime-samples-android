8ea52f790f3ac94672c7ee10db4d51dad0c41f8d
Dan O'Neill <doneill@esri.com>
2017-04-25 13:58:16 -0700

    simplified pattern

--- list-related-features/README.md
+++ list-related-features/README.md
@@ -11,47 +11,41 @@ The List Related Features app has a `MapView` preloaded with a web map `FeatureL
 * RelationshipInfo
 
 ## Developer Pattern
-Relationships for tables are represented in `RelationshipInfo` which are used as parameters on a `FeatureTable.queryRelatedFeaturesAsync` method.  The results returned represent related `Feature`s of which you can obtain the attribute values which are displayed.
+The `FeatureTable` from a selected `Feature` is used as parameters on a `FeatureTable.queryRelatedFeaturesAsync` method.  The results returned represent related `Feature`s of which you can obtain the attribute values which are displayed.
 
 ```java
-// get FeatureTable RelationshipInfos for table relationship
 ArcGISFeatureTable selectedTable = (ArcGISFeatureTable)feature.getFeatureTable();
-List<RelationshipInfo> relationshipInfos = selectedTable.getLayerInfo().getRelationshipInfos();
-// iterate through returned relationship infos
-for(RelationshipInfo relationshipInfo : relationshipInfos){
-    RelatedQueryParameters relatedQueryParameters = new RelatedQueryParameters(relationshipInfo);
-    // query for features related to selectedTable, returns results from tables added to the Map
-    final ListenableFuture<List<RelatedFeatureQueryResult>> relatedFeatureQueryResultFuture = selectedTable.queryRelatedFeaturesAsync(arcGISFeature, relatedQueryParameters);
-    relatedFeatureQueryResultFuture.addDoneListener(new Runnable() {
-        @Override
-        public void run() {
-            try {
-                List<RelatedFeatureQueryResult> relatedFeatureQueryResultList = relatedFeatureQueryResultFuture.get();
-                // iterate over returned RelatedFeatureQueryResults
-                for(RelatedFeatureQueryResult relatedQueryResult : relatedFeatureQueryResultList){
-                    // iterate over Features returned
-                    for (Feature relatedFeature : relatedQueryResult) {
-                        // check if FeatureTable name has been added to List
-                        if(!mRelatedValues.contains("Table - " + relatedFeature.getFeatureTable().getTableName() + ":")){
-                            mRelatedValues.add("Table - " + relatedFeature.getFeatureTable().getTableName() + ":");
-                        }
-                        // feature returned from selection query
-                        Map<String, Object> attributes = relatedFeature.getAttributes();
-                        // add related feature attributes to List
-                        for (Map.Entry<String, Object> attribute : attributes.entrySet()) {
-                            if(!attribute.getKey().equals("OBJECTID")){
-                                mRelatedValues.add(attribute.getValue().toString());
-                            }
-                        }
-                        // notify ListAdapter content has changed
-                        mArrayAdapter.notifyDataSetChanged();
-                    }
-                }
 
-            } catch (Exception e) {
-                Log.e(TAG, "Exception occurred: " + e.getMessage());
+final ListenableFuture<List<RelatedFeatureQueryResult>> relatedFeatureQueryResultFuture = selectedTable.queryRelatedFeaturesAsync(arcGISFeature);
+relatedFeatureQueryResultFuture.addDoneListener(new Runnable() {
+    @Override
+    public void run() {
+        try {
+            List<RelatedFeatureQueryResult> relatedFeatureQueryResultList = relatedFeatureQueryResultFuture.get();
+            // iterate over returned RelatedFeatureQueryResults
+            for(RelatedFeatureQueryResult relatedQueryResult : relatedFeatureQueryResultList){
+                // iterate over Features returned
+                for (Feature relatedFeature : relatedQueryResult) {
+                    // check if FeatureTable name has been added to List
+                    if(!mRelatedValues.contains("Table - " + relatedFeature.getFeatureTable().getTableName() + ":")){
+                        mRelatedValues.add("Table - " + relatedFeature.getFeatureTable().getTableName() + ":");
+                    }
+                    // feature returned from selection query
+                    Map<String, Object> attributes = relatedFeature.getAttributes();
+                    // add related feature attributes to List
+                    for (Map.Entry<String, Object> attribute : attributes.entrySet()) {
+                        if(!attribute.getKey().equals("OBJECTID")){
+                            mRelatedValues.add(attribute.getValue().toString());
+                        }
+                    }
+                    // notify ListAdapter content has changed
+                    mArrayAdapter.notifyDataSetChanged();
+                }
             }
+
+        } catch (Exception e) {
+            Log.e(TAG, "Exception occurred: " + e.getMessage());
         }
-    });
-}
+    }
+});
 ```
--- list-related-features/src/main/java/com/esri/arcgisruntime/sample/listrelatedfeatures/MainActivity.java
+++ list-related-features/src/main/java/com/esri/arcgisruntime/sample/listrelatedfeatures/MainActivity.java
@@ -13,7 +13,6 @@ import android.view.MotionEvent;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
-import com.esri.arcgisruntime.arcgisservices.RelationshipInfo;
 import com.esri.arcgisruntime.concurrent.ListenableFuture;
 import com.esri.arcgisruntime.data.ArcGISFeature;
 import com.esri.arcgisruntime.data.ArcGISFeatureTable;
@@ -21,7 +20,6 @@ import com.esri.arcgisruntime.data.Feature;
 import com.esri.arcgisruntime.data.FeatureQueryResult;
 import com.esri.arcgisruntime.data.QueryParameters;
 import com.esri.arcgisruntime.data.RelatedFeatureQueryResult;
-import com.esri.arcgisruntime.data.RelatedQueryParameters;
 import com.esri.arcgisruntime.geometry.Envelope;
 import com.esri.arcgisruntime.geometry.Point;
 import com.esri.arcgisruntime.layers.FeatureLayer;
@@ -125,46 +123,42 @@ public class MainActivity extends AppCompatActivity {
                         ArcGISFeature arcGISFeature = (ArcGISFeature)feature;
                         featureLayer.setSelectionColor(Color.YELLOW);
                         featureLayer.setSelectionWidth(5);
-                        // get FeatureTable RelationshipInfos for table relationship
-                        ArcGISFeatureTable selectedTable = (ArcGISFeatureTable)feature.getFeatureTable();
-                        List<RelationshipInfo> relationshipInfos = selectedTable.getLayerInfo().getRelationshipInfos();
-                        // iterate through returned relationship infos
-                        for(RelationshipInfo relationshipInfo : relationshipInfos){
-                            RelatedQueryParameters relatedQueryParameters = new RelatedQueryParameters(relationshipInfo);
-                            // query for features related to selectedTable, returns results from tables added to the Map
-                            final ListenableFuture<List<RelatedFeatureQueryResult>> relatedFeatureQueryResultFuture = selectedTable.queryRelatedFeaturesAsync(arcGISFeature, relatedQueryParameters);
-                            relatedFeatureQueryResultFuture.addDoneListener(new Runnable() {
-                                @Override
-                                public void run() {
-                                    try {
-                                        List<RelatedFeatureQueryResult> relatedFeatureQueryResultList = relatedFeatureQueryResultFuture.get();
-                                        // iterate over returned RelatedFeatureQueryResults
-                                        for(RelatedFeatureQueryResult relatedQueryResult : relatedFeatureQueryResultList){
-                                            // iterate over Features returned
-                                            for (Feature relatedFeature : relatedQueryResult) {
-                                                // check if FeatureTable name has been added to List
-                                                if(!mRelatedValues.contains("Table - " + relatedFeature.getFeatureTable().getTableName() + ":")){
-                                                    mRelatedValues.add("Table - " + relatedFeature.getFeatureTable().getTableName() + ":");
-                                                }
-                                                // feature returned from selection query
-                                                Map<String, Object> attributes = relatedFeature.getAttributes();
-                                                // add related feature attributes to List
-                                                for (Map.Entry<String, Object> attribute : attributes.entrySet()) {
-                                                    if(!attribute.getKey().equals("OBJECTID")){
-                                                        mRelatedValues.add(attribute.getValue().toString());
-                                                    }
-                                                }
-                                                // notify ListAdapter content has changed
-                                                mArrayAdapter.notifyDataSetChanged();
-                                            }
-                                        }
 
-                                    } catch (Exception e) {
-                                        Log.e(TAG, "Exception occurred: " + e.getMessage());
+                        ArcGISFeatureTable selectedTable = (ArcGISFeatureTable)feature.getFeatureTable();
+
+                        final ListenableFuture<List<RelatedFeatureQueryResult>> relatedFeatureQueryResultFuture = selectedTable.queryRelatedFeaturesAsync(arcGISFeature);
+                        relatedFeatureQueryResultFuture.addDoneListener(new Runnable() {
+                            @Override
+                            public void run() {
+                                try {
+                                    List<RelatedFeatureQueryResult> relatedFeatureQueryResultList = relatedFeatureQueryResultFuture.get();
+                                    // iterate over returned RelatedFeatureQueryResults
+                                    for(RelatedFeatureQueryResult relatedQueryResult : relatedFeatureQueryResultList){
+                                        // iterate over Features returned
+                                        for (Feature relatedFeature : relatedQueryResult) {
+                                            // check if FeatureTable name has been added to List
+                                            if(!mRelatedValues.contains("Table - " + relatedFeature.getFeatureTable().getTableName() + ":")){
+                                                mRelatedValues.add("Table - " + relatedFeature.getFeatureTable().getTableName() + ":");
+                                            }
+                                            // feature returned from selection query
+                                            Map<String, Object> attributes = relatedFeature.getAttributes();
+                                            // add related feature attributes to List
+                                            for (Map.Entry<String, Object> attribute : attributes.entrySet()) {
+                                                if(!attribute.getKey().equals("OBJECTID")){
+                                                    mRelatedValues.add(attribute.getValue().toString());
+                                                }
+                                            }
+                                            // notify ListAdapter content has changed
+                                            mArrayAdapter.notifyDataSetChanged();
+                                        }
                                     }
+
+                                } catch (Exception e) {
+                                    Log.e(TAG, "Exception occurred: " + e.getMessage());
                                 }
-                            });
-                        }
+                            }
+                        });
+
                     }
                 } catch (Exception e) {
                     Log.e(TAG, "Exception occurred: " + e.getMessage());
