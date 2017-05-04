046b4315d7e90a0b947c58e0d11872898d5d5b4b
Dan O'Neill <doneill@esri.com>
2017-05-04 11:23:36 -0700

    added license to source

--- blend-renderer/src/main/java/com/esri/arcgisruntime/sample/blendrenderer/MainActivity.java
+++ blend-renderer/src/main/java/com/esri/arcgisruntime/sample/blendrenderer/MainActivity.java
@@ -1,5 +1,23 @@
+/* Copyright 2017 Esri
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
 package com.esri.arcgisruntime.sample.blendrenderer;
 
+import java.io.File;
+import java.util.Collections;
 import android.Manifest;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
@@ -13,6 +31,7 @@ import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.widget.Toast;
+
 import com.esri.arcgisruntime.layers.RasterLayer;
 import com.esri.arcgisruntime.mapping.ArcGISMap;
 import com.esri.arcgisruntime.mapping.Basemap;
@@ -21,9 +40,6 @@ import com.esri.arcgisruntime.raster.BlendRenderer;
 import com.esri.arcgisruntime.raster.ColorRamp;
 import com.esri.arcgisruntime.raster.Raster;
 import com.esri.arcgisruntime.raster.SlopeType;
-
-import java.io.File;
-import java.util.Collections;
 
 public class MainActivity extends AppCompatActivity implements ParametersDialogFragment.ParametersListener {
 
