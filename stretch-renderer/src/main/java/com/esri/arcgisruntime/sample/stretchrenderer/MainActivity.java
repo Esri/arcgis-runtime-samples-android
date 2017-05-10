561ef1e20155930cad6d4a39fcddba8e7efbb37b
trev8939 <tdraeseke@esri.com>
2017-05-10 12:27:34 +0100

    formatting fixes

--- stretch-renderer/src/main/java/com/esri/arcgisruntime/sample/stretchrenderer/MainActivity.java
+++ stretch-renderer/src/main/java/com/esri/arcgisruntime/sample/stretchrenderer/MainActivity.java
@@ -25,7 +25,6 @@ import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
