4bed176d6b54075f9933d6b4fa175de8049b22d5
trev8939 <tdraeseke@esri.com>
2017-07-07 16:45:53 +0100

    intial commit scene-layer

---
+++ scene-layer/src/main/java/com/esri/arcgisruntime/sample/scenelayer/MainActivity.java
@@ -0,0 +1,73 @@
+/* Copyright 2017 Esri
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package com.esri.arcgisruntime.sample.scenelayer;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.esri.arcgisruntime.layers.ArcGISSceneLayer;
+import com.esri.arcgisruntime.mapping.ArcGISScene;
+import com.esri.arcgisruntime.mapping.ArcGISTiledElevationSource;
+import com.esri.arcgisruntime.mapping.Basemap;
+import com.esri.arcgisruntime.mapping.view.Camera;
+import com.esri.arcgisruntime.mapping.view.SceneView;
+
+public class MainActivity extends AppCompatActivity {
+
+  private SceneView mSceneView;
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main);
+
+    // create a scene and add a basemap to it
+    ArcGISScene scene = new ArcGISScene();
+    scene.setBasemap(Basemap.createImagery());
+
+    mSceneView = (SceneView) findViewById(R.id.sceneView);
+    mSceneView.setScene(scene);
+
+    // add a scene service to the scene for viewing buildings
+    ArcGISSceneLayer sceneLayer = new ArcGISSceneLayer(
+        "http://tiles.arcgis.com/tiles/P3ePLMYs2RVChkJx/arcgis/rest/services/Buildings_Brest/SceneServer");
+    scene.getOperationalLayers().add(sceneLayer);
+
+    // create an elevation source, and add this to the base surface of the scene
+    ArcGISTiledElevationSource elevationSource = new ArcGISTiledElevationSource(
+        getResources().getString(R.string.elevation_image_service));
+    scene.getBaseSurface().getElevationSources().add(elevationSource);
+
+    // add a camera and initial camera position
+    Camera camera = new Camera(48.378, -4.494, 200, 345, 65, 0);
+    mSceneView.setViewpointCamera(camera);
+  }
+
+  @Override
+  protected void onPause() {
+    super.onPause();
+    // pause SceneView
+    mSceneView.pause();
+  }
+
+  @Override
+  protected void onResume() {
+    super.onResume();
+    // resume SceneView
+    mSceneView.resume();
+  }
+}
