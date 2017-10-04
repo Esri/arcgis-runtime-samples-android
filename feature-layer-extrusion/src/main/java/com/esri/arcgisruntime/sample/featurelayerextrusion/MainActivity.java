2d6e796ed7854d81e35d6555176852bd5df87700
trev8939 <tdraeseke@esri.com>
2017-10-04 16:48:37 +0100

    remove graphicOverlays

--- feature-layer-extrusion/src/main/java/com/esri/arcgisruntime/sample/featurelayerextrusion/MainActivity.java
+++ feature-layer-extrusion/src/main/java/com/esri/arcgisruntime/sample/featurelayerextrusion/MainActivity.java
@@ -16,19 +16,12 @@
 
 package com.esri.arcgisruntime.sample.featurelayerextrusion;
 
-import java.util.concurrent.ExecutionException;
-
 import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
-import com.esri.arcgisruntime.concurrent.ListenableFuture;
-import com.esri.arcgisruntime.data.Feature;
-import com.esri.arcgisruntime.data.FeatureQueryResult;
-import com.esri.arcgisruntime.data.QueryParameters;
 import com.esri.arcgisruntime.data.ServiceFeatureTable;
 import com.esri.arcgisruntime.geometry.Point;
 import com.esri.arcgisruntime.geometry.SpatialReferences;
@@ -36,8 +29,6 @@ import com.esri.arcgisruntime.layers.FeatureLayer;
 import com.esri.arcgisruntime.mapping.ArcGISScene;
 import com.esri.arcgisruntime.mapping.Basemap;
 import com.esri.arcgisruntime.mapping.view.Camera;
-import com.esri.arcgisruntime.mapping.view.Graphic;
-import com.esri.arcgisruntime.mapping.view.GraphicsOverlay;
 import com.esri.arcgisruntime.mapping.view.OrbitLocationCameraController;
 import com.esri.arcgisruntime.mapping.view.SceneView;
 import com.esri.arcgisruntime.symbology.Renderer;
@@ -68,44 +59,7 @@ public class MainActivity extends AppCompatActivity {
     ServiceFeatureTable serviceFeatureTable = new ServiceFeatureTable(
         getResources().getString(R.string.us_census_feature_service));
 
-    //TODO Remove
-    final GraphicsOverlay extrusionOverlay = new GraphicsOverlay(GraphicsOverlay.RenderingMode.DYNAMIC);
-
     FeatureLayer states = new FeatureLayer(serviceFeatureTable);
-
-
-
-
-
-    // set a query parameter which will get all features
-    QueryParameters queryParameters = new QueryParameters();
-    queryParameters.setWhereClause("1=1");
-    // execute the query being sure to load all fields
-    final ListenableFuture<FeatureQueryResult> featureQueryResultFuture = serviceFeatureTable
-        .queryFeaturesAsync(queryParameters, ServiceFeatureTable.QueryFeatureFields.LOAD_ALL);
-    featureQueryResultFuture.addDoneListener(new Runnable() {
-      @Override public void run() {
-        try {
-          // get the feature query result
-          FeatureQueryResult featureQueryResult = featureQueryResultFuture.get();
-          for (Feature state : featureQueryResult) {
-            //TODO Remove
-            Graphic stateGraphic = new Graphic(state.getGeometry());
-            int totalPopulation = Integer.parseInt(state.getAttributes().get("POP2007").toString());
-            int densityPopulation = (int) Math
-                .round(Double.parseDouble(state.getAttributes().get("POP07_SQMI").toString()));
-            Log.d("density", String.valueOf(densityPopulation));
-            // scale the total population down by a factor of 10 and set it as an attribute
-            stateGraphic.getAttributes().put("TOTAL_POP", totalPopulation / 10);
-            // scale the population density up by a factor of 1000 and set it as an attribute
-            stateGraphic.getAttributes().put("DENSITY_POP", densityPopulation * 10000);
-            extrusionOverlay.getGraphics().add(stateGraphic);
-          }
-        } catch (InterruptedException | ExecutionException e) {
-          Log.e(TAG, "Error getting FeatureQueryResult: " + e.getMessage());
-        }
-      }
-    });
 
     // create a scene and add a basemap to it
     ArcGISScene scene = new ArcGISScene();
@@ -114,7 +68,7 @@ public class MainActivity extends AppCompatActivity {
     mSceneView = (SceneView) findViewById(R.id.sceneView);
     // set the scene to the scene view
     mSceneView.setScene(scene);
-    mSceneView.getGraphicsOverlays().add(extrusionOverlay);
+
 
     // define a look at point for the camera at geographical center of the continental US
     Point lookAtPoint = new Point(-10974490, 4814376, 0, SpatialReferences.getWebMercator());
@@ -148,8 +102,6 @@ public class MainActivity extends AppCompatActivity {
           togglePopButton.setText(R.string.density_pop);
           showTotalPopulation = true;
         }
-        //TODO Remove
-        extrusionOverlay.setRenderer(renderer);
       }
     });
     // click to set initial state
