d0ba7831ac97ae5d497d1a2a59da5e47f6922b1c
trev8939 <tdraeseke@esri.com>
2017-10-06 14:49:28 +0100

    working sample

--- feature-layer-extrusion/src/main/java/com/esri/arcgisruntime/sample/featurelayerextrusion/MainActivity.java
+++ feature-layer-extrusion/src/main/java/com/esri/arcgisruntime/sample/featurelayerextrusion/MainActivity.java
@@ -22,6 +22,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 
+import com.esri.arcgisruntime.data.QueryParameters;
 import com.esri.arcgisruntime.data.ServiceFeatureTable;
 import com.esri.arcgisruntime.geometry.Point;
 import com.esri.arcgisruntime.geometry.SpatialReferences;
@@ -38,10 +39,6 @@ import com.esri.arcgisruntime.symbology.SimpleRenderer;
 
 public class MainActivity extends AppCompatActivity {
 
-  private final String TAG = MainActivity.class.getSimpleName();
-
-  private SceneView mSceneView;
-
   private boolean showTotalPopulation;
 
   @Override
@@ -49,36 +46,42 @@ public class MainActivity extends AppCompatActivity {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
 
-    // set flag for showing total or density population
+    // set flag for showing total population or population density
     showTotalPopulation = true;
 
     // inflate population type toggle button
     final Button togglePopButton = (Button) findViewById(R.id.toggle_button);
 
     // get us census data as a service feature table
-    ServiceFeatureTable serviceFeatureTable = new ServiceFeatureTable(
+    ServiceFeatureTable statesServiceFeatureTable = new ServiceFeatureTable(
         getResources().getString(R.string.us_census_feature_service));
 
+    // load all fields in the service feature table
+    QueryParameters queryParams = new QueryParameters();
+    queryParams.setWhereClause("1=1");
+    statesServiceFeatureTable.queryFeaturesAsync(queryParams, ServiceFeatureTable.QueryFeatureFields.LOAD_ALL);
 
+    // add the service feature table to a feature layer
+    final FeatureLayer statesFeatureLayer = new FeatureLayer(statesServiceFeatureTable);
+    // set the feature layer to render dynamically to allow extrusion
+    statesFeatureLayer.setFeatureRenderingMode(FeatureLayer.RenderingMode.DYNAMIC);
 
-    // create a scene and add a basemap to it
+    // create a scene and add it to the scene view
     ArcGISScene scene = new ArcGISScene(Basemap.createImagery());
-    //scene.setBasemap(Basemap.createImagery());
-    // create SceneView from layout
-    mSceneView = (SceneView) findViewById(R.id.sceneView);
-    scene.getLoadSettings().setPreferredPolygonFeatureRenderingMode(FeatureLayer.RenderingMode.DYNAMIC);
-    // set the scene to the scene view
-    mSceneView.setScene(scene);
-    final FeatureLayer states = new FeatureLayer(serviceFeatureTable);
-    //states.setFeatureRenderingMode(FeatureLayer.RenderingMode.DYNAMIC);
-    scene.getOperationalLayers().add(states);
+    SceneView sceneView = (SceneView) findViewById(R.id.sceneView);
+    sceneView.setScene(scene);
 
-    // define line and fill renderers
+    // add the feature layer to the scene
+    scene.getOperationalLayers().add(statesFeatureLayer);
+
+    // define line and fill symbols for a simple renderer
     SimpleLineSymbol lineSymbol = new SimpleLineSymbol(SimpleLineSymbol.Style.SOLID, Color.BLACK, 1.0f);
     SimpleFillSymbol fillSymbol = new SimpleFillSymbol(SimpleFillSymbol.Style.SOLID, Color.BLUE, lineSymbol);
-    SimpleRenderer renderer = new SimpleRenderer(fillSymbol);
-    states.setRenderer(renderer);
-    states.getRenderer().getSceneProperties().setExtrusionMode(Renderer.SceneProperties.ExtrusionMode.ABSOLUTE_HEIGHT);
+    final SimpleRenderer renderer = new SimpleRenderer(fillSymbol);
+    // set renderer extrusion mode to base height, which includes base height of each vertex in calculating z values
+    renderer.getSceneProperties().setExtrusionMode(Renderer.SceneProperties.ExtrusionMode.BASE_HEIGHT);
+    // set the simple renderer to the feature layer
+    statesFeatureLayer.setRenderer(renderer);
 
     // define a look at point for the camera at geographical center of the continental US
     Point lookAtPoint = new Point(-10974490, 4814376, 0, SpatialReferences.getWebMercator());
@@ -86,21 +89,23 @@ public class MainActivity extends AppCompatActivity {
     Camera camera = new Camera(lookAtPoint, 20000000, 0, 55, 0);
     // set the camera to orbit the look at point
     OrbitLocationCameraController orbitCamera = new OrbitLocationCameraController(lookAtPoint, 20000000);
-    mSceneView.setCameraController(orbitCamera);
-    mSceneView.setViewpointCamera(camera);
+    sceneView.setCameraController(orbitCamera);
+    sceneView.setViewpointCamera(camera);
 
     // set button listener
     togglePopButton.setOnClickListener(new View.OnClickListener() {
       @Override public void onClick(View v) {
         // set extrusion properties to either show total population or population density based on flag
         if (showTotalPopulation) {
-          states.getRenderer().getSceneProperties().setExtrusionExpression("[POP2007]");
-          // change text of button to reflect showing total population
+          // divide total population by 10 to make data legible
+          renderer.getSceneProperties().setExtrusionExpression("[POP2007] / 10");
+          // change text of button to total pop
           togglePopButton.setText(R.string.total_pop);
           showTotalPopulation = false;
         } else {
-          states.getRenderer().getSceneProperties().setExtrusionExpression("[POP07_SQMI]");
-          // change text of button to reflect showing population density
+          // multiple population density by 5000 to make data legible
+          renderer.getSceneProperties().setExtrusionExpression("[POP07_SQMI] * 5000");
+          // change text of button to pop density
           togglePopButton.setText(R.string.density_pop);
           showTotalPopulation = true;
         }
