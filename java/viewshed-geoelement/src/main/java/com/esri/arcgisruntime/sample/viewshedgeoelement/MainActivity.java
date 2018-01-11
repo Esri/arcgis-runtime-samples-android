60c209752612c9a41df96a57e91c18904e674edb
trev8939 <tdraeseke@esri.com>
2018-01-11 16:45:34 +0000

    working sample

--- java/viewshed-geoelement/src/main/java/com/esri/arcgisruntime/sample/viewshedgeoelement/MainActivity.java
+++ java/viewshed-geoelement/src/main/java/com/esri/arcgisruntime/sample/viewshedgeoelement/MainActivity.java
@@ -1,11 +1,27 @@
 package com.esri.arcgisruntime.sample.viewshedgeoelement;
 
-import java.io.File;
+import java.util.Timer;
+import java.util.TimerTask;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.os.Environment;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.view.MotionEvent;
+import android.widget.Toast;
 
 import com.esri.arcgisruntime.geoanalysis.GeoElementViewshed;
+import com.esri.arcgisruntime.geometry.AngularUnit;
+import com.esri.arcgisruntime.geometry.AngularUnitId;
+import com.esri.arcgisruntime.geometry.GeodeticCurveType;
+import com.esri.arcgisruntime.geometry.GeodeticDistanceResult;
+import com.esri.arcgisruntime.geometry.GeometryEngine;
+import com.esri.arcgisruntime.geometry.LinearUnit;
+import com.esri.arcgisruntime.geometry.LinearUnitId;
 import com.esri.arcgisruntime.geometry.Point;
 import com.esri.arcgisruntime.geometry.SpatialReferences;
 import com.esri.arcgisruntime.layers.ArcGISSceneLayer;
@@ -14,9 +30,11 @@ import com.esri.arcgisruntime.mapping.ArcGISTiledElevationSource;
 import com.esri.arcgisruntime.mapping.Basemap;
 import com.esri.arcgisruntime.mapping.Surface;
 import com.esri.arcgisruntime.mapping.view.AnalysisOverlay;
+import com.esri.arcgisruntime.mapping.view.DefaultSceneViewOnTouchListener;
 import com.esri.arcgisruntime.mapping.view.Graphic;
 import com.esri.arcgisruntime.mapping.view.GraphicsOverlay;
 import com.esri.arcgisruntime.mapping.view.LayerSceneProperties;
+import com.esri.arcgisruntime.mapping.view.OrbitGeoElementCameraController;
 import com.esri.arcgisruntime.mapping.view.SceneView;
 import com.esri.arcgisruntime.symbology.ModelSceneSymbol;
 import com.esri.arcgisruntime.symbology.Renderer;
@@ -24,6 +42,13 @@ import com.esri.arcgisruntime.symbology.SceneSymbol;
 import com.esri.arcgisruntime.symbology.SimpleRenderer;
 
 public class MainActivity extends AppCompatActivity {
+
+  private static final LinearUnit METERS = new LinearUnit(LinearUnitId.METERS);
+  private static final AngularUnit DEGREES = new AngularUnit(AngularUnitId.DEGREES);
+  private SceneView mSceneView;
+  private Point mWaypoint;
+  private Graphic mTankGraphic;
+  private Timer mTimer;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
@@ -35,8 +60,8 @@ public class MainActivity extends AppCompatActivity {
     scene.setBasemap(Basemap.createImagery());
 
     // add the SceneView to the stack pane
-    SceneView sceneView = findViewById(R.id.sceneView);
-    sceneView.setScene(scene);
+    mSceneView = findViewById(R.id.sceneView);
+    mSceneView.setScene(scene);
 
     // add base surface for elevation data
     Surface surface = new Surface();
@@ -47,10 +72,15 @@ public class MainActivity extends AppCompatActivity {
     ArcGISSceneLayer sceneLayer = new ArcGISSceneLayer(getString(R.string.buildings_layer));
     scene.getOperationalLayers().add(sceneLayer);
 
+    // request read permission
+    requestReadPermission();
+  }
+
+  private void viewshedGeoElement() {
     // create a graphics overlay for the tank
     GraphicsOverlay graphicsOverlay = new GraphicsOverlay();
     graphicsOverlay.getSceneProperties().setSurfacePlacement(LayerSceneProperties.SurfacePlacement.RELATIVE);
-    sceneView.getGraphicsOverlays().add(graphicsOverlay);
+    mSceneView.getGraphicsOverlays().add(graphicsOverlay);
 
     // set up heading expression for tank
     SimpleRenderer renderer3D = new SimpleRenderer();
@@ -59,23 +89,123 @@ public class MainActivity extends AppCompatActivity {
     graphicsOverlay.setRenderer(renderer3D);
 
     // create a graphic of a tank
-    String modelURI = new File("./samples-data/bradley_low_3ds/bradle.3ds").getAbsolutePath();
+
+    String modelURI = Environment.getExternalStorageDirectory() + getString(R.string.bradley_model);
     ModelSceneSymbol tankSymbol = new ModelSceneSymbol(modelURI, 10.0);
     tankSymbol.setHeading(90);
     tankSymbol.setAnchorPosition(SceneSymbol.AnchorPosition.BOTTOM);
     tankSymbol.loadAsync();
-    Graphic tankGraphic = new Graphic(new Point(-4.506390, 48.385624, SpatialReferences.getWgs84()), tankSymbol);
-    tankGraphic.getAttributes().put("HEADING", 0.0);
-    graphicsOverlay.getGraphics().add(tankGraphic);
+    mTankGraphic = new Graphic(new Point(-4.506390, 48.385624, SpatialReferences.getWgs84()), tankSymbol);
+    mTankGraphic.getAttributes().put("HEADING", 0.0);
+    graphicsOverlay.getGraphics().add(mTankGraphic);
 
     // create a viewshed to attach to the tank
-    GeoElementViewshed geoElementViewshed = new GeoElementViewshed(tankGraphic, 90.0, 40.0, 0.1, 250.0, 0.0, 0.0);
+    GeoElementViewshed geoElementViewshed = new GeoElementViewshed(mTankGraphic, 90.0, 40.0, 0.1, 250.0, 0.0, 0.0);
     // offset viewshed observer location to top of tank
     geoElementViewshed.setOffsetZ(3.0);
 
     // create an analysis overlay to add the viewshed to the scene view
     AnalysisOverlay analysisOverlay = new AnalysisOverlay();
     analysisOverlay.getAnalyses().add(geoElementViewshed);
-    sceneView.getAnalysisOverlays().add(analysisOverlay);
+    mSceneView.getAnalysisOverlays().add(analysisOverlay);
+
+    // set the waypoint where the user clicks
+    mSceneView.setOnTouchListener(new DefaultSceneViewOnTouchListener(mSceneView) {
+      @Override public boolean onSingleTapConfirmed(MotionEvent motionEvent) {
+        // get a screen point from the motion event
+        android.graphics.Point screenPoint = new android.graphics.Point(Math.round(motionEvent.getX()),
+            Math.round(motionEvent.getY()));
+
+        // convert the screen point to a scene point
+        mWaypoint = mSceneView.screenToBaseSurface(screenPoint);
+
+        // create a timer to animate the tank
+        mTimer = new Timer();
+        mTimer.scheduleAtFixedRate(new TimerTask() {
+          @Override public void run() {
+            animate();
+          }
+        }, 0, 100);
+
+        return super.onSingleTapConfirmed(motionEvent);
+      }
+    });
+
+    // set camera controller to follow tank
+    OrbitGeoElementCameraController cameraController = new OrbitGeoElementCameraController(mTankGraphic, 200.0);
+    cameraController.setCameraPitchOffset(45.0);
+    mSceneView.setCameraController(cameraController);
   }
+
+  /**
+   * Moves the tank toward the current waypoint a short distance.
+   */
+  private void animate() {
+    if (mWaypoint != null) {
+      // get current location and distance from waypoint
+      Point location = (Point) mTankGraphic.getGeometry();
+      GeodeticDistanceResult distance = GeometryEngine
+          .distanceGeodetic(location, mWaypoint, METERS, DEGREES, GeodeticCurveType.GEODESIC);
+
+      // move toward waypoint a short distance
+      location = GeometryEngine
+          .moveGeodetic(location, 1.0, METERS, distance.getAzimuth1(), DEGREES, GeodeticCurveType.GEODESIC);
+      mTankGraphic.setGeometry(location);
+
+      // rotate toward waypoint
+      double heading = (double) mTankGraphic.getAttributes().get("HEADING");
+      mTankGraphic.getAttributes().put("HEADING", heading + ((distance.getAzimuth1() - heading) / 10));
+
+      // reached waypoint, stop moving and set waypoint to null
+      if (distance.getDistance() <= 5) {
+        mTimer.cancel();
+        mWaypoint = null;
+      }
+    }
+  }
+
+  /**
+   * Request read permission on the device.
+   */
+  private void requestReadPermission() {
+    // define permission to request
+    String[] reqPermission = new String[] { Manifest.permission.READ_EXTERNAL_STORAGE };
+    int requestCode = 2;
+    // For API level 23+ request permission at runtime
+    if (ContextCompat.checkSelfPermission(MainActivity.this,
+        reqPermission[0]) == PackageManager.PERMISSION_GRANTED) {
+      viewshedGeoElement();
+    } else {
+      // request permission
+      ActivityCompat.requestPermissions(MainActivity.this, reqPermission, requestCode);
+    }
+  }
+
+  /**
+   * Handle permission request response.
+   */
+  public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+    if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+      viewshedGeoElement();
+    } else {
+      // report to user that permission was denied
+      Toast.makeText(MainActivity.this, getResources().getString(R.string.read_permission_denied),
+          Toast.LENGTH_SHORT).show();
+    }
+  }
+
+  @Override
+  protected void onPause() {
+    super.onPause();
+    // pause SceneView
+    mSceneView.pause();
+  }
+
+  @Override
+  protected void onResume() {
+    super.onResume();
+    // resume SceneView
+    mSceneView.resume();
+  }
+
 }
