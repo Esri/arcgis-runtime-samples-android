a814bd4da27d595afa569f2f3a1fb49c85db3715
trev8939 <tdraeseke@esri.com>
2018-01-18 17:18:27 +0000

    working sample, except snackbar bug

--- java/map-sketching-api/src/main/java/com/esri/arcgisruntime/sample/mapsketchingapi/MainActivity.java
+++ java/map-sketching-api/src/main/java/com/esri/arcgisruntime/sample/mapsketchingapi/MainActivity.java
@@ -1,11 +1,12 @@
 package com.esri.arcgisruntime.sample.mapsketchingapi;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.ImageButton;
-import android.widget.Toast;
+import android.widget.TextView;
 
 import com.esri.arcgisruntime.geometry.Geometry;
 import com.esri.arcgisruntime.geometry.GeometryType;
@@ -16,8 +17,6 @@ import com.esri.arcgisruntime.mapping.view.GraphicsOverlay;
 import com.esri.arcgisruntime.mapping.view.MapView;
 import com.esri.arcgisruntime.mapping.view.SketchCreationMode;
 import com.esri.arcgisruntime.mapping.view.SketchEditor;
-import com.esri.arcgisruntime.mapping.view.SketchGeometryChangedEvent;
-import com.esri.arcgisruntime.mapping.view.SketchGeometryChangedListener;
 import com.esri.arcgisruntime.symbology.LineSymbol;
 import com.esri.arcgisruntime.symbology.SimpleFillSymbol;
 import com.esri.arcgisruntime.symbology.SimpleLineSymbol;
@@ -25,23 +24,25 @@ import com.esri.arcgisruntime.symbology.SimpleMarkerSymbol;
 import com.esri.arcgisruntime.symbology.Symbol;
 
 public class MainActivity extends AppCompatActivity {
-  private String TAG = "MapViewSketchEditor";
-  private MapView mMapView;
-  private SketchEditor mSketchEditor;
-  //to draw the sketched geometries
-  private GraphicsOverlay mGraphicsOverlay;
-
-  //action buttons
-  private ImageButton mPointButton, mMultiPointButton,  mPolylineButton,
-      mPolygonButton, mFreehandLineButton, mFreehandPolygonButton, mStopButton;
 
   //the symbols to render the sketched geometries on the graphic overlay
   private final Symbol mPointSymbol = new SimpleMarkerSymbol(SimpleMarkerSymbol.Style.SQUARE, 0xFFFF0000, 20);
   private final Symbol mLineSymbol = new SimpleLineSymbol(SimpleLineSymbol.Style.SOLID, 0xFFFF8800, 4);
-  private final Symbol mFillSymbol = new SimpleFillSymbol(SimpleFillSymbol.Style.CROSS, 0x40FFA9A9, (LineSymbol) mLineSymbol);
-
-  private String mSketchGeometryInJson = null;
-  private boolean mExportToJson = false;
+  private final Symbol mFillSymbol = new SimpleFillSymbol(SimpleFillSymbol.Style.CROSS, 0x40FFA9A9,
+      (LineSymbol) mLineSymbol);
+  private String TAG = MainActivity.class.getSimpleName();
+  private MapView mMapView;
+  private SketchEditor mSketchEditor;
+  private GraphicsOverlay mGraphicsOverlay;
+  private ImageButton mPointButton;
+  private ImageButton mMultiPointButton;
+  private ImageButton mPolylineButton;
+  private ImageButton mPolygonButton;
+  private ImageButton mFreehandLineButton;
+  private ImageButton mFreehandPolygonButton;
+  private ImageButton mRedoButton;
+  private ImageButton mUndoButton;
+  private ImageButton mStopButton;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
@@ -51,9 +52,9 @@ public class MainActivity extends AppCompatActivity {
     // inflate MapView from layout
     mMapView = findViewById(R.id.mapView);
     // create a map with the Basemap Type topographic
-    ArcGISMap mMap = new ArcGISMap(Basemap.Type.LIGHT_GRAY_CANVAS, 34.056295, -117.195800, 16);
+    ArcGISMap map = new ArcGISMap(Basemap.Type.LIGHT_GRAY_CANVAS, 34.056295, -117.195800, 16);
     // set the map to be displayed in this view
-    mMapView.setMap(mMap);
+    mMapView.setMap(map);
 
     mGraphicsOverlay = new GraphicsOverlay();
     mMapView.getGraphicsOverlays().add(mGraphicsOverlay);
@@ -62,40 +63,19 @@ public class MainActivity extends AppCompatActivity {
     mSketchEditor = new SketchEditor();
     mMapView.setSketchEditor(mSketchEditor);
 
-    mSketchEditor.addGeometryChangedListener(new SketchGeometryChangedListener() {
-      @Override
-      public void geometryChanged(SketchGeometryChangedEvent event) {
-        Log.i(TAG,"Geometry Updated");
-      }
-    });
+    mSketchEditor.addGeometryChangedListener(sketchGeometryChangedEvent -> Log.i(TAG, "Geometry Updated"));
 
     // Get references to all of the bottom action bar bottoms for highlighting and disabling/enabling
-    mPointButton = (ImageButton)findViewById(R.id.pointButton);
-    mMultiPointButton = (ImageButton)findViewById(R.id.pointsButton);
-    mPolylineButton = (ImageButton)findViewById(R.id.polylineButton);
-    mPolygonButton = (ImageButton)findViewById(R.id.polygonButton);
-    mFreehandLineButton = (ImageButton)findViewById(R.id.freehandPolylineButton);
-    mFreehandPolygonButton = (ImageButton)findViewById(R.id.freehandPolyButton);
-
-    // Disable the undo, redo, and clear button to start with
-    /*
-    mUndoButton = (ImageButton)findViewById(R.id.undoButton);
-    mUndoButton.setClickable(true);
-    mUndoButton.setEnabled(true);
-
-    mRedoButton = (ImageButton)findViewById(R.id.redoButton);
-    mRedoButton.setClickable(true);
-    mRedoButton.setEnabled(true);
-    */
-
-    mStopButton = (ImageButton)findViewById(R.id.clearButton);
-    mStopButton.setClickable(true);
+    mPointButton = findViewById(R.id.pointButton);
+    mMultiPointButton = findViewById(R.id.pointsButton);
+    mPolylineButton = findViewById(R.id.polylineButton);
+    mPolygonButton = findViewById(R.id.polygonButton);
+    mFreehandLineButton = findViewById(R.id.freehandPolylineButton);
+    mFreehandPolygonButton = findViewById(R.id.freehandPolyButton);
   }
 
-
-
   /**
-   * Unselects all start buttons
+   * De-selects all buttons.
    */
   private void resetButtons() {
     mPointButton.setSelected(false);
@@ -112,7 +92,7 @@ public class MainActivity extends AppCompatActivity {
    * @return true if one of start buttons is selected.
    */
   private boolean isSelected(View v) {
-    ImageButton button = (ImageButton)v;
+    ImageButton button = (ImageButton) v;
     return button.isSelected();
   }
 
@@ -124,13 +104,8 @@ public class MainActivity extends AppCompatActivity {
   public void pointClick(View v) {
     if (!isSelected(v)) {
       resetButtons();
-      if (mExportToJson && mSketchGeometryInJson != null) {
-        v.setSelected(true);
-        mSketchEditor.start(Geometry.fromJson(mSketchGeometryInJson), SketchCreationMode.POINT);
-      } else {
-        v.setSelected(true);
-        mSketchEditor.start(SketchCreationMode.POINT);
-      }
+      v.setSelected(true);
+      mSketchEditor.start(SketchCreationMode.POINT);
     }
   }
 
@@ -142,13 +117,8 @@ public class MainActivity extends AppCompatActivity {
   public void pointsClick(View v) {
     if (!isSelected(v)) {
       resetButtons();
-      if (mExportToJson && mSketchGeometryInJson != null) {
-        v.setSelected(true);
-        mSketchEditor.start(Geometry.fromJson(mSketchGeometryInJson), SketchCreationMode.MULTIPOINT);
-      } else {
-        v.setSelected(true);
-        mSketchEditor.start(SketchCreationMode.MULTIPOINT);
-      }
+      v.setSelected(true);
+      mSketchEditor.start(SketchCreationMode.MULTIPOINT);
     }
   }
 
@@ -160,13 +130,8 @@ public class MainActivity extends AppCompatActivity {
   public void polylineClick(View v) {
     if (!isSelected(v)) {
       resetButtons();
-      if (mExportToJson && mSketchGeometryInJson != null) {
-        v.setSelected(true);
-        mSketchEditor.start(Geometry.fromJson(mSketchGeometryInJson), SketchCreationMode.POLYLINE);
-      } else {
-        v.setSelected(true);
-        mSketchEditor.start(SketchCreationMode.POLYLINE);
-      }
+      v.setSelected(true);
+      mSketchEditor.start(SketchCreationMode.POLYLINE);
     }
   }
 
@@ -178,13 +143,8 @@ public class MainActivity extends AppCompatActivity {
   public void polygonClick(View v) {
     if (!isSelected(v)) {
       resetButtons();
-      if (mExportToJson && mSketchGeometryInJson != null) {
-        v.setSelected(true);
-        mSketchEditor.start(Geometry.fromJson(mSketchGeometryInJson), SketchCreationMode.POLYGON);
-      } else {
-        v.setSelected(true);
-        mSketchEditor.start(SketchCreationMode.POLYGON);
-      }
+      v.setSelected(true);
+      mSketchEditor.start(SketchCreationMode.POLYGON);
     }
   }
 
@@ -220,7 +180,9 @@ public class MainActivity extends AppCompatActivity {
    * @param v the button view
    */
   public void undoClick(View v) {
-    //mSketchEditorImpl.undo();
+    if (mSketchEditor.canUndo()) {
+      mSketchEditor.undo();
+    }
   }
 
   /**
@@ -229,7 +191,9 @@ public class MainActivity extends AppCompatActivity {
    * @param v the button view
    */
   public void redoClick(View v) {
-    //mSketchEditorImpl.redo();
+    if (mSketchEditor.canRedo()) {
+      mSketchEditor.redo();
+    }
   }
 
   /**
@@ -239,56 +203,71 @@ public class MainActivity extends AppCompatActivity {
    */
   public void doneClick(View v) {
     if (!mSketchEditor.isSketchValid()) {
+      reportNotValid();
       mSketchEditor.stop();
       resetButtons();
       return;
     }
 
-    Geometry mSketchGeometry = mSketchEditor.getGeometry();
+    // get the geometry from sketch editor
+    Geometry sketchGeometry = mSketchEditor.getGeometry();
     mSketchEditor.stop();
     resetButtons();
 
-    if (mExportToJson) {
-      mSketchGeometryInJson = mSketchGeometry.toJson();
-    }
+    if (sketchGeometry != null) {
 
-    if (mSketchGeometry != null) {
-      Graphic g = new Graphic(mSketchGeometry);
-      if (g.getGeometry() != null) {
-        if (g.getGeometry().getGeometryType() == GeometryType.POLYGON) {
-          g.setSymbol(mFillSymbol);
+      // create a graphic from the sketch editor geometry
+      Graphic graphic = new Graphic(sketchGeometry);
 
-        } else if (g.getGeometry().getGeometryType() == GeometryType.POLYLINE) {
-          g.setSymbol(mLineSymbol);
-
-        } else if (g.getGeometry().getGeometryType() == GeometryType.POINT ||
-            g.getGeometry().getGeometryType() == GeometryType.MULTIPOINT) {
-
-          g.setSymbol(mPointSymbol);
-
-        }
-        mGraphicsOverlay.getGraphics().add(g);
+      // assign a symbol based on geometry type
+      if (graphic.getGeometry().getGeometryType() == GeometryType.POLYGON) {
+        graphic.setSymbol(mFillSymbol);
+      } else if (graphic.getGeometry().getGeometryType() == GeometryType.POLYLINE) {
+        graphic.setSymbol(mLineSymbol);
+      } else if (graphic.getGeometry().getGeometryType() == GeometryType.POINT ||
+          graphic.getGeometry().getGeometryType() == GeometryType.MULTIPOINT) {
+        graphic.setSymbol(mPointSymbol);
       }
+
+      // add the graphic to the graphics overlay
+      mGraphicsOverlay.getGraphics().add(graphic);
     }
   }
 
   /**
-   * Toggles showing the magnifier
+   * Called if sketch is invalid. Reports to user why the sketch was invalid.
    */
-  private void toggleMagnifier() {
-    mMapView.setMagnifierEnabled(!mMapView.isMagnifierEnabled());
-
-    Toast.makeText(this, "Magnifier="+mMapView.isMagnifierEnabled(), Toast.LENGTH_LONG).show();
+  private void reportNotValid() {
+    String validIf;
+    if (mSketchEditor.getSketchCreationMode() == SketchCreationMode.POINT) {
+      validIf = "Point only valid if it contains an x & y coordinate.";
+    } else if (mSketchEditor.getSketchCreationMode() == SketchCreationMode.MULTIPOINT) {
+      validIf = "Multipoint only valid if it contains at least one vertex.";
+    } else if (mSketchEditor.getSketchCreationMode() == SketchCreationMode.POLYLINE
+        || mSketchEditor.getSketchCreationMode() == SketchCreationMode.FREEHAND_LINE) {
+      validIf = "Polyline only valid if it contains at least one part of 2 or more vertices.";
+    } else if (mSketchEditor.getSketchCreationMode() == SketchCreationMode.POLYGON
+        || mSketchEditor.getSketchCreationMode() == SketchCreationMode.FREEHAND_POLYGON) {
+      validIf = "Polygon only valid if it contains at least one part of 3 or more vertices which form a closed ring.";
+    } else {
+      validIf = "No sketch creation mode selected.";
+    }
+    String report = "Sketch geometry invalid:\n" + validIf;
+    Snackbar reportSnackbar = Snackbar.make(findViewById(R.id.coordinator), report, Snackbar.LENGTH_INDEFINITE);
+    TextView snackbarTextView = reportSnackbar.getView().findViewById(android.support.design.R.id.snackbar_text);
+    snackbarTextView.setSingleLine(false);
+    reportSnackbar.show();
+    Log.e(TAG, report);
   }
 
   @Override
-  protected void onPause(){
+  protected void onPause() {
     mMapView.pause();
     super.onPause();
   }
 
   @Override
-  protected void onResume(){
+  protected void onResume() {
     super.onResume();
     mMapView.resume();
   }
