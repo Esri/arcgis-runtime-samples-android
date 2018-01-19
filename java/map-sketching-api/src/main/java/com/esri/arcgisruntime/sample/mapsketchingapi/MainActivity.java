ca2450909ef9fff5c6c0be0ff58545a39494e6ee
trev8939 <tdraeseke@esri.com>
2018-01-19 11:37:15 +0000

    fixing coordinator

--- java/map-sketching-api/src/main/java/com/esri/arcgisruntime/sample/mapsketchingapi/MainActivity.java
+++ java/map-sketching-api/src/main/java/com/esri/arcgisruntime/sample/mapsketchingapi/MainActivity.java
@@ -4,7 +4,6 @@ import android.os.Bundle;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
-import android.view.View;
 import android.widget.ImageButton;
 import android.widget.TextView;
 
@@ -17,39 +16,39 @@ import com.esri.arcgisruntime.mapping.view.GraphicsOverlay;
 import com.esri.arcgisruntime.mapping.view.MapView;
 import com.esri.arcgisruntime.mapping.view.SketchCreationMode;
 import com.esri.arcgisruntime.mapping.view.SketchEditor;
-import com.esri.arcgisruntime.symbology.LineSymbol;
 import com.esri.arcgisruntime.symbology.SimpleFillSymbol;
 import com.esri.arcgisruntime.symbology.SimpleLineSymbol;
 import com.esri.arcgisruntime.symbology.SimpleMarkerSymbol;
-import com.esri.arcgisruntime.symbology.Symbol;
 
 public class MainActivity extends AppCompatActivity {
 
-  //the symbols to render the sketched geometries on the graphic overlay
-  private final Symbol mPointSymbol = new SimpleMarkerSymbol(SimpleMarkerSymbol.Style.SQUARE, 0xFFFF0000, 20);
-  private final Symbol mLineSymbol = new SimpleLineSymbol(SimpleLineSymbol.Style.SOLID, 0xFFFF8800, 4);
-  private final Symbol mFillSymbol = new SimpleFillSymbol(SimpleFillSymbol.Style.CROSS, 0x40FFA9A9,
-      (LineSymbol) mLineSymbol);
-  private String TAG = MainActivity.class.getSimpleName();
+  private final String TAG = MainActivity.class.getSimpleName();
+
+  private SimpleMarkerSymbol mPointSymbol;
+  private SimpleLineSymbol mLineSymbol;
+  private SimpleFillSymbol mFillSymbol;
   private MapView mMapView;
   private SketchEditor mSketchEditor;
   private GraphicsOverlay mGraphicsOverlay;
+
   private ImageButton mPointButton;
   private ImageButton mMultiPointButton;
   private ImageButton mPolylineButton;
   private ImageButton mPolygonButton;
   private ImageButton mFreehandLineButton;
   private ImageButton mFreehandPolygonButton;
-  private ImageButton mRedoButton;
-  private ImageButton mUndoButton;
-  private ImageButton mStopButton;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
 
-    // inflate MapView from layout
+    // define symbols
+    mPointSymbol = new SimpleMarkerSymbol(SimpleMarkerSymbol.Style.SQUARE, 0xFFFF0000, 20);
+    mLineSymbol = new SimpleLineSymbol(SimpleLineSymbol.Style.SOLID, 0xFFFF8800, 4);
+    mFillSymbol = new SimpleFillSymbol(SimpleFillSymbol.Style.CROSS, 0x40FFA9A9, mLineSymbol);
+
+    // inflate map view from layout
     mMapView = findViewById(R.id.mapView);
     // create a map with the Basemap Type topographic
     ArcGISMap map = new ArcGISMap(Basemap.Type.LIGHT_GRAY_CANVAS, 34.056295, -117.195800, 16);
@@ -59,127 +58,98 @@ public class MainActivity extends AppCompatActivity {
     mGraphicsOverlay = new GraphicsOverlay();
     mMapView.getGraphicsOverlays().add(mGraphicsOverlay);
 
-    // Create a new SketchEditor with a new listener
+    // create a new sketch editor and add it to the map view
     mSketchEditor = new SketchEditor();
     mMapView.setSketchEditor(mSketchEditor);
 
-    mSketchEditor.addGeometryChangedListener(sketchGeometryChangedEvent -> Log.i(TAG, "Geometry Updated"));
-
-    // Get references to all of the bottom action bar bottoms for highlighting and disabling/enabling
+    // inflate buttons
     mPointButton = findViewById(R.id.pointButton);
     mMultiPointButton = findViewById(R.id.pointsButton);
     mPolylineButton = findViewById(R.id.polylineButton);
     mPolygonButton = findViewById(R.id.polygonButton);
-    mFreehandLineButton = findViewById(R.id.freehandPolylineButton);
-    mFreehandPolygonButton = findViewById(R.id.freehandPolyButton);
+    mFreehandLineButton = findViewById(R.id.freehandLineButton);
+    mFreehandPolygonButton = findViewById(R.id.freehandPolygonButton);
+    ImageButton undoButton = findViewById(R.id.undoButton);
+    ImageButton redoButton = findViewById(R.id.redoButton);
+    ImageButton stopButton = findViewById(R.id.stopButton);
+
+    // add click listeners
+    mPointButton.setOnClickListener(view -> createModePoint());
+    mMultiPointButton.setOnClickListener(view -> createModeMultipoint());
+    mPolylineButton.setOnClickListener(view -> createModePolyline());
+    mPolygonButton.setOnClickListener(view -> createModePolygon());
+    mFreehandLineButton.setOnClickListener(view -> createModeFreehandLine());
+    mFreehandPolygonButton.setOnClickListener(view -> createModeFreehandPolygon());
+    undoButton.setOnClickListener(view -> undo());
+    redoButton.setOnClickListener(view -> redo());
+    stopButton.setOnClickListener(view -> stop());
   }
 
   /**
-   * De-selects all buttons.
+   * When the point button is clicked, reset other buttons, show the point button as selected, and start point
+   * drawing mode.
    */
-  private void resetButtons() {
-    mPointButton.setSelected(false);
-    mMultiPointButton.setSelected(false);
-    mPolylineButton.setSelected(false);
-    mPolygonButton.setSelected(false);
-    mFreehandLineButton.setSelected(false);
-    mFreehandPolygonButton.setSelected(false);
+  private void createModePoint() {
+    resetButtons();
+    mPointButton.setSelected(true);
+    mSketchEditor.start(SketchCreationMode.POINT);
   }
 
   /**
-   * Tests the start button have been selected.
-   *
-   * @return true if one of start buttons is selected.
+   * When the multipoint button is clicked, reset other buttons, show the multipoint button as selected, and start
+   * point drawing mode.
    */
-  private boolean isSelected(View v) {
-    ImageButton button = (ImageButton) v;
-    return button.isSelected();
+  private void createModeMultipoint() {
+    resetButtons();
+    mMultiPointButton.setSelected(true);
+    mSketchEditor.start(SketchCreationMode.MULTIPOINT);
   }
 
   /**
-   * When the point button is clicked, show it as selected and enable point drawing mode.
-   *
-   * @param v the button view
+   * When the polyline button is clicked, reset other buttons, show the polyline button as selected, and start
+   * polyline drawing mode.
    */
-  public void pointClick(View v) {
-    if (!isSelected(v)) {
-      resetButtons();
-      v.setSelected(true);
-      mSketchEditor.start(SketchCreationMode.POINT);
-    }
+  private void createModePolyline() {
+    resetButtons();
+    mPolylineButton.setSelected(true);
+    mSketchEditor.start(SketchCreationMode.POLYLINE);
+
   }
 
   /**
-   * When the point button is clicked, show it as selected and enable point drawing mode.
-   *
-   * @param v the button view
+   * When the polygon button is clicked, reset other buttons, show the polygon button as selected, and start polygon
+   * drawing mode.
    */
-  public void pointsClick(View v) {
-    if (!isSelected(v)) {
-      resetButtons();
-      v.setSelected(true);
-      mSketchEditor.start(SketchCreationMode.MULTIPOINT);
-    }
+  private void createModePolygon() {
+    resetButtons();
+    mPolygonButton.setSelected(true);
+    mSketchEditor.start(SketchCreationMode.POLYGON);
   }
 
   /**
-   * When the polyline button is clicked, show it as selected and enable polyline drawing mode.
-   *
-   * @param v the button view
+   * When the freehand line button is clicked, reset other buttons, show the freehand line button as selected, and
+   * start freehand line drawing mode.
    */
-  public void polylineClick(View v) {
-    if (!isSelected(v)) {
-      resetButtons();
-      v.setSelected(true);
-      mSketchEditor.start(SketchCreationMode.POLYLINE);
-    }
+  private void createModeFreehandLine() {
+    resetButtons();
+    mFreehandLineButton.setSelected(true);
+    mSketchEditor.start(SketchCreationMode.FREEHAND_LINE);
   }
 
   /**
-   * When the polygon button is clicked, show it as selected and enable polygon drawing mode.
-   *
-   * @param v the button view
+   * When the freehand polygon button is clicked, reset other buttons, show the freehand polygon button as selected,
+   * and enable freehand polygon drawing mode.
    */
-  public void polygonClick(View v) {
-    if (!isSelected(v)) {
-      resetButtons();
-      v.setSelected(true);
-      mSketchEditor.start(SketchCreationMode.POLYGON);
-    }
-  }
-
-  /**
-   * When the polygon button is clicked, show it as selected and enable polygon drawing mode.
-   *
-   * @param v the button view
-   */
-  public void freehandPolygonClick(View v) {
-    if (!isSelected(v)) {
-      resetButtons();
-      v.setSelected(true);
-      mSketchEditor.start(SketchCreationMode.FREEHAND_POLYGON);
-    }
-  }
-
-  /**
-   * When the polygon button is clicked, show it as selected and enable polygon drawing mode.
-   *
-   * @param v the button view
-   */
-  public void freehandPolylineClick(View v) {
-    if (!isSelected(v)) {
-      resetButtons();
-      v.setSelected(true);
-      mSketchEditor.start(SketchCreationMode.FREEHAND_LINE);
-    }
+  private void createModeFreehandPolygon() {
+    resetButtons();
+    mFreehandPolygonButton.setSelected(true);
+    mSketchEditor.start(SketchCreationMode.FREEHAND_POLYGON);
   }
 
   /**
    * When the undo button is clicked, undo the last event on the SketchEditor.
-   *
-   * @param v the button view
    */
-  public void undoClick(View v) {
+  private void undo() {
     if (mSketchEditor.canUndo()) {
       mSketchEditor.undo();
     }
@@ -187,21 +157,18 @@ public class MainActivity extends AppCompatActivity {
 
   /**
    * When the redo button is clicked, redo the last undone event on the SketchEditor.
-   *
-   * @param v the button view
    */
-  public void redoClick(View v) {
+  private void redo() {
     if (mSketchEditor.canRedo()) {
       mSketchEditor.redo();
     }
   }
 
   /**
-   * When the clear button is clicked, clear all graphics on the SketchEditor.
-   *
-   * @param v the button view
+   * When the stop button is clicked, check that sketch if valid. If so, get the geometry from the sketch, set its
+   * symbol and add it to the graphics overlay.
    */
-  public void doneClick(View v) {
+  private void stop() {
     if (!mSketchEditor.isSketchValid()) {
       reportNotValid();
       mSketchEditor.stop();
@@ -258,6 +225,18 @@ public class MainActivity extends AppCompatActivity {
     snackbarTextView.setSingleLine(false);
     reportSnackbar.show();
     Log.e(TAG, report);
+  }
+
+  /**
+   * De-selects all buttons.
+   */
+  private void resetButtons() {
+    mPointButton.setSelected(false);
+    mMultiPointButton.setSelected(false);
+    mPolylineButton.setSelected(false);
+    mPolygonButton.setSelected(false);
+    mFreehandLineButton.setSelected(false);
+    mFreehandPolygonButton.setSelected(false);
   }
 
   @Override
