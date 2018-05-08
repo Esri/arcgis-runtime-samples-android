7bb77439ae75d81595201e8c059242cb88697dd8
eri9600 <erick_solis@esri.com>
2018-05-08 15:28:55 -0700

    Clicking on the screen now creates a green circle around the point with a default radius of 10 miles

--- java/buffer/src/main/java/com/esri/arcgisruntime/sample/buffer/MainActivity.java
+++ java/buffer/src/main/java/com/esri/arcgisruntime/sample/buffer/MainActivity.java
@@ -17,9 +17,17 @@ package com.esri.arcgisruntime.sample.buffer;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MotionEvent;
+import com.esri.arcgisruntime.geometry.*;
 import com.esri.arcgisruntime.mapping.ArcGISMap;
 import com.esri.arcgisruntime.mapping.Basemap;
+import com.esri.arcgisruntime.mapping.view.DefaultMapViewOnTouchListener;
+import com.esri.arcgisruntime.mapping.view.Graphic;
+import com.esri.arcgisruntime.mapping.view.GraphicsOverlay;
 import com.esri.arcgisruntime.mapping.view.MapView;
+import com.esri.arcgisruntime.symbology.SimpleFillSymbol;
+import com.esri.arcgisruntime.symbology.SimpleLineSymbol;
+import com.esri.arcgisruntime.symbology.SimpleMarkerSymbol;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -32,10 +40,50 @@ public class MainActivity extends AppCompatActivity {
     // inflate MapViews
     mMapView = findViewById(R.id.mapView);
     // create a map with the Basemap
-    ArcGISMap map = new ArcGISMap(Basemap.Type.TOPOGRAPHIC, 34.056295, -117.195800, 16);
+    ArcGISMap map = new ArcGISMap(SpatialReferences.getWebMercator());
+    map.setBasemap(Basemap.createTopographic());
     // set the map to be displayed in this view
     mMapView.setMap(map);
 
+    Geometry startingEnvelope = new Envelope(-10863035.97, 3838021.34, -10744801.344, 3887145.299,
+        SpatialReferences.getWebMercator());
+    mMapView.setViewpointGeometryAsync(startingEnvelope);
+
+    // create a graphics overlay to contain the buffered geometry graphics
+    final GraphicsOverlay graphicsOverlay = new GraphicsOverlay();
+    mMapView.getGraphicsOverlays().add(graphicsOverlay);
+
+    // set up units to convert from miles to meters
+    final LinearUnit miles = new LinearUnit(LinearUnitId.MILES);
+    final LinearUnit meters = new LinearUnit(LinearUnitId.METERS);
+
+    //create a semi-transparent green fill symbol for the buffer regions
+    final SimpleFillSymbol fillSymbol = new SimpleFillSymbol(SimpleFillSymbol.Style.SOLID,  0x8800FF00, new
+        SimpleLineSymbol(SimpleLineSymbol.Style.SOLID, 0xFF057e15, 5));
+
+    // create a buffer around the clicked location
+    mMapView.setOnTouchListener(new DefaultMapViewOnTouchListener(this,mMapView){
+
+      @Override
+      public boolean onSingleTapConfirmed(MotionEvent motionEvent){
+
+        // get the point that was clicked and convert it to a point in the map
+        android.graphics.Point clickLocation = new android.graphics.Point(Math.round(motionEvent.getX()),
+            Math.round(motionEvent.getY()));
+        Point mapPoint = mMapView.screenToLocation(clickLocation);
+        Polygon bufferGeometry = GeometryEngine.buffer(mapPoint,miles.convertTo(meters,10));
+        // show the buffered region as a green graphic
+        Graphic bufferGraphic = new Graphic(bufferGeometry,fillSymbol);
+        graphicsOverlay.getGraphics().add(bufferGraphic);
+        //show a red marker where clicked
+        Graphic markerGraphic = new Graphic(mapPoint, new SimpleMarkerSymbol(SimpleMarkerSymbol.Style.CIRCLE,
+            0xFFFF0000, 5));
+        graphicsOverlay.getGraphics().add(markerGraphic);
+        return true;
+      }
+    }
+
+    );
 
 
 
