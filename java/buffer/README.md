01b0f7f9f099feaa07c8bc7fce799cba1698f55b
eri9600 <erick_solis@esri.com>
2018-05-10 14:44:32 -0700

    Fix README to use markdown instead of HTML, format code

--- java/buffer/README.md
+++ java/buffer/README.md
@@ -1,36 +1,30 @@
-<h1>Buffer</h1>
+# Buffer
+This sample demonstrates how to generate a polygon of a fixed distance around a point.
 
-<p>This sample demonstrates how to generate a polygon of a fixed distance around a point.</p>
-
-<p>Creating buffers is a core concept in GIS as it allows for proximity analysis to find geographic features contained 
+Creating buffers is a core concept in GIS as it allows for proximity analysis to find geographic features contained 
 within a polygon. For example, suppose you wanted to know how many restaurants are within a short walking distance of
  your home. The simplest proximity analysis would be to generate a buffer polygon of a certain distance 
- (say 1 mile) around your house.</p>
+ (say 1 mile) around your house.
 
-<p><img src="Buffer.png"></p>
+![Buffer Sample Image](Buffer.png)
 
-<h2>How to use the sample</h2>
-<p>Tap on the map to create a buffer around the tapped location. Change the spinner value (in miles) to set the 
-buffer distance.</p>
+## How to use the sample
+Tap on the map to create a buffer around the tapped location. Change the spinner value (in miles) to set the 
+buffer distance.
 
-<h2>How it works</h2>
-<p>To create a Polygon with a radius:</p>
-<ol>
-<li>The static method, <code>GeometryEngine.buffer</code>, is called by passing in a <code>Point</code> and a distance
- in meters. This returns a <code>Polygon</code> which can be displayed using a <code>Graphic</code>.</li>
-</ol>
+## How it works
+To create a Polygon with a radius:
+The static method, `GeometryEngine.buffer`, is called by passing in a `Point` and a distance
+ in meters. This returns a `Polygon` which can be displayed using a `Graphic`.
 
-<h2>Features</h2>
-<ul>
-<li>GeometryEngine.Buffer</li>
-<li>GraphicsOverlay</li>
-<li>Point</li>
-<li>Polygon</li>
-</ul>
+## Features
+* GeometryEngine.Buffer
+* GraphicsOverlay 
+* Point
+* Polygon
 
-<h2>Tags</h2>
-<ul>
-<li>Analysis</li>
-<li>Buffer</li>
-<li>GeometryEngine</li>
-</ul>
+## Tags
+
+* Analysis
+* Buffer
+* GeometryEngine
--- java/buffer/src/main/java/com/esri/arcgisruntime/sample/buffer/MainActivity.java
+++ java/buffer/src/main/java/com/esri/arcgisruntime/sample/buffer/MainActivity.java
@@ -17,11 +17,16 @@ package com.esri.arcgisruntime.sample.buffer;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.MotionEvent;
 import android.widget.EditText;
-import android.widget.NumberPicker;
-import com.esri.arcgisruntime.geometry.*;
+import com.esri.arcgisruntime.geometry.Envelope;
+import com.esri.arcgisruntime.geometry.Geometry;
+import com.esri.arcgisruntime.geometry.GeometryEngine;
+import com.esri.arcgisruntime.geometry.LinearUnit;
+import com.esri.arcgisruntime.geometry.LinearUnitId;
+import com.esri.arcgisruntime.geometry.Point;
+import com.esri.arcgisruntime.geometry.Polygon;
+import com.esri.arcgisruntime.geometry.SpatialReferences;
 import com.esri.arcgisruntime.mapping.ArcGISMap;
 import com.esri.arcgisruntime.mapping.Basemap;
 import com.esri.arcgisruntime.mapping.view.DefaultMapViewOnTouchListener;
@@ -36,7 +41,7 @@ public class MainActivity extends AppCompatActivity {
 
   private MapView mMapView;
   private EditText mBufferInput;
-  private Double bufferDistance =0.0;
+
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
@@ -52,7 +57,7 @@ public class MainActivity extends AppCompatActivity {
     // set the map to be displayed in this view
     mMapView.setMap(map);
 
-    Geometry startingEnvelope = new Envelope(-10863035.97, 3838021.34, -10744801.344, 3887145.299,
+    final Geometry startingEnvelope = new Envelope(-10863035.97, 3838021.34, -10744801.344, 3887145.299,
         SpatialReferences.getWebMercator());
     mMapView.setViewpointGeometryAsync(startingEnvelope);
 
@@ -64,42 +69,35 @@ public class MainActivity extends AppCompatActivity {
     final LinearUnit miles = new LinearUnit(LinearUnitId.MILES);
     final LinearUnit meters = new LinearUnit(LinearUnitId.METERS);
 
-    //create a semi-transparent green fill symbol for the buffer regions
+    // create a semi-transparent green fill symbol for the buffer regions
     final SimpleFillSymbol fillSymbol = new SimpleFillSymbol(SimpleFillSymbol.Style.SOLID,  0x8800FF00, new
         SimpleLineSymbol(SimpleLineSymbol.Style.SOLID, 0xFF057e15, 5));
 
     // create a buffer around the clicked location
     mMapView.setOnTouchListener(new DefaultMapViewOnTouchListener(this,mMapView){
-
       @Override
       public boolean onSingleTapConfirmed(MotionEvent motionEvent){
-
         // get the point that was clicked and convert it to a point in the map
         android.graphics.Point clickLocation = new android.graphics.Point(Math.round(motionEvent.getX()),
             Math.round(motionEvent.getY()));
         Point mapPoint = mMapView.screenToLocation(clickLocation);
         // only draw a buffer if a value was entered
         if(mBufferInput.getText().toString().length() !=0){
-          bufferDistance = Double.valueOf(mBufferInput.getText().toString());
-          Polygon bufferGeometry = GeometryEngine.buffer(mapPoint,miles.convertTo(meters,bufferDistance));
+          Polygon bufferGeometry = GeometryEngine
+              .buffer(mapPoint,miles.convertTo(meters,Double.valueOf(mBufferInput.getText().toString())));
           // show the buffered region as a green graphic
           Graphic bufferGraphic = new Graphic(bufferGeometry,fillSymbol);
           graphicsOverlay.getGraphics().add(bufferGraphic);
-          //show a red marker where clicked
+          // show a red marker where clicked
           Graphic markerGraphic = new Graphic(mapPoint, new SimpleMarkerSymbol(SimpleMarkerSymbol.Style.CIRCLE,
               0xFFFF0000, 5));
           graphicsOverlay.getGraphics().add(markerGraphic);
         }
-
         return true;
       }
-    }
-
-    );
-
-
-
+    });
   }
+
   @Override
   protected void onPause() {
     super.onPause();
