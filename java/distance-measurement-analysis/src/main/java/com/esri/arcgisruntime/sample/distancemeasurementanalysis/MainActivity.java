a906c7624856679ebcd4ae9e113a9bc221068241
eri9600 <erick_solis@esri.com>
2018-05-21 08:50:22 -0700

    display the distance between two points in metric and imperial units.

--- java/distance-measurement-analysis/src/main/java/com/esri/arcgisruntime/sample/distancemeasurementanalysis/MainActivity.java
+++ java/distance-measurement-analysis/src/main/java/com/esri/arcgisruntime/sample/distancemeasurementanalysis/MainActivity.java
@@ -19,6 +19,17 @@ package com.esri.arcgisruntime.sample.distancemeasurementanalysis;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
+import android.util.Log;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.TextView;
+import com.esri.arcgisruntime.UnitSystem;
+import com.esri.arcgisruntime.concurrent.ListenableFuture;
+import com.esri.arcgisruntime.geoanalysis.LocationDistanceMeasurement;
+import com.esri.arcgisruntime.geometry.Distance;
 import com.esri.arcgisruntime.geometry.Point;
 import com.esri.arcgisruntime.geometry.SpatialReferences;
 import com.esri.arcgisruntime.layers.ArcGISSceneLayer;
@@ -28,13 +39,24 @@ import com.esri.arcgisruntime.mapping.Basemap;
 import com.esri.arcgisruntime.mapping.Surface;
 import com.esri.arcgisruntime.mapping.view.AnalysisOverlay;
 import com.esri.arcgisruntime.mapping.view.Camera;
+import com.esri.arcgisruntime.mapping.view.DefaultMapViewOnTouchListener;
+import com.esri.arcgisruntime.mapping.view.DefaultSceneViewOnTouchListener;
 import com.esri.arcgisruntime.mapping.view.SceneView;
+
+import java.text.DecimalFormat;
+import java.util.ArrayList;
+import java.util.concurrent.ExecutionException;
 
 public class MainActivity extends AppCompatActivity {
 
   private SceneView mSceneView;
+  private TextView mDirectDistance;
+  private TextView mVerticalDistance;
+  private TextView mHorizontalDistance;
+  private Spinner mUnitSpinner;
+  boolean firstClick = true;
 
-  private LocationDistance
+  private LocationDistanceMeasurement distanceMeasurement;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
@@ -42,38 +64,113 @@ public class MainActivity extends AppCompatActivity {
     setContentView(R.layout.activity_main);
 
     // create a scene and add a basemap to it
-    ArcGISScene scene = new ArcGISScene();
+    final ArcGISScene scene = new ArcGISScene();
     scene.setBasemap(Basemap.createImagery());
 
-    // create SceneView from layout
+    // create views from layout
     mSceneView = findViewById(R.id.sceneView);
+    mDirectDistance = findViewById(R.id.total_distance);
+    mHorizontalDistance = findViewById(R.id.horizontal_distance);
+    mVerticalDistance = findViewById(R.id.vertical_distance);
+    mUnitSpinner = findViewById(R.id.units_spinner);
     mSceneView.setScene(scene);
+
 
     // add base surface for elevation data
     Surface surface = new Surface();
     surface.getElevationSources().add(new ArcGISTiledElevationSource("http://elevation3d.arcgis" +
-            ".com/arcgis/rest/services/WorldElevation3D/Terrain3D/ImageServer"));
-    surface.getElevationSources().add(new ArcGISTiledElevationSource("https://tiles.arcgis.com/tiles/d3voDfTFbHOCRwVR/arcgis/rest/services/MNT_IDF/ImageServer"));
+        ".com/arcgis/rest/services/WorldElevation3D/Terrain3D/ImageServer"));
+    surface.getElevationSources().add(new ArcGISTiledElevationSource(
+        "https://tiles.arcgis.com/tiles/d3voDfTFbHOCRwVR/arcgis/rest/services/MNT_IDF/ImageServer"));
     scene.setBaseSurface(surface);
 
-    final String buildings =  "http://tiles.arcgis.com/tiles/P3ePLMYs2RVChkJx/arcgis/rest/services/Buildings_Brest/SceneServer/layers/0";
+    final String buildings = "http://tiles.arcgis.com/tiles/P3ePLMYs2RVChkJx/arcgis/rest/services/Buildings_Brest/SceneServer/layers/0";
     ArcGISSceneLayer sceneLayer = new ArcGISSceneLayer(buildings);
     scene.getOperationalLayers().add(sceneLayer);
 
     // create analysis overlay and add it to scene
     AnalysisOverlay analysisOverlay = new AnalysisOverlay();
-    mSceneView.getAnalysisOverlays().addAll(analysisOverlay);
+    mSceneView.getAnalysisOverlays().add(analysisOverlay);
 
     //initialize a distance measurement and add it to the analyssis overlay
     Point start = new Point(-4.494677, 48.384472, 24.772694, SpatialReferences.getWgs84());
     Point end = new Point(-4.495646, 48.384377, 58.501115, SpatialReferences.getWgs84());
     distanceMeasurement = new LocationDistanceMeasurement(start, end);
     analysisOverlay.getAnalyses().add(distanceMeasurement);
-    // add a camera and initial camera position
-    Camera camera = new Camera(28.4, 83.9, 10010.0, 10.0, 80.0, 0.0);
-    mSceneView.setViewpointCamera(camera);
-  }
 
+    // zoom to initial measurement
+    Camera camera = new Camera(start, 200.0, 0.0, 45.0, 0.0);
+    mSceneView.setViewpointCamera(camera);
+
+    ArrayList<String> unitsList = new ArrayList<>();
+    for(UnitSystem unitSystemItem :UnitSystem.values()){
+      unitsList.add(unitSystemItem.toString());
+    }
+    ArrayAdapter<String> adapter = new ArrayAdapter<>(MainActivity.this,android.R.layout.simple_spinner_item,
+        unitsList);
+    adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+    mUnitSpinner.setAdapter(adapter);
+    mUnitSpinner.setSelection(1);
+
+    mUnitSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+      @Override public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+        switch (i){
+          case 0: distanceMeasurement.setUnitSystem(UnitSystem.IMPERIAL);
+            break;
+          case 1: distanceMeasurement.setUnitSystem(UnitSystem.METRIC);
+            break;
+        }
+      }
+
+      @Override public void onNothingSelected(AdapterView<?> adapterView) {
+
+      }
+    });
+
+    // show the distances in the UI when the measurement changes
+    final DecimalFormat decimalFormat = new DecimalFormat("#.##");
+    distanceMeasurement.addMeasurementChangedListener(new LocationDistanceMeasurement.MeasurementChangedListener() {
+      @Override public void measurementChanged(
+          LocationDistanceMeasurement.MeasurementChangedEvent measurementChangedEvent) {
+        Distance directDistance = distanceMeasurement.getDirectDistance();
+        Distance verticalDistance = distanceMeasurement.getVerticalDistance();
+        Distance horizonalDistance = distanceMeasurement.getHorizontalDistance();
+
+        mDirectDistance.setText(decimalFormat.format(directDistance.getValue()) + " " + directDistance.getUnit().getAbbreviation());
+        mHorizontalDistance.setText(decimalFormat.format(verticalDistance.getValue()) + " " + directDistance.getUnit().getAbbreviation());
+        mVerticalDistance.setText(decimalFormat.format(horizonalDistance.getValue()) + " " + directDistance.getUnit().getAbbreviation());
+        Log.e("distance?",
+            distanceMeasurement.getDirectDistance().getValue() + " " + distanceMeasurement.getHorizontalDistance()
+                .getValue() + " " + distanceMeasurement.getVerticalDistance().getValue() + "");
+      }
+    });
+    // add onTouchListener to get the location of the user tap
+    mSceneView.setOnTouchListener(new DefaultSceneViewOnTouchListener(mSceneView){
+      @Override
+      public boolean onSingleTapConfirmed(MotionEvent event){
+        android.graphics.Point clickLocation = new android.graphics.Point(Math.round(event.getX()),Math.round(event.getY()));
+        final ListenableFuture<Point> pointFuture = mSceneView.screenToLocationAsync(clickLocation);
+        pointFuture.removeDoneListener(new Runnable() {
+          @Override public void run() {
+            try {
+              Point point = pointFuture.get();
+              if(firstClick){
+                distanceMeasurement.setStartLocation(point);
+              }else{
+                distanceMeasurement.setEndLocation(point);
+              }
+              firstClick  = !firstClick;
+              Log.e("firstClick", Boolean.toString(firstClick));
+            } catch (InterruptedException | ExecutionException e) {
+              e.printStackTrace();
+            }
+          }
+        });
+
+        return true;
+      }
+    });
+  }
   @Override
   protected void onPause() {
     super.onPause();
