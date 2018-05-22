64e87519e3677bc9c2cd31ce51c7d7b9ed32bf33
eri9600 <erick_solis@esri.com>
2018-05-22 15:49:25 -0700

    Fix PR comments

--- java/cut-geometry/src/main/java/com/esri/arcgisruntime/sample/cutgeometry/MainActivity.java
+++ java/cut-geometry/src/main/java/com/esri/arcgisruntime/sample/cutgeometry/MainActivity.java
@@ -21,7 +21,6 @@ import java.util.List;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-import android.view.View;
 import android.widget.Button;
 import com.esri.arcgisruntime.geometry.Geometry;
 import com.esri.arcgisruntime.geometry.Polygon;
