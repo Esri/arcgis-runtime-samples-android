8ed6385dad24e0469c53db619d9c26a4f3a8af72
trev8939 <tdraeseke@esri.com>
2018-05-23 17:47:53 +0100

    identify and select working

--- java/scene-layer-identify/src/main/java/com/esri/arcgisruntime/scenelayeridentify/MainActivity.java
+++ java/scene-layer-identify/src/main/java/com/esri/arcgisruntime/scenelayeridentify/MainActivity.java
@@ -1,14 +1,19 @@
 package com.esri.arcgisruntime.scenelayeridentify;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 import java.util.concurrent.ExecutionException;
-
 import android.graphics.Point;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.MotionEvent;
 import android.widget.Toast;
+
 import com.esri.arcgisruntime.concurrent.ListenableFuture;
+import com.esri.arcgisruntime.data.Feature;
+import com.esri.arcgisruntime.data.FeatureQueryResult;
 import com.esri.arcgisruntime.layers.ArcGISSceneLayer;
 import com.esri.arcgisruntime.loadable.LoadStatus;
 import com.esri.arcgisruntime.mapping.ArcGISScene;
@@ -19,6 +24,7 @@ import com.esri.arcgisruntime.mapping.view.IdentifyLayerResult;
 import com.esri.arcgisruntime.mapping.view.SceneView;
 import com.esri.arcgisruntime.portal.Portal;
 import com.esri.arcgisruntime.portal.PortalItem;
+import com.esri.arcgisruntime.security.UserCredential;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -38,10 +44,10 @@ public class MainActivity extends AppCompatActivity {
     mSceneView = findViewById(R.id.sceneView);
     mSceneView.setScene(scene);
 
-    Portal portal = new Portal("http://www.arcgis.com/");
+    //Portal portal = new Portal("http://arcgisruntime.maps.arcgis.com/");
 
-    //Portal portal = new Portal("http://runtimecoretest.maps.arcgis.com");
-    //portal.setCredential(new UserCredential("android_2", "android@100"));
+    Portal portal = new Portal("http://runtimecoretest.maps.arcgis.com");
+    portal.setCredential(new UserCredential("android_2", "android@100"));
 
     portal.loadAsync();
     portal.addDoneLoadingListener(() -> {
@@ -49,8 +55,7 @@ public class MainActivity extends AppCompatActivity {
         Log.e(TAG, "Portal failed to load: " + portal.getLoadError());
         return;
       }
-      PortalItem portalItem = new PortalItem(portal, "2342ab7928834076a1240fb93c60e978");
-      //PortalItem portalItem = new PortalItem(portal, "daeb3918364f452291a17c510630e385");
+      PortalItem portalItem = new PortalItem(portal, "daeb3918364f452291a17c510630e385");
       portalItem.loadAsync();
       portalItem.addDoneLoadingListener(() -> {
         if (portalItem.getLoadStatus() != LoadStatus.LOADED) {
@@ -62,17 +67,11 @@ public class MainActivity extends AppCompatActivity {
         Log.d("portalItem", portalItem.getType().toString());
         Log.d("portalItem", portalItem.getAccess().toString());
 
-        // add a scene service to the scene for viewing buildings
-        //ArcGISSceneLayer sceneLayer = new ArcGISSceneLayer(getResources().getString(R.string.brest_buildings));
-        ArcGISSceneLayer sceneLayer = new ArcGISSceneLayer(getString(R.string.buildings_layer));
+        ArcGISSceneLayer sceneLayer = new ArcGISSceneLayer(portalItem);
         scene.getOperationalLayers().add(sceneLayer);
+        Log.d(TAG, "identify enabled: " + sceneLayer.isIdentifyEnabled());
 
-        // add a camera and initial camera position
-        //Camera camera = new Camera(48.378, -4.494, 200, 345, 65, 0);
-        //mSceneView.setViewpointCamera(camera);
-
-        Camera camera = new Camera(42.36, -71.05, 6.68, 2.02, 88.44, 0.0);
-        //Camera camera = new Camera(42.37, -71.12, 30.53, 14.37, 86.47, 0.0);
+        Camera camera = new Camera(34.05, -117.2, 6.68, 2.02, 88.44, 0.0);
         mSceneView.setViewpointCamera(camera);
 
         mSceneView.setOnTouchListener(new DefaultSceneViewOnTouchListener(mSceneView) {
@@ -91,8 +90,32 @@ public class MainActivity extends AppCompatActivity {
                   int elements = result.getElements().size();
                   int sublayers = result.getSublayerResults().size();
                   int popups = result.getPopups().size();
+                  Feature identifiedFeature = (Feature) result.getElements().get(0);
                   Toast.makeText(MainActivity.this,
-                      "Identify results. Elements: " + elements + " Sublayers: " + sublayers + " Popups: " + popups, Toast.LENGTH_LONG).show();
+                      "Identify results. Elements: " + elements + " Sublayers: " + sublayers + " Popups: " + popups,
+                      Toast.LENGTH_LONG).show();
+                  ListenableFuture<FeatureQueryResult> featureQueryResultFuture = sceneLayer.getSelectedFeaturesAsync();
+                  featureQueryResultFuture.addDoneListener(() -> {
+                    try {
+                      FeatureQueryResult featureQueryResult = featureQueryResultFuture.get();
+                      Iterator<Feature> featureQueryResultIterator = featureQueryResult.iterator();
+                      List<Feature> selectedFeatures = new ArrayList<>();
+                      while(featureQueryResultIterator.hasNext()) {
+                        selectedFeatures.add(featureQueryResultIterator.next());
+                      }
+                      Log.d(TAG, "num selected features: " + selectedFeatures.size());
+                      if (selectedFeatures.contains(identifiedFeature)) {
+                        sceneLayer.unselectFeature(identifiedFeature);
+                        Log.d(TAG, "unselect " + identifiedFeature.getAttributes().get("name"));
+                      } else {
+                        sceneLayer.selectFeature(identifiedFeature);
+                      }
+                    } catch (InterruptedException e) {
+                      e.printStackTrace();
+                    } catch (ExecutionException e) {
+                      e.printStackTrace();
+                    }
+                  });
                 }
               } catch (InterruptedException | ExecutionException e) {
                 e.printStackTrace();
