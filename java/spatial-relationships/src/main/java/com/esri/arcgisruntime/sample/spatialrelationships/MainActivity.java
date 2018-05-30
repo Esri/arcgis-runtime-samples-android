6b3cbf6b47858423b88b5d01b6b77cfa04781745
eri9600 <erick_solis@esri.com>
2018-05-30 10:01:31 -0700

    add graphics to mapview

--- java/spatial-relationships/src/main/java/com/esri/arcgisruntime/sample/spatialrelationships/MainActivity.java
+++ java/spatial-relationships/src/main/java/com/esri/arcgisruntime/sample/spatialrelationships/MainActivity.java
@@ -18,10 +18,20 @@ package com.esri.arcgisruntime.sample.spatialrelationships;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import com.esri.arcgisruntime.geometry.SpatialReference;
+import com.esri.arcgisruntime.geometry.Point;
+import com.esri.arcgisruntime.geometry.PointCollection;
+import com.esri.arcgisruntime.geometry.Polygon;
+import com.esri.arcgisruntime.geometry.Polyline;
+import com.esri.arcgisruntime.geometry.SpatialReferences;
 import com.esri.arcgisruntime.mapping.ArcGISMap;
 import com.esri.arcgisruntime.mapping.Basemap;
+import com.esri.arcgisruntime.mapping.view.DefaultMapViewOnTouchListener;
+import com.esri.arcgisruntime.mapping.view.Graphic;
+import com.esri.arcgisruntime.mapping.view.GraphicsOverlay;
 import com.esri.arcgisruntime.mapping.view.MapView;
+import com.esri.arcgisruntime.symbology.SimpleFillSymbol;
+import com.esri.arcgisruntime.symbology.SimpleLineSymbol;
+import com.esri.arcgisruntime.symbology.SimpleMarkerSymbol;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -34,12 +44,53 @@ public class MainActivity extends AppCompatActivity {
 
     // inflate MapView from layout
     mMapView = findViewById(R.id.mapView);
-    // create a map with a web mercator basemap
-    ArcGISMap map = new ArcGISMap(SpatialReference.create(3857));
-    map.setBasemap(Basemap.createNationalGeographic());
+    // create a map with a topographic  basemap
+    ArcGISMap map = new ArcGISMap(Basemap.createTopographic());
 
     // set the map to be displayed in this view
     mMapView.setMap(map);
+
+    // create a grphics overlay
+    GraphicsOverlay graphicsOverlay = new GraphicsOverlay();
+    mMapView.getGraphicsOverlays().add(graphicsOverlay);
+    graphicsOverlay.setSelectionColor(0xFFFFF00);
+
+    //create a polygon graphic
+    PointCollection polygonPoints = new PointCollection(SpatialReferences.getWebMercator());
+    polygonPoints.add(new Point(-5991501.677830, 5599295.131468));
+    polygonPoints.add(new Point(-6928550.398185, 2087936.739807));
+    polygonPoints.add(new Point(-3149463.800709, 1840803.011362));
+    polygonPoints.add(new Point(-1563689.043184, 3714900.452072));
+    polygonPoints.add(new Point(-3180355.516764, 5619889.608838));
+    Polygon polygon = new Polygon(polygonPoints);
+    SimpleFillSymbol polygonSymbol = new SimpleFillSymbol(SimpleFillSymbol.Style.FORWARD_DIAGONAL, 0xFF00FF00,
+        new SimpleLineSymbol(SimpleLineSymbol.Style.SOLID, 0xFF00FF00, 2));
+    Graphic polygonGraphic = new Graphic(polygon, polygonSymbol);
+    graphicsOverlay.getGraphics().add(polygonGraphic);
+
+    // create a polyline graphic
+    PointCollection polylinePoints = new PointCollection(SpatialReferences.getWebMercator());
+    polylinePoints.add(new Point(-4354240.726880, -609939.795721));
+    polylinePoints.add(new Point(-3427489.245210, 2139422.933233));
+    polylinePoints.add(new Point(-2109442.693501, 4301843.057130));
+    polylinePoints.add(new Point(-1810822.771630, 7205664.366363));
+    Polyline polyline = new Polyline(polylinePoints);
+    Graphic polylineGraphic = new Graphic(polyline, new SimpleLineSymbol(SimpleLineSymbol.Style.DASH, 0xFFFF0000,
+        4));
+    graphicsOverlay.getGraphics().add(polylineGraphic);
+
+    // create a point graphic
+    Point point = new Point(-4487263.495911, 3699176.480377, SpatialReferences.getWebMercator());
+    SimpleMarkerSymbol locationMarker = new SimpleMarkerSymbol(SimpleMarkerSymbol.Style.CIRCLE, 0xFF0000FF, 10);
+    Graphic pointGraphic = new Graphic(point, locationMarker);
+    graphicsOverlay.getGraphics().add(pointGraphic);
+
+    mMapView.setOnTouchListener(new DefaultMapViewOnTouchListener(this,mMapView){
+
+      @Override
+      public boolean
+    });
+
 
   }
 
