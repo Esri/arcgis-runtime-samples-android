f71269fa5c0172082a6849aae05e44b710507429
trev8939 <tdraeseke@esri.com>
2018-11-21 15:23:48 +0000

    set ref scale working

--- java/map-reference-scale/src/main/java/com/esri/arcgisruntime/mapreferencescale/MainActivity.java
+++ java/map-reference-scale/src/main/java/com/esri/arcgisruntime/mapreferencescale/MainActivity.java
@@ -1,3 +1,19 @@
+/* Copyright 2018 Esri
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
 package com.esri.arcgisruntime.mapreferencescale;
 
 import android.Manifest;
@@ -44,6 +60,7 @@ public class MainActivity extends AppCompatActivity {
     mReferenceScaleSeekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
       @Override public void onProgressChanged(SeekBar seekBar, int progress, boolean b) {
         currReferenceScaleTextView.setText(String.valueOf(progress));
+        mMapView.getMap().setReferenceScale(seekBar.getProgress());
       }
 
       @Override public void onStartTrackingTouch(SeekBar seekBar) {
@@ -51,9 +68,7 @@ public class MainActivity extends AppCompatActivity {
       }
 
       @Override public void onStopTrackingTouch(SeekBar seekBar) {
-        mMapView.getMap().setReferenceScale(seekBar.getProgress());
-        Toast.makeText(MainActivity.this, "Map's reference scale set to " + mMapView.getMap().getReferenceScale(),
-            Toast.LENGTH_SHORT).show();
+
       }
     });
 
