f211a7b270d3ee0ea45dbada0ea45e3596ffec25
trev8939 <tdraeseke@esri.com>
2018-12-14 16:19:12 +0000

    add copyright

--- java/line-of-sight-geoelement/src/main/java/com/esri/arcgisruntime/sample/lineofsightgeoelement/MainActivity.java
+++ java/line-of-sight-geoelement/src/main/java/com/esri/arcgisruntime/sample/lineofsightgeoelement/MainActivity.java
@@ -1,3 +1,19 @@
+/* Copyright 2018 Esri
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
 package com.esri.arcgisruntime.sample.lineofsightgeoelement;
 
 import java.io.File;
@@ -73,7 +89,7 @@ public class MainActivity extends AppCompatActivity {
     ArcGISScene scene = new ArcGISScene();
     scene.setBasemap(Basemap.createTopographic());
 
-    // add the SceneView to the stack pane
+    // get a reference to the scene view and set the scene to it
     mSceneView = findViewById(R.id.sceneView);
     mSceneView.setScene(scene);
 
