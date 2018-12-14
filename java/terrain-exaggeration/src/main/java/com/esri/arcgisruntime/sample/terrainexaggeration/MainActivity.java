d50b6a3e437701df3c76887639a1b8543f4b04a3
AVWORLD\hard7971 <hshah@esri.com>
2018-12-14 11:30:35 -0800

    adding new sample terrain-exaggeration

---
+++ java/terrain-exaggeration/src/main/java/com/esri/arcgisruntime/sample/terrainexaggeration/MainActivity.java
@@ -0,0 +1,100 @@
+/* Copyright 2018 Esri
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package com.esri.arcgisruntime.sample.terrainexaggeration;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.SeekBar;
+import android.widget.TextView;
+
+import com.esri.arcgisruntime.geometry.Point;
+import com.esri.arcgisruntime.mapping.ArcGISScene;
+import com.esri.arcgisruntime.mapping.ArcGISTiledElevationSource;
+import com.esri.arcgisruntime.mapping.Basemap;
+import com.esri.arcgisruntime.mapping.Surface;
+import com.esri.arcgisruntime.mapping.view.Camera;
+import com.esri.arcgisruntime.mapping.view.SceneView;
+
+public class MainActivity extends AppCompatActivity {
+
+  private SceneView mSceneView;
+  private TextView mExaggerationValueText;
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main);
+
+    // create a scene and add a basemap to it
+    ArcGISScene scene = new ArcGISScene();
+    scene.setBasemap(Basemap.createNationalGeographic());
+    // create SceneView from layout
+    mSceneView = findViewById(R.id.sceneView);
+    mSceneView.setScene(scene);
+
+    // add base surface for elevation data
+    final Surface surface = new Surface();
+    ArcGISTiledElevationSource elevationSource = new ArcGISTiledElevationSource(
+        getString(R.string.elevation_image_service));
+    surface.getElevationSources().add(elevationSource);
+    scene.setBaseSurface(surface);
+
+    // add a camera and initial camera position
+    Point initialLocation = new Point(-119.94891542688772, 46.75792111605992, 3183, mSceneView.getSpatialReference());
+    Camera camera = new Camera(initialLocation, 0, 7, 70, 0);
+    mSceneView.setViewpointCamera(camera);
+
+    // create TextView to show SeekBar value
+    mExaggerationValueText = findViewById(R.id.exaggerationSeekBarValueText);
+    // create SeekBar
+    final SeekBar mExaggerationSeekBar = findViewById(R.id.exaggerationSeekBar);
+    mExaggerationSeekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+      @Override public void onProgressChanged(SeekBar seekBar, int progress, boolean b) {
+        // set the text to SeekBar value
+        mExaggerationValueText.setText(String.valueOf((float) progress));
+        // set exaggeration of surface to the value the user selected
+        surface.setElevationExaggeration((float) progress);
+      }
+
+      @Override public void onStartTrackingTouch(SeekBar seekBar) {
+
+      }
+
+      @Override public void onStopTrackingTouch(SeekBar seekBar) {
+
+      }
+    });
+  }
+
+  @Override
+  protected void onPause() {
+    mSceneView.pause();
+    super.onPause();
+  }
+
+  @Override
+  protected void onResume() {
+    super.onResume();
+    mSceneView.resume();
+  }
+
+  @Override
+  protected void onDestroy() {
+    mSceneView.dispose();
+    super.onDestroy();
+  }
+}
