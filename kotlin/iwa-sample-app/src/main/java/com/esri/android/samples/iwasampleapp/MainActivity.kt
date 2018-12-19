7be811a5f7439c306e63ab7c100915150d83ec24
xuem6538 <xwu@esri>
2018-12-18 17:10:42 -0800

    removed const

--- kotlin/iwa-sample-app/src/main/java/com/esri/android/samples/iwasampleapp/MainActivity.kt
+++ kotlin/iwa-sample-app/src/main/java/com/esri/android/samples/iwasampleapp/MainActivity.kt
@@ -23,16 +23,16 @@ import java.util.concurrent.CountDownLatch
  * 1. Enter url of the portal you want to connect in the edit text box. The url of a default test portal has
  * been set to the box.
  * 2. Tap on the "Sign In" button to connect to the portal as a named user. A dialog will be popped up for
- * username/password. When a correct credential is passed, portal will be loaded. If you keep passing in
- * wrong credential, no more prompt for credential after 5 attempts, which is actually 4 times due to a known
- * issue in our SDK. Then portal will fail to load. You can click the "Cancel" button to cancel the sign-in
- * process. Portal will be failed to load with different error message.
+ * username/password. When a correct credential is passed, portal will be loaded. If you keep passing in a
+ * wrong credential, no more prompt for a credential will occur after the default number of attempts. 
+ * Then portal will fail to load. You can click the "Cancel" button to cancel the sign-in process.
+ * Portal will be failed to load with different error message.
  */
 class MainActivity : AppCompatActivity() {
 
     private lateinit var map: ArcGISMap
 
-    private lateinit var portal : Portal
+    private lateinit var portal: Portal
     
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -81,8 +81,6 @@ class MainActivity : AppCompatActivity() {
     }
 }
 
-const val MAX_ATTEMPTS = 5
-
 /**
  * A custom authentication challenge handler to handle user credential challenges.
  * 
@@ -99,9 +97,11 @@ const val MAX_ATTEMPTS = 5
  */
 class IWACustomChallengeHandler(val activity: Activity) : AuthenticationChallengeHandler {
 
+    val maxAttempts = 5
+    
     override fun handleChallenge(challenge: AuthenticationChallenge): AuthenticationChallengeResponse {
         if (challenge.type == AuthenticationChallenge.Type.USER_CREDENTIAL_CHALLENGE) {
-            if (challenge.failureCount > MAX_ATTEMPTS) {
+            if (challenge.failureCount > maxAttempts) {
                 // Exceeded maximum amount of attempts. Act like it was a cancel
                 Toast.makeText(activity, "Exceeded maximum amount of attempts. Please try again!", Toast.LENGTH_SHORT).show()
                 return AuthenticationChallengeResponse(AuthenticationChallengeResponse.Action.CANCEL, challenge)
