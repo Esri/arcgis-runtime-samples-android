5a270ffc74a27459717d6f7093fa11f7eadbad92
Puneet Prakash <puneet_prakash@esri.com>
2019-01-30 03:18:41 -0800

    added iwasampleapp (#554)
    * added iwasampleapp
    
    * updated doc
    
    * addressed code review feedback
    
    * addressed code review feedback
    
    * removed logic to call cancelLoad() on remoteResource
    
    * updated toast message
    
    * removed const
    
    * added copyright, error handling, life cycle; reformatted comments
    
    * working on recycler view
    
    * add Java sample
    
    * linting
    
    * PR fixes
    
    * further PR fixes
    
    * typo fix in java sample and remove kotlin project

---
+++ java/integrated-windows-authentication/src/main/java/com/example/arcgisruntime/sample/integratedwindowsauthentication/PortalItemAdapter.java
@@ -0,0 +1,59 @@
+package com.example.arcgisruntime.sample.integratedwindowsauthentication;
+
+import java.util.List;
+
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.esri.arcgisruntime.portal.PortalItem;
+
+public class PortalItemAdapter extends RecyclerView.Adapter<PortalItemAdapter.PortalItemViewHolder> {
+
+  public interface OnItemClickListener {
+    void onItemClick(PortalItem portalItem);
+  }
+
+  private final List<PortalItem> mPortalItemList;
+  private final OnItemClickListener mOnItemClickListener;
+
+  static class PortalItemViewHolder extends RecyclerView.ViewHolder {
+
+    final TextView mPortalItemTextView;
+
+    PortalItemViewHolder(View itemView) {
+      super(itemView);
+      mPortalItemTextView = itemView.findViewById(R.id.itemTextView);
+    }
+
+    void bind(final PortalItem portalItem, final OnItemClickListener listener) {
+      mPortalItemTextView.setText(portalItem.getTitle());
+      // return the portal item object's itemID, rather than its title
+      itemView.setOnClickListener(v -> listener.onItemClick(portalItem));
+    }
+  }
+
+  public PortalItemAdapter(List<PortalItem> portalItemNames, OnItemClickListener onItemClickListener) {
+    mPortalItemList = portalItemNames;
+    mOnItemClickListener = onItemClickListener;
+  }
+
+  @NonNull @Override
+  public PortalItemViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+    View portalItemTextView = LayoutInflater.from(parent.getContext()).inflate(R.layout.portal_item_row, parent, false);
+    return new PortalItemViewHolder(portalItemTextView);
+  }
+
+  @Override
+  public void onBindViewHolder(@NonNull PortalItemViewHolder holder, int position) {
+    holder.bind(mPortalItemList.get(position), mOnItemClickListener);
+  }
+
+  @Override
+  public int getItemCount() {
+    return mPortalItemList.size();
+  }
+}
