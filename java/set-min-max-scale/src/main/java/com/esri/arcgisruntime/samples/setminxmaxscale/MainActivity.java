54e01a616dd93cce7990b50937d2bd4d16cae1b0
Luke William Duncan <lduncan@esri.com>
2019-02-13 15:01:47 +0000

    Setting min and max scale and applying an initial `ViewPoint` to the `ArcGISMap` instance.

--- java/set-min-max-scale/src/main/java/com/esri/arcgisruntime/samples/setminxmaxscale/MainActivity.java
+++ java/set-min-max-scale/src/main/java/com/esri/arcgisruntime/samples/setminxmaxscale/MainActivity.java
@@ -19,8 +19,11 @@ package com.esri.arcgisruntime.samples.setminxmaxscale;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import com.esri.arcgisruntime.geometry.Point;
+import com.esri.arcgisruntime.geometry.SpatialReferences;
 import com.esri.arcgisruntime.mapping.ArcGISMap;
 import com.esri.arcgisruntime.mapping.Basemap;
+import com.esri.arcgisruntime.mapping.Viewpoint;
 import com.esri.arcgisruntime.mapping.view.MapView;
 
 public class MainActivity extends AppCompatActivity {
@@ -36,6 +39,13 @@ public class MainActivity extends AppCompatActivity {
 
     // create a ArcGISMap with basemap streets
     ArcGISMap map = new ArcGISMap(Basemap.createStreets());
+
+    // set the scale at which this layer can be viewed
+    map.setMinScale(8000);
+    map.setMaxScale(2000);
+
+    // a point where the map view will focus and zoom to
+    map.setInitialViewpoint(new Viewpoint(new Point(-355453, 7548720, SpatialReferences.getWebMercator()), 3000));
 
     // create a view for this ArcGISMap and set ArcGISMap to it
     mMapView.setMap(map);
