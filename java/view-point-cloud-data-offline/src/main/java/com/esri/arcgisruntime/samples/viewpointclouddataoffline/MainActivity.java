b9757c85276803657d90f827a0701b29171b54e3
Luke William Duncan <lduncan@esri.com>
2019-02-14 14:30:12 +0000

    Loading the `PointCloudLayer` by using the `.slpk` file located on the device. Handling permissions and notifying user of errors.

--- java/view-point-cloud-data-offline/src/main/java/com/esri/arcgisruntime/samples/viewpointclouddataoffline/MainActivity.java
+++ java/view-point-cloud-data-offline/src/main/java/com/esri/arcgisruntime/samples/viewpointclouddataoffline/MainActivity.java
@@ -16,20 +16,97 @@
 
 package com.esri.arcgisruntime.samples.viewpointclouddataoffline;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.os.Environment;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
+import android.widget.Toast;
+import com.esri.arcgisruntime.layers.PointCloudLayer;
+import com.esri.arcgisruntime.loadable.LoadStatus;
+import com.esri.arcgisruntime.mapping.ArcGISScene;
+import com.esri.arcgisruntime.mapping.ArcGISTiledElevationSource;
+import com.esri.arcgisruntime.mapping.Basemap;
+import com.esri.arcgisruntime.mapping.Surface;
+import com.esri.arcgisruntime.mapping.view.Camera;
+import com.esri.arcgisruntime.mapping.view.SceneView;
 
 public class MainActivity extends AppCompatActivity {
 
-  private View mSceneView;
+  private static final int PERMISSIONS_REQUEST_CODE = 1;
+
+  private static final String[] PERMISSIONS = { Manifest.permission.READ_EXTERNAL_STORAGE };
+
+  private SceneView mSceneView;
+
+  private ArcGISScene mScene;
 
   @Override protected void onCreate(@Nullable Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
-
     setContentView(R.layout.activity_main);
 
     mSceneView = findViewById(R.id.sceneView);
+
+    // create a scene and add it to the scene view
+    mScene = new ArcGISScene(Basemap.createImagery());
+    mSceneView.setScene(mScene);
+
+    // create a camera and initial camera position
+    Camera camera = new Camera(32.7321157, -117.150072, 452.282774, 25.481533, 78.0945859, 0.0);
+
+    // set Viewpoint for SceneView using camera
+    mSceneView.setViewpointCamera(camera);
+
+    // set the base surface with world elevation
+    Surface surface = new Surface();
+    surface.getElevationSources().add(new ArcGISTiledElevationSource(getString(R.string.elevation_source_url)));
+    mScene.setBaseSurface(surface);
+
+    // For API level 23+ request permission at runtime
+    if (ContextCompat.checkSelfPermission(this, PERMISSIONS[0]) == PackageManager.PERMISSION_GRANTED) {
+      createPointCloudLayer();
+    } else {
+      // request permission
+      ActivityCompat.requestPermissions(this, PERMISSIONS, PERMISSIONS_REQUEST_CODE);
+    }
+  }
+
+  /**
+   * Handle the permissions request response
+   */
+  public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+    if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+      createPointCloudLayer();
+    } else {
+      // report to user that permission was denied
+      Toast.makeText(this, getResources().getString(R.string.read_permission_denied_message),
+          Toast.LENGTH_SHORT).show();
+    }
+  }
+
+  private void createPointCloudLayer() {
+    // Add a point cloud layer to the scene by passing the URI of the Scene Layer Package to the constructor
+    PointCloudLayer pointCloudLayer = new PointCloudLayer(
+        Environment.getExternalStorageDirectory() + getString(R.string.scene_layer_package_location));
+
+    // Add a listener to perform operations when the load status of the PointCloudLayer changes
+    pointCloudLayer.addLoadStatusChangedListener(loadStatusChangedEvent -> {
+
+      // When PointCloudLayer loads
+      if (loadStatusChangedEvent.getNewLoadStatus() == LoadStatus.LOADED) {
+        // Add the PointCloudLayer to the Operational Layers of the Scene
+        mScene.getOperationalLayers().add(pointCloudLayer);
+      } else if (loadStatusChangedEvent.getNewLoadStatus() == LoadStatus.FAILED_TO_LOAD) {
+        // Notify user that the PointCloudLayer has failed to load
+        Toast.makeText(this, R.string.point_cloud_layer_load_failure_message, Toast.LENGTH_LONG).show();
+      }
+    });
+
+    // Load the PointCloudLayer
+    pointCloudLayer.loadAsync();
   }
 }
