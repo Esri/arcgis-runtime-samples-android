ed609dd7f480d5729056a76df9ed5312bb362c31
Luke William Duncan <lduncan@esri.com>
2019-02-26 09:06:02 +0000

    Setting up `MapView`

--- kotlin/add-features-feature-service/src/main/java/com/esri/arcgisruntime/sample/addfeaturesfeatureservice/MainActivity.kt
+++ kotlin/add-features-feature-service/src/main/java/com/esri/arcgisruntime/sample/addfeaturesfeatureservice/MainActivity.kt
@@ -17,8 +17,51 @@
 
 package com.esri.arcgisruntime.sample.addfeaturesfeatureservice
 
+import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import com.esri.arcgisruntime.data.ServiceFeatureTable
+import com.esri.arcgisruntime.layers.FeatureLayer
+import com.esri.arcgisruntime.mapping.ArcGISMap
+import com.esri.arcgisruntime.mapping.Basemap
+import kotlinx.android.synthetic.main.activity_main.*
+
 
 class MainActivity : AppCompatActivity() {
-  // TODO
+
+  private lateinit var featureTable: ServiceFeatureTable
+
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContentView(R.layout.activity_main)
+
+    // create a map with streets basemap
+    with(ArcGISMap(Basemap.Type.STREETS, 40.0, -95.0, 4)) {
+      // create service feature table from URL
+      featureTable = ServiceFeatureTable(getString(R.string.service_layer_url))
+
+      // create a feature layer from table
+      val featureLayer = FeatureLayer(featureTable)
+
+      // add the layer to the ArcGISMap
+      this.operationalLayers.add(featureLayer)
+
+      // set ArcGISMap to be displayed in map view
+      mapView.map = this
+    }
+  }
+
+  override fun onResume() {
+    super.onResume()
+    mapView.resume()
+  }
+
+  override fun onPause() {
+    mapView.pause()
+    super.onPause()
+  }
+
+  override fun onDestroy() {
+    mapView.dispose()
+    super.onDestroy()
+  }
 }
