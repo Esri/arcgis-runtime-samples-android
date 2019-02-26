f0957a2ee9824866d061775bc70f7263ff08cd73
Luke William Duncan <lduncan@esri.com>
2019-02-26 09:50:51 +0000

    Adding features to `ServiceFeatureTable` and submitting to server.

--- kotlin/add-features-feature-service/src/main/java/com/esri/arcgisruntime/sample/addfeaturesfeatureservice/MainActivity.kt
+++ kotlin/add-features-feature-service/src/main/java/com/esri/arcgisruntime/sample/addfeaturesfeatureservice/MainActivity.kt
@@ -19,16 +19,26 @@ package com.esri.arcgisruntime.sample.addfeaturesfeatureservice
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.util.Log
+import android.view.MotionEvent
+import android.widget.Toast
 import com.esri.arcgisruntime.data.ServiceFeatureTable
+import com.esri.arcgisruntime.geometry.GeometryEngine
+import com.esri.arcgisruntime.geometry.Point
 import com.esri.arcgisruntime.layers.FeatureLayer
 import com.esri.arcgisruntime.mapping.ArcGISMap
 import com.esri.arcgisruntime.mapping.Basemap
+import com.esri.arcgisruntime.mapping.view.DefaultMapViewOnTouchListener
 import kotlinx.android.synthetic.main.activity_main.*
 
 
 class MainActivity : AppCompatActivity() {
 
   private lateinit var featureTable: ServiceFeatureTable
+
+  companion object {
+    private val TAG = MainActivity::class.java.simpleName
+  }
 
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
@@ -48,6 +58,80 @@ class MainActivity : AppCompatActivity() {
       // set ArcGISMap to be displayed in map view
       mapView.map = this
     }
+
+    mapView.onTouchListener = object : DefaultMapViewOnTouchListener(this, mapView) {
+      override fun onSingleTapConfirmed(motionEvent: MotionEvent?): Boolean {
+        motionEvent?.let { event ->
+          // create a point from where the user clicked
+          val point = android.graphics.Point(event.x.toInt(), event.y.toInt())
+
+          // create a map point from a point
+          val mapPoint = mapView.screenToLocation(point)
+
+          // for a wrapped around map, the point coordinates include the wrapped around value
+          // for a service in projected coordinate system, this wrapped around value has to be normalized
+          val normalizedMapPoint = GeometryEngine.normalizeCentralMeridian(mapPoint) as Point
+
+          // add a new feature to the service feature table
+          addFeature(normalizedMapPoint, featureTable)
+        }
+        return super.onSingleTapConfirmed(motionEvent)
+      }
+    }
+  }
+
+  /**
+   * Adds a new Feature to a ServiceFeatureTable and applies the changes to the
+   * server.
+   *
+   * @param mapPoint location to add feature
+   * @param featureTable service feature table to add feature
+   */
+  private fun addFeature(mapPoint: Point, featureTable: ServiceFeatureTable) {
+
+    // create default attributes for the feature
+    val attributes = HashMap<String, Any>()
+    attributes["typdamage"] = "Destroyed"
+    attributes["primcause"] = "Earthquake"
+
+    // creates a new feature using default attributes and point
+    val feature = featureTable.createFeature(attributes, mapPoint)
+
+    // check if feature can be added to feature table
+    if (featureTable.canAdd()) {
+      // add the new feature to the feature table and to server
+      featureTable.addFeatureAsync(feature).addDoneListener { applyEdits(featureTable) }
+    } else {
+      logToUser(getString(R.string.error_cannot_add_to_feature_table))
+    }
+  }
+
+  /**
+   * Sends any edits on the ServiceFeatureTable to the server.
+   *
+   * @param featureTable service feature table
+   */
+  private fun applyEdits(featureTable: ServiceFeatureTable) {
+
+    // apply the changes to the server
+    val editResult = featureTable.applyEditsAsync()
+    editResult.addDoneListener {
+      try {
+        val edits = editResult.get()
+        // check if the server edit was successful
+        if (edits != null && edits.size > 0) {
+          if (!edits[0].hasCompletedWithErrors()) {
+            logToUser(getString(R.string.feature_added))
+          } else {
+            throw edits[0].error
+          }
+        }
+      } catch (e: InterruptedException) {
+        logToUser(getString(R.string.error_applying_edits, e.cause?.message))
+      } catch (e: Exception) {
+
+      }
+    }
   }
 
   override fun onResume() {
@@ -64,4 +148,12 @@ class MainActivity : AppCompatActivity() {
     mapView.dispose()
     super.onDestroy()
   }
+}
+
+/*
+* AppCompatActivity Extensions
+*/
+fun AppCompatActivity.logToUser(message: String) {
+  Toast.makeText(this, message, Toast.LENGTH_LONG).show()
+  Log.d(this::class.java.simpleName, message)
 }
