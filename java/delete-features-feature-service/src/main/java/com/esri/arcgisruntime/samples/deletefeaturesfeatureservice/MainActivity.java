1f61d74e6f8f212708ba1700c4024133f53ac0e6
Luke William Duncan <lduncan@esri.com>
2019-02-28 16:50:10 +0000

    Using Callout to allow user to delete feature. WIP.

--- java/delete-features-feature-service/src/main/java/com/esri/arcgisruntime/samples/deletefeaturesfeatureservice/MainActivity.java
+++ java/delete-features-feature-service/src/main/java/com/esri/arcgisruntime/samples/deletefeaturesfeatureservice/MainActivity.java
@@ -16,17 +16,38 @@
 
 package com.esri.arcgisruntime.samples.deletefeaturesfeatureservice;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.ExecutionException;
+
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.Toast;
 
+import com.esri.arcgisruntime.concurrent.ListenableFuture;
+import com.esri.arcgisruntime.data.Feature;
+import com.esri.arcgisruntime.data.FeatureQueryResult;
 import com.esri.arcgisruntime.data.ServiceFeatureTable;
+import com.esri.arcgisruntime.geometry.GeometryEngine;
+import com.esri.arcgisruntime.geometry.Point;
 import com.esri.arcgisruntime.layers.FeatureLayer;
 import com.esri.arcgisruntime.mapping.ArcGISMap;
 import com.esri.arcgisruntime.mapping.Basemap;
+import com.esri.arcgisruntime.mapping.GeoElement;
+import com.esri.arcgisruntime.mapping.view.Callout;
+import com.esri.arcgisruntime.mapping.view.DefaultMapViewOnTouchListener;
+import com.esri.arcgisruntime.mapping.view.IdentifyLayerResult;
 import com.esri.arcgisruntime.mapping.view.MapView;
 
 public class MainActivity extends AppCompatActivity {
+
+  private static final String TAG = MainActivity.class.getSimpleName();
 
   private MapView mMapView;
 
@@ -34,6 +55,7 @@ public class MainActivity extends AppCompatActivity {
 
   private FeatureLayer mFeatureLayer;
 
+  @SuppressLint("ClickableViewAccessibility")
   @Override protected void onCreate(@Nullable Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
@@ -52,7 +74,104 @@ public class MainActivity extends AppCompatActivity {
     // add the layer to the ArcGISMap
     map.getOperationalLayers().add(mFeatureLayer);
 
+    mMapView.setOnTouchListener(new DefaultMapViewOnTouchListener(this, mMapView) {
+      @Override public boolean onSingleTapConfirmed(MotionEvent event) {
+        // create a point from where the user clicked
+        android.graphics.Point point = new android.graphics.Point((int) event.getX(), (int) event.getY());
+
+        // create a map point from a point
+        Point mapPoint = mMapView.screenToLocation(point);
+
+        // for a wrapped around map, the point coordinates include the wrapped around value
+        // for a service in projected coordinate system, this wrapped around value has to be normalized
+        Point normalizedMapPoint = (Point) GeometryEngine.normalizeCentralMeridian(mapPoint);
+
+        // identify the clicked feature
+        ListenableFuture<IdentifyLayerResult> results = mMapView.identifyLayerAsync(mFeatureLayer, point, 1, false);
+        results.addDoneListener(() -> {
+          try {
+            IdentifyLayerResult layer = results.get();
+            // search the layers for identified features
+            List<Feature> foundFeatures = new ArrayList<>();
+
+            for (GeoElement element : layer.getElements()) {
+              if (element instanceof Feature) {
+                foundFeatures.add((Feature) element);
+              }
+            }
+
+            if (foundFeatures.size() > 0) {
+              inflateCallout(mMapView, foundFeatures.get(0), normalizedMapPoint).show();
+              mFeatureLayer.selectFeature(foundFeatures.get(0));
+            }
+          } catch (InterruptedException | ExecutionException e) {
+            logToUser(getString(R.string.error_getting_identify_result, e.getCause().getMessage()));
+          }
+        });
+        return super.onSingleTapConfirmed(event);
+      }
+    });
+
     // set ArcGISMap to be displayed in map view
     mMapView.setMap(map);
+  }
+
+  private Callout inflateCallout(MapView mapView, GeoElement geoElement, Point point) {
+    Callout callout = mapView.getCallout();
+    View view = LayoutInflater.from(this).inflate(R.layout.view_callout, null);
+    view.findViewById(R.id.calloutViewCallToAction).setOnClickListener(v -> {
+      // get selected features
+      ListenableFuture<FeatureQueryResult> selectionResult = mFeatureLayer.getSelectedFeaturesAsync();
+      selectionResult.addDoneListener(() -> {
+        try {
+          FeatureQueryResult selected = selectionResult.get();
+          // delete selected features
+          deleteFeatures(selected, mFeatureTable);
+        } catch (InterruptedException | ExecutionException e) {
+          logToUser(e.getCause().getMessage());
+        }
+      });
+    });
+    callout.setContent(view);
+    callout.setGeoElement(geoElement, point);
+    return callout;
+  }
+
+  /**
+   * Deletes features from a ServiceFeatureTable and applies the changes to the
+   * server.
+   */
+  private void deleteFeatures(FeatureQueryResult features, ServiceFeatureTable featureTable) {
+    // delete feature from the feature table and apply edit to server
+    featureTable.deleteFeaturesAsync(features).addDoneListener(() -> applyEdits(featureTable));
+  }
+
+  private void applyEdits(ServiceFeatureTable featureTable) {
+    //TODO
+  }
+
+  /**
+   * Shows a Toast to user and logs to logcat.
+   *
+   * @param message message to display
+   */
+  private void logToUser(String message) {
+    Toast.makeText(this, message, Toast.LENGTH_LONG).show();
+    Log.d(TAG, message);
+  }
+
+  @Override protected void onResume() {
+    super.onResume();
+    mMapView.resume();
+  }
+
+  @Override protected void onPause() {
+    mMapView.pause();
+    super.onPause();
+  }
+
+  @Override protected void onDestroy() {
+    mMapView.dispose();
+    super.onDestroy();
   }
 }
