71ce73f677b0977a38a5db85e5770c285bd3f278
Luke William Duncan <lduncan@esri.com>
2019-02-28 12:29:59 +0000

    Removed TAG

--- kotlin/add-features-feature-service/src/main/java/com/esri/arcgisruntime/sample/addfeaturesfeatureservice/MainActivity.kt
+++ kotlin/add-features-feature-service/src/main/java/com/esri/arcgisruntime/sample/addfeaturesfeatureservice/MainActivity.kt
@@ -35,10 +35,6 @@ import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
-  companion object {
-    private val TAG = MainActivity::class.java.simpleName
-  }
-
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.activity_main)
