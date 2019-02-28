d4c3b33c05bc575a021885c48e4514aace2611a0
Luke William Duncan <lduncan@esri.com>
2019-02-28 15:49:12 +0000

    Setting up map, layers etc.

--- java/delete-features-feature-service/src/main/java/com/esri/arcgisruntime/samples/deletefeaturesfeatureservice/MainActivity.java
+++ java/delete-features-feature-service/src/main/java/com/esri/arcgisruntime/samples/deletefeaturesfeatureservice/MainActivity.java
@@ -20,10 +20,39 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 
+import com.esri.arcgisruntime.data.ServiceFeatureTable;
+import com.esri.arcgisruntime.layers.FeatureLayer;
+import com.esri.arcgisruntime.mapping.ArcGISMap;
+import com.esri.arcgisruntime.mapping.Basemap;
+import com.esri.arcgisruntime.mapping.view.MapView;
+
 public class MainActivity extends AppCompatActivity {
+
+  private MapView mMapView;
+
+  private ServiceFeatureTable mFeatureTable;
+
+  private FeatureLayer mFeatureLayer;
 
   @Override protected void onCreate(@Nullable Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
+
+    mMapView = findViewById(R.id.mapView);
+
+    // create a map with streets basemap
+    ArcGISMap map = new ArcGISMap(Basemap.Type.STREETS, 40, -95, 4);
+
+    // create service feature table from URL
+    mFeatureTable = new ServiceFeatureTable(getString(R.string.feature_layer_url));
+
+    // create a feature layer from table
+    mFeatureLayer = new FeatureLayer(mFeatureTable);
+
+    // add the layer to the ArcGISMap
+    map.getOperationalLayers().add(mFeatureLayer);
+
+    // set ArcGISMap to be displayed in map view
+    mMapView.setMap(map);
   }
 }
