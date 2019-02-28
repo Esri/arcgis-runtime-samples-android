be5eef94a6ceea95c5c5e11ce2fe7f3bc69bda5f
Luke William Duncan <lduncan@esri.com>
2019-02-28 14:19:55 +0000

    Added implementation code for Java sample.

--- java/add-features-feature-service/src/main/java/com/esri/arcgisruntime/sample/addfeaturesfeatureservice/MainActivity.java
+++ java/add-features-feature-service/src/main/java/com/esri/arcgisruntime/sample/addfeaturesfeatureservice/MainActivity.java
@@ -16,18 +16,40 @@
 
 package com.esri.arcgisruntime.sample.addfeaturesfeatureservice;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ExecutionException;
+
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.MotionEvent;
+import android.widget.Toast;
 
+import com.esri.arcgisruntime.concurrent.ListenableFuture;
+import com.esri.arcgisruntime.data.Feature;
+import com.esri.arcgisruntime.data.FeatureEditResult;
+import com.esri.arcgisruntime.data.ServiceFeatureTable;
+import com.esri.arcgisruntime.geometry.GeometryEngine;
+import com.esri.arcgisruntime.geometry.Point;
+import com.esri.arcgisruntime.layers.FeatureLayer;
 import com.esri.arcgisruntime.mapping.ArcGISMap;
 import com.esri.arcgisruntime.mapping.Basemap;
+import com.esri.arcgisruntime.mapping.view.DefaultMapViewOnTouchListener;
 import com.esri.arcgisruntime.mapping.view.MapView;
 
 public class MainActivity extends AppCompatActivity {
 
+  private static final String TAG = MainActivity.class.getSimpleName();
+
   private MapView mMapView;
 
+  private ServiceFeatureTable mServiceFeatureTable;
+
+  @SuppressLint("ClickableViewAccessibility")
   @Override protected void onCreate(@Nullable Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
@@ -37,10 +59,117 @@ public class MainActivity extends AppCompatActivity {
     // create a map with streets basemap
     ArcGISMap map = new ArcGISMap(Basemap.Type.STREETS, 40.0, -95.0, 4);
 
+    // create service feature table from URL
+    mServiceFeatureTable = new ServiceFeatureTable(getString(R.string.service_layer_url));
+
+    // create a feature layer from table
+    FeatureLayer featureLayer = new FeatureLayer(mServiceFeatureTable);
+
+    // add the layer to the ArcGISMap
+    map.getOperationalLayers().add(featureLayer);
+
+    // add a listener to the MapView to detect when a user has performed a single tap to add a new feature to
+    // the service feature table
+    mMapView.setOnTouchListener(new DefaultMapViewOnTouchListener(this, mMapView) {
+      @Override public boolean onSingleTapConfirmed(MotionEvent event) {
+        // create a point from where the user clicked
+        android.graphics.Point point = new android.graphics.Point((int) event.getX(), (int) event.getY());
+
+        // create a map point from a point
+        Point mapPoint = mMapView.screenToLocation(point);
+
+        // for a wrapped around map, the point coordinates include the wrapped around value
+        // for a service in projected coordinate system, this wrapped around value has to be normalized
+        Point normalizedMapPoint = (Point) GeometryEngine.normalizeCentralMeridian(mapPoint);
+
+        // add a new feature to the service feature table
+        addFeature(normalizedMapPoint, mServiceFeatureTable);
+        return super.onSingleTapConfirmed(event);
+      }
+    });
+
     // set ArcGISMap to be displayed in map view
     mMapView.setMap(map);
+  }
 
-    // TODO
+  /**
+   * Adds a new Feature to a ServiceFeatureTable and applies the changes to the
+   * server.
+   *
+   * @param mapPoint     location to add feature
+   * @param featureTable service feature table to add feature
+   */
+  private void addFeature(Point mapPoint, final ServiceFeatureTable featureTable) {
+
+    // create default attributes for the feature
+    Map<String, Object> attributes = new HashMap<>();
+    attributes.put("typdamage", "Destroyed");
+    attributes.put("primcause", "Earthquake");
+
+    // creates a new feature using default attributes and point
+    Feature feature = featureTable.createFeature(attributes, mapPoint);
+
+    // check if feature can be added to feature table
+    if (featureTable.canAdd()) {
+      // add the new feature to the feature table and to server
+      featureTable.addFeatureAsync(feature).addDoneListener(new Runnable() {
+        @Override public void run() {
+          applyEdits(featureTable);
+        }
+      });
+    } else {
+      runOnUiThread(new Runnable() {
+        @Override public void run() {
+          logToUser(getString(R.string.error_cannot_add_to_feature_table));
+        }
+      });
+    }
+  }
+
+  /**
+   * Sends any edits on the ServiceFeatureTable to the server.
+   *
+   * @param featureTable service feature table
+   */
+  private void applyEdits(ServiceFeatureTable featureTable) {
+
+    // apply the changes to the server
+    final ListenableFuture<List<FeatureEditResult>> editResult = featureTable.applyEditsAsync();
+    editResult.addDoneListener(new Runnable() {
+      @Override public void run() {
+        try {
+          List<FeatureEditResult> edits = editResult.get();
+          // check if the server edit was successful
+          if (edits != null && edits.size() > 0) {
+            if (!edits.get(0).hasCompletedWithErrors()) {
+              runOnUiThread(new Runnable() {
+                @Override public void run() {
+                  logToUser(getString(R.string.feature_added));
+                }
+              });
+            } else {
+              throw edits.get(0).getError();
+            }
+          }
+        } catch (InterruptedException | ExecutionException e) {
+          runOnUiThread(new Runnable() {
+            @Override public void run() {
+              logToUser(getString(R.string.error_applying_edits, e.getCause().getMessage()));
+            }
+          });
+        }
+      }
+    });
+  }
+
+  /**
+   * Shows a Toast to user and logs to logcat.
+   *
+   * @param message message to display
+   */
+  private void logToUser(String message) {
+    Toast.makeText(this, message, Toast.LENGTH_LONG).show();
+    Log.d(TAG, message);
   }
 
   @Override protected void onResume() {
