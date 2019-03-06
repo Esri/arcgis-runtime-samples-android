4aae939899ebbcf26758ea7cb797d58cd14d6899
Luke William Duncan <lduncan@esri.com>
2019-03-06 12:59:14 +0000

    Basic sample completed.

--- java/terrain-surface-from-local-tile-package/src/main/java/com/esri/arcgisruntime/samples/terrainsurfacefromlocaltilepackage/MainActivity.java
+++ java/terrain-surface-from-local-tile-package/src/main/java/com/esri/arcgisruntime/samples/terrainsurfacefromlocaltilepackage/MainActivity.java
@@ -16,19 +16,100 @@
 
 package com.esri.arcgisruntime.samples.terrainsurfacefromlocaltilepackage;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.os.Environment;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.esri.arcgisruntime.loadable.LoadStatus;
+import com.esri.arcgisruntime.mapping.ArcGISScene;
+import com.esri.arcgisruntime.mapping.ArcGISTiledElevationSource;
+import com.esri.arcgisruntime.mapping.Basemap;
+import com.esri.arcgisruntime.mapping.view.Camera;
+import com.esri.arcgisruntime.mapping.view.SceneView;
 
 public class MainActivity extends AppCompatActivity {
 
-  private View mSceneView;
+  private static final String TAG = MainActivity.class.getSimpleName();
+
+  private static final int PERMISSIONS_REQUEST_CODE = 1;
+
+  private static final String[] PERMISSIONS = { Manifest.permission.READ_EXTERNAL_STORAGE };
+
+  private SceneView mSceneView;
 
   @Override protected void onCreate(@Nullable Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
 
     mSceneView = findViewById(R.id.sceneView);
+
+    // create a scene and add a basemap to it
+    ArcGISScene scene = new ArcGISScene();
+    scene.setBasemap(Basemap.createImagery());
+
+    mSceneView.setScene(scene);
+
+    // specify the initial camera position
+    Camera camera = new Camera(36.525, -121.80, 300.0, 180, 80.0, 0.0);
+    mSceneView.setViewpointCamera(camera);
+
+    requestReadPermission();
+  }
+
+  /**
+   * Request read external storage for API level 23+.
+   */
+  private void requestReadPermission() {
+    if (ContextCompat.checkSelfPermission(this, PERMISSIONS[0]) == PackageManager.PERMISSION_GRANTED) {
+      createTiledElevationSource();
+    } else {
+      // Request permission
+      ActivityCompat.requestPermissions(this, PERMISSIONS, PERMISSIONS_REQUEST_CODE);
+    }
+  }
+
+  /**
+   * Handle the permissions request response
+   */
+  public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+    if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+      createTiledElevationSource();
+    } else {
+      // Report to user that permission was denied
+      String error = getString(R.string.error_read_permission_denied_message);
+      Toast.makeText(this, error, Toast.LENGTH_SHORT).show();
+      Log.e(TAG, error);
+    }
+  }
+
+  private void createTiledElevationSource() {
+    // Add a ArcGISTiledElevationSource to the scene by passing the URI of the local tile package to the constructor
+    ArcGISTiledElevationSource tiledElevationSource = new ArcGISTiledElevationSource(
+        Environment.getExternalStorageDirectory() + getString(R.string.local_tile_package_location));
+
+    // Add a listener to perform operations when the load status of the ArcGISTiledElevationSource changes
+    tiledElevationSource.addLoadStatusChangedListener(loadStatusChangedEvent -> {
+      // When ArcGISTiledElevationSource loads
+      if (loadStatusChangedEvent.getNewLoadStatus() == LoadStatus.LOADED) {
+        // Add the ArcGISTiledElevationSource to the elevation sources of the scene
+        mSceneView.getScene().getBaseSurface().getElevationSources().add(tiledElevationSource);
+      } else if (loadStatusChangedEvent.getNewLoadStatus() == LoadStatus.FAILED_TO_LOAD) {
+        // Notify user that the ArcGISTiledElevationSource has failed to load
+        String error = getString(R.string.error_tiled_elevation_source_load_failure_message);
+        Toast.makeText(this, error, Toast.LENGTH_LONG).show();
+        Log.e(TAG, error);
+      }
+    });
+
+    // Load the ArcGISTiledElevationSource asynchronously
+    tiledElevationSource.loadAsync();
   }
 }
