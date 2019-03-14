37c2bac54b56fe1ad3b570fc403cf9599f622314
Luke William Duncan <lduncan@esri.com>
2019-03-14 10:17:55 +0000

    Implemented Java sample.

--- java/token-authentication/src/main/java/com/esri/tokenauthentication/MainActivity.java
+++ java/token-authentication/src/main/java/com/esri/tokenauthentication/MainActivity.java
@@ -20,8 +20,17 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 
+import com.esri.arcgisruntime.geometry.Point;
+import com.esri.arcgisruntime.geometry.SpatialReference;
+import com.esri.arcgisruntime.layers.ArcGISMapImageLayer;
+import com.esri.arcgisruntime.mapping.ArcGISMap;
+import com.esri.arcgisruntime.mapping.Basemap;
+import com.esri.arcgisruntime.mapping.Viewpoint;
 import com.esri.arcgisruntime.mapping.view.MapView;
 import com.esri.arcgisruntime.samples.tokenauthentication.R;
+import com.esri.arcgisruntime.security.AuthenticationChallengeHandler;
+import com.esri.arcgisruntime.security.AuthenticationManager;
+import com.esri.arcgisruntime.security.DefaultAuthenticationChallengeHandler;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -32,5 +41,42 @@ public class MainActivity extends AppCompatActivity {
     setContentView(R.layout.activity_main);
 
     mMapView = findViewById(R.id.mapView);
+
+    // set up an authentication handler to take credentials for access to the protected layer
+    AuthenticationChallengeHandler handler = new DefaultAuthenticationChallengeHandler(this);
+    AuthenticationManager.setAuthenticationChallengeHandler(handler);
+
+    // create a ArcGISMap with a topographic basemap
+    ArcGISMap map = new ArcGISMap(Basemap.createTopographic());
+
+    // center for initial viewpoint of map
+    Point center = new Point(-12649954, 7123527, SpatialReference.create(3857));
+
+    // set initial viewpoint of map
+    map.setInitialViewpoint(new Viewpoint(center, 167233023));
+
+    // create a layer with dynamically generated map images
+    ArcGISMapImageLayer mapImageLayer = new ArcGISMapImageLayer(getString(R.string.map_image_layer_url));
+
+    // add the layer to the operational layers of the map
+    map.getOperationalLayers().add(mapImageLayer);
+
+    // set the map to be displayed in the map view
+    mMapView.setMap(map);
+  }
+
+  @Override protected void onResume() {
+    super.onResume();
+    mMapView.resume();
+  }
+
+  @Override protected void onPause() {
+    mMapView.pause();
+    super.onPause();
+  }
+
+  @Override protected void onDestroy() {
+    mMapView.dispose();
+    super.onDestroy();
   }
 }
