d1136f49f15edc244b96bc5f5e26c35c381c7bab
trev8939 <tdraeseke@esri.com>
2019-04-02 15:26:03 +0100

    possible WFS fix

--- java/display-wfs-layer/src/main/java/com/esri/arcgisruntime/sample/displaywfslayer/MainActivity.java
+++ java/display-wfs-layer/src/main/java/com/esri/arcgisruntime/sample/displaywfslayer/MainActivity.java
@@ -19,19 +19,24 @@ package com.esri.arcgisruntime.sample.displaywfslayer;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 
 import com.esri.arcgisruntime.data.QueryParameters;
 import com.esri.arcgisruntime.data.ServiceFeatureTable;
+import com.esri.arcgisruntime.geometry.Envelope;
+import com.esri.arcgisruntime.geometry.Point;
+import com.esri.arcgisruntime.geometry.SpatialReferences;
 import com.esri.arcgisruntime.layers.FeatureLayer;
 import com.esri.arcgisruntime.mapping.ArcGISMap;
 import com.esri.arcgisruntime.mapping.Basemap;
-import com.esri.arcgisruntime.mapping.Viewpoint;
 import com.esri.arcgisruntime.mapping.view.MapView;
 import com.esri.arcgisruntime.ogc.wfs.WfsFeatureTable;
 import com.esri.arcgisruntime.symbology.SimpleLineSymbol;
 import com.esri.arcgisruntime.symbology.SimpleRenderer;
 
 public class MainActivity extends AppCompatActivity {
+
+  private static final String TAG = MainActivity.class.getSimpleName();
 
   private MapView mMapView;
 
@@ -45,7 +50,10 @@ public class MainActivity extends AppCompatActivity {
     // create a map with topographic basemap and set it to the map view
     ArcGISMap map = new ArcGISMap(Basemap.createTopographic());
     mMapView.setMap(map);
-    mMapView.setViewpointAsync(new Viewpoint(47.608013, -122.335167, 2000));
+    Point topLeft = new Point(-122.341581, 47.617207, SpatialReferences.getWgs84());
+    Point bottomRight = new Point(-122.332662, 47.613758, SpatialReferences.getWgs84());
+    Envelope initialExtent = new Envelope(topLeft, bottomRight);
+    mMapView.setViewpointGeometryAsync(initialExtent);
 
     // create a FeatureTable from the WFS service URL and name of the layer
     WfsFeatureTable wfsFeatureTable = new WfsFeatureTable(getString(R.string.wfs_service_url),
@@ -58,6 +66,9 @@ public class MainActivity extends AppCompatActivity {
     // create a feature layer to visualize the WFS features
     FeatureLayer wfsFeatureLayer = new FeatureLayer(wfsFeatureTable);
 
+    Log.d(TAG, "calling populate");
+    populateFromServer(wfsFeatureTable, initialExtent);
+
     // apply a renderer to the feature layer
     SimpleRenderer renderer = new SimpleRenderer(new SimpleLineSymbol(SimpleLineSymbol.Style.SOLID, Color.RED, 3));
     wfsFeatureLayer.setRenderer(renderer);
@@ -69,13 +80,23 @@ public class MainActivity extends AppCompatActivity {
     mMapView.addNavigationChangedListener(navigationChangedEvent -> {
       // once the map view has stopped navigating
       if (!navigationChangedEvent.isNavigating()) {
-        // create a query based on the current visible extent
-        QueryParameters visibleExtentQuery = new QueryParameters();
-        visibleExtentQuery.setGeometry(mMapView.getVisibleArea().getExtent());
-        visibleExtentQuery.setSpatialRelationship(QueryParameters.SpatialRelationship.INTERSECTS);
-        // populate the WFS feature table based on the current extent
-        wfsFeatureTable.populateFromServiceAsync(visibleExtentQuery, false, null);
+        populateFromServer(wfsFeatureTable, mMapView.getVisibleArea().getExtent());
       }
+    });
+  }
+
+  private void populateFromServer(WfsFeatureTable wfsFeatureTable, Envelope extent) {
+
+    Log.d(TAG, "Call to POPULATE");
+
+    wfsFeatureTable.addDoneLoadingListener(() -> {
+      Log.d(TAG, "wfs feature table loaded");
+      // create a query based on the current visible extent
+      QueryParameters visibleExtentQuery = new QueryParameters();
+      visibleExtentQuery.setGeometry(extent);
+      visibleExtentQuery.setSpatialRelationship(QueryParameters.SpatialRelationship.INTERSECTS);
+      // populate the WFS feature table based on the current extent
+      wfsFeatureTable.populateFromServiceAsync(visibleExtentQuery, false, null);
     });
   }
 
