e10ca3c0820cebd5c7721062412e2f2323f3c4cc
Luke William Duncan <lduncan@esri.com>
2019-04-03 16:59:08 +0100

    Loading query from xml

--- java/wfs-xml-query/src/main/java/com/esri/arcgisruntime/samples/wfsxmlquery/MainActivity.java
+++ java/wfs-xml-query/src/main/java/com/esri/arcgisruntime/samples/wfsxmlquery/MainActivity.java
@@ -16,13 +16,26 @@
 
 package com.esri.arcgisruntime.samples.wfsxmlquery;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.concurrent.ExecutionException;
+
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.widget.Toast;
 
+import com.esri.arcgisruntime.concurrent.ListenableFuture;
+import com.esri.arcgisruntime.data.FeatureQueryResult;
+import com.esri.arcgisruntime.data.ServiceFeatureTable;
+import com.esri.arcgisruntime.layers.FeatureLayer;
 import com.esri.arcgisruntime.mapping.ArcGISMap;
 import com.esri.arcgisruntime.mapping.Basemap;
 import com.esri.arcgisruntime.mapping.view.MapView;
+import com.esri.arcgisruntime.ogc.wfs.OgcAxisOrder;
+import com.esri.arcgisruntime.ogc.wfs.WfsFeatureTable;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -37,5 +50,59 @@ public class MainActivity extends AppCompatActivity {
     // create a map and set the map to display in the map view
     ArcGISMap map = new ArcGISMap(Basemap.createNavigationVector());
     mMapView.setMap(map);
+
+    // create the WFS feature table from URL and name
+    WfsFeatureTable statesTable = new WfsFeatureTable(getString(R.string.wfs_feature_table_url),
+        getString(R.string.wfs_feature_table_table_name));
+
+    // set the feature request mode and axis order
+    statesTable.setAxisOrder(OgcAxisOrder.NO_SWAP);
+    statesTable.setFeatureRequestMode(ServiceFeatureTable.FeatureRequestMode.MANUAL_CACHE);
+
+    statesTable.addDoneLoadingListener(() -> {
+      String xmlQuery = null;
+
+      try {
+        xmlQuery = loadQueryFromAssets();
+      } catch (IOException e) {
+        String error = "Error reading XML query file file: " + e.getMessage();
+        Toast.makeText(this, error, Toast.LENGTH_LONG).show();
+        Log.e(MainActivity.class.getSimpleName(), error);
+      }
+
+      ListenableFuture<FeatureQueryResult> featureQueryResultFuture = statesTable
+          .populateFromServiceAsync(xmlQuery, true);
+
+      new Thread(() -> {
+        try {
+          featureQueryResultFuture.get();
+
+          // Create a feature layer to visualize the table.
+          FeatureLayer statesLayer = new FeatureLayer(statesTable);
+
+          runOnUiThread(() -> {
+            // Add the layer to the map.
+            mMapView.getMap().getOperationalLayers().add(statesLayer);
+
+            mMapView.setViewpointGeometryAsync(statesLayer.getFullExtent(), 50);
+          });
+        } catch (InterruptedException | ExecutionException e) {
+          e.printStackTrace();
+        }
+      }).start();
+    });
+
+    statesTable.loadAsync();
+  }
+
+  private String loadQueryFromAssets() throws IOException {
+    StringBuilder xmlString = new StringBuilder();
+    BufferedReader bufferedReader = new BufferedReader(
+        new InputStreamReader(getAssets().open(getString(R.string.xml_query_file_name))));
+    String line;
+    while ((line = bufferedReader.readLine()) != null) {
+      xmlString.append(line);
+    }
+    return xmlString.toString();
   }
 }
