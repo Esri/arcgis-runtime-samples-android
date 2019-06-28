a10952b44bae8db0f0f1aaee1fe08e23218be880
trev8939 <tdraeseke@esri.com>
2019-06-28 12:45:38 +0100

    initial commit

--- java/annotation-sublayer/src/main/java/com/esri/arcgisruntime/sample/annotationsublayer/MainActivity.java
+++ java/annotation-sublayer/src/main/java/com/esri/arcgisruntime/sample/annotationsublayer/MainActivity.java
@@ -18,29 +18,62 @@ package com.esri.arcgisruntime.sample.annotationsublayer;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
 
 import com.esri.arcgisruntime.layers.AnnotationLayer;
 import com.esri.arcgisruntime.layers.AnnotationSublayer;
 import com.esri.arcgisruntime.layers.LayerContent;
+import com.esri.arcgisruntime.mapping.ArcGISMap;
+import com.esri.arcgisruntime.mapping.Basemap;
+import com.esri.arcgisruntime.mapping.view.MapView;
 
 public class MainActivity extends AppCompatActivity {
+
+  private MapView mMapView;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
 
+    // get a reference to the map view
+    mMapView = findViewById(R.id.mapView);
+    // create a map with the BasemapType topographic
+    ArcGISMap map = new ArcGISMap(Basemap.Type.TOPOGRAPHIC, 34.056295, -117.195800, 16);
+    // set the map to be displayed in this view
+    mMapView.setMap(map);
     AnnotationLayer annotationLayer = new AnnotationLayer(
         "https://craigwilliams.esri.com/server/rest/services/Loudoun/FeatureServer/1");
+    map.getOperationalLayers().add(annotationLayer);
 
     LayerContent sublayerContent = annotationLayer.getSubLayerContents().get(0);
     AnnotationSublayer sublayer = (AnnotationSublayer) sublayerContent;
     sublayer.getDefinitionExpression();
-    sublayer.getMaxScale();
-    sublayer.getMinScale();
-    sublayer.getOpacity();
-    sublayer.
+    TextView minScaleTextView = findViewById(R.id.currMinScale);
+    minScaleTextView.setText(String.valueOf(sublayer.getMinScale()));
+    TextView maxScaleTextView = findViewById(R.id.currMaxScale);
+    maxScaleTextView.setText(String.valueOf(sublayer.getMaxScale()));
+    TextView isVisibleTextView = findViewById(R.id.currIsVisible);
+    mMapView.addMapScaleChangedListener(mapScaleChangedEvent -> isVisibleTextView
+        .setText(String.valueOf(sublayer.isVisibleAtScale(mMapView.getMapScale()))));
+  }
 
+  @Override
+  protected void onPause() {
+    super.onPause();
+    mMapView.pause();
+  }
+
+  @Override
+  protected void onResume() {
+    super.onResume();
+    mMapView.resume();
+  }
+
+  @Override
+  protected void onDestroy() {
+    super.onDestroy();
+    mMapView.dispose();
   }
 
 }
