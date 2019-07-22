ee3f86de1cbe4a4235102d9172adc4c075792c99
TADraeseke <TADraeseke@users.noreply.github.com>
2019-07-22 15:26:37 +0100

    Identify KML features

---
+++ java/identify-kml-features/src/main/java/com/esri/arcgisruntime/sample/identifykmlfeatures/MainActivity.java
@@ -0,0 +1,129 @@
+/*
+ *  Copyright 2019 Esri
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+package com.esri.arcgisruntime.sample.identifykmlfeatures;
+
+import java.util.concurrent.ExecutionException;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.text.Html;
+import android.util.Log;
+import android.view.MotionEvent;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.esri.arcgisruntime.concurrent.ListenableFuture;
+import com.esri.arcgisruntime.geometry.Envelope;
+import com.esri.arcgisruntime.geometry.SpatialReferences;
+import com.esri.arcgisruntime.layers.KmlLayer;
+import com.esri.arcgisruntime.mapping.ArcGISMap;
+import com.esri.arcgisruntime.mapping.Basemap;
+import com.esri.arcgisruntime.mapping.GeoElement;
+import com.esri.arcgisruntime.mapping.view.Callout;
+import com.esri.arcgisruntime.mapping.view.DefaultMapViewOnTouchListener;
+import com.esri.arcgisruntime.mapping.view.IdentifyLayerResult;
+import com.esri.arcgisruntime.mapping.view.MapView;
+import com.esri.arcgisruntime.ogc.kml.KmlDataset;
+import com.esri.arcgisruntime.ogc.kml.KmlPlacemark;
+
+public class MainActivity extends AppCompatActivity {
+
+  private static final String TAG = MainActivity.class.getSimpleName();
+
+  private MapView mMapView;
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main);
+
+    // create a map and add it to the map view
+    ArcGISMap map = new ArcGISMap(Basemap.createDarkGrayCanvasVector());
+    mMapView = findViewById(R.id.mapView);
+    mMapView.setMap(map);
+
+    // start zoomed in over the US
+    mMapView.setViewpointGeometryAsync(
+        new Envelope(-19195297.778679, 512343.939994, -3620418.579987, 8658913.035426, 0.0, 0.0,
+            SpatialReferences.getWebMercator()));
+
+    // create a KML dataset of weather forecasts
+    KmlDataset forecastKmlDataset = new KmlDataset("https://www.wpc.ncep.noaa.gov/kml/noaa_chart/WPC_Day1_SigWx.kml");
+
+    // create a KML layer and add it as an operational layer
+    KmlLayer forecastKmlLayer = new KmlLayer(forecastKmlDataset);
+    map.getOperationalLayers().add(forecastKmlLayer);
+
+    // add a click listener to identify clicked features
+    mMapView.setOnTouchListener(new DefaultMapViewOnTouchListener(this, mMapView) {
+
+      @Override public boolean onSingleTapConfirmed(MotionEvent e) {
+        // hide the callout if it's showing
+        mMapView.getCallout().dismiss();
+
+        // get the identified geoelements at the clicked location
+        android.graphics.Point screenPoint = new android.graphics.Point(Math.round(e.getX()), Math.round(e.getY()));
+        ListenableFuture<IdentifyLayerResult> identify = mMapView
+            .identifyLayerAsync(forecastKmlLayer, screenPoint, 5, false);
+        identify.addDoneListener(() -> {
+          try {
+            IdentifyLayerResult result = identify.get();
+            // find the first geoElement that is a KML placemark
+            for (GeoElement geoElement : result.getElements()) {
+              if (geoElement instanceof KmlPlacemark) {
+                // show a callout at the placemark with custom content using the placemark's "balloon content"
+                KmlPlacemark placemark = (KmlPlacemark) geoElement;
+                TextView calloutContent = new TextView(getApplicationContext());
+                calloutContent.setText(Html.fromHtml(placemark.getBalloonContent()));
+
+                // get callout, set content and show
+                Callout callout = mMapView.getCallout();
+                callout.setLocation(mMapView.screenToLocation(screenPoint));
+                callout.setContent(calloutContent);
+                callout.show();
+                break;
+              }
+            }
+          } catch (InterruptedException | ExecutionException ex) {
+            String error = "Error identifying features in layer: " + ex.getMessage();
+            Toast.makeText(MainActivity.this, error, Toast.LENGTH_LONG).show();
+            Log.e(TAG, error);
+          }
+        });
+        return true;
+      }
+    });
+  }
+
+  @Override
+  protected void onPause() {
+    mMapView.pause();
+    super.onPause();
+  }
+
+  @Override
+  protected void onResume() {
+    super.onResume();
+    mMapView.resume();
+  }
+
+  @Override
+  protected void onDestroy() {
+    mMapView.dispose();
+    super.onDestroy();
+  }
+}
