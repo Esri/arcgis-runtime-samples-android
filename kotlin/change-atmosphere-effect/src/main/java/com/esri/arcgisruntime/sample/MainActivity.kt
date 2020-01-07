dbed0aa13f4c0097183d67c0fe699ba5a0e46ea6
Rachael Ellen <rellen@esri.com>
2020-01-07 17:12:01 +0000

    add menu items

--- kotlin/change-atmosphere-effect/src/main/java/com/esri/arcgisruntime/sample/MainActivity.kt
+++ kotlin/change-atmosphere-effect/src/main/java/com/esri/arcgisruntime/sample/MainActivity.kt
@@ -2,13 +2,19 @@ package com.esri.arcgisruntime.sample.changeatmosphereeffect
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import com.esri.arcgisruntime.mapping.ArcGISScene
 import com.esri.arcgisruntime.mapping.ArcGISTiledElevationSource
 import com.esri.arcgisruntime.mapping.Basemap
 import com.esri.arcgisruntime.mapping.Surface
+import com.esri.arcgisruntime.mapping.view.AtmosphereEffect
+import com.esri.arcgisruntime.mapping.view.Camera
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    private val AppCompatActivity.logTag: String get() = this::class.java.simpleName
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,13 +26,33 @@ class MainActivity : AppCompatActivity() {
 
         // add base surface for elevation data
         with(Surface()) {
-          this.elevationSources.add(
-            ArcGISTiledElevationSource(
-              getString(R.string.)
+            this.elevationSources.add(
+              ArcGISTiledElevationSource(
+                      getString(R.string.elevation_image_service)
+              )
             )
-          )
             sceneView.scene.baseSurface = this
         }
+
+        // create a camera and set it as the viewpoint for when the scene loads
+        val camera = Camera(64.416919, -14.483728, 100.0, 318.0, 105.0, 0.0)
+        sceneView.setViewpointCamera(camera)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.change_atmosphere_effect_menu, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // set different atmosphere effects on the scene view
+        sceneView.atmosphereEffect = when (item.itemId) {
+            R.id.action_no_atmosphere_effect -> AtmosphereEffect.NONE
+            R.id.action_realistic_atmosphere_effect -> AtmosphereEffect.REALISTIC
+            R.id.action_horizon_atmosphere_effect -> AtmosphereEffect.HORIZON_ONLY
+            else -> return super.onOptionsItemSelected(item)
+        }
+        return true
     }
 
     override fun onResume() {
