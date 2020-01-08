f8c65ac08ab99e16fc827fc5fc125573e8ceb57b
Rachael Ellen <rellen@esri.com>
2020-01-08 11:58:58 +0000

    Update MainActivity.kt

--- kotlin/change-atmosphere-effect/src/main/java/com/esri/arcgisruntime/sample/changeatmosphereeffect/MainActivity.kt
+++ kotlin/change-atmosphere-effect/src/main/java/com/esri/arcgisruntime/sample/changeatmosphereeffect/MainActivity.kt
@@ -1,3 +1,19 @@
+/* Copyright 2020 Esri
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
 package com.esri.arcgisruntime.sample.changeatmosphereeffect
 
 import androidx.appcompat.app.AppCompatActivity
@@ -13,8 +29,6 @@ import com.esri.arcgisruntime.mapping.view.Camera
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
-
-    private val AppCompatActivity.logTag: String get() = this::class.java.simpleName
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
