b85328b7f6ca3960453a440800d106b2c38d1b48
trev8939 <tdraeseke@esri.com>
2020-01-29 11:34:47 +0000

    rename sample, add screenshot, readme and metadata files

--- kotlin/trace-utility-network/src/main/java/com/esri/arcgisruntime/sample/traceutilitynetwork/MainActivity.kt
+++ kotlin/trace-utility-network/src/main/java/com/esri/arcgisruntime/sample/traceutilitynetwork/MainActivity.kt
@@ -14,7 +14,7 @@
  *  limitations under the License.
  */
 
-package com.esri.arcgisruntime.sample.findconnectedfeaturesinutilitynetworks
+package com.esri.arcgisruntime.sample.traceutilitynetwork
 
 import android.graphics.Color
 import android.os.Bundle
