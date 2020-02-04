f2ec568795af31f61d4ab36f4651fe64dad05eb9
Soren Roth <sor10874@sroth.esri.com>
2020-02-03 17:10:50 -0800

    some but not all boilerplate from contributor guidelines

---
+++ kotlin/feature-layer-query/README.md
@@ -0,0 +1,18 @@
+# Feature Layer Query
+Query a feature layer via feature table.
+
+![Feature Layer Query App](feature-layer-query.png)
+
+## How to use this sample
+The sample provides a search bar on the top, where you can input the name of a US State. When you search the app performs a query on the feature table and based on the result either highlights the state geometry or provides an error.
+
+## How it works
+When you hit the search button, the sample creates an query parameter object and specifies the where clause on it, using the text you provided. It then fires the query on the feature table using the query features method. In the completion block it gets back an feature query result. It iterates through the results and finds the first  feature which it then highlights using select features method on the feature layer.
+
+## Relevant API
+* FeatureLayer
+* FeatureTable
+* GeoPackage
+
+#### Tags
+Search and Query
---
+++ kotlin/feature-layer-query/src/main/java/com/esri/arcgisruntime/sample/featurelayerquery/MainActivity.kt
@@ -0,0 +1,79 @@
+/*
+ * Copyright 2020 Esri
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package com.esri.arcgisruntime.sample.featurelayerquery
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.app.SearchManager
+import android.content.Intent
+import android.R
+import android.view.Menu
+
+
+
+
+
+
+class MainActivity : AppCompatActivity() {
+
+
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContentView(R.layout.activity_main)
+  }
+
+
+  /**
+   * Handle the search intent from the search widget
+   */
+  override fun onNewIntent(intent: Intent) {
+    setIntent(intent)
+    if (Intent.ACTION_SEARCH == intent.action) {
+      val searchString = intent.getStringExtra(SearchManager.QUERY)
+      //searchForState(searchString)
+    }
+  }
+
+  override fun onCreateOptionsMenu(menu: Menu): Boolean {
+//    // inflate the menu; this adds items to the action bar if it is present.
+//    menuInflater.inflate(R.menu.menu_main, menu)
+//    // get the SearchView and set the searchable configuration
+//    val searchManager = getSystemService<Any>(Context.SEARCH_SERVICE) as SearchManager
+//    val searchView = menu.findItem(R.id.action_search).getActionView() as SearchView
+//    // assumes current activity is the searchable activity
+//    searchView.setSearchableInfo(searchManager.getSearchableInfo(componentName))
+//    searchView.setIconifiedByDefault(false)
+    return true
+  }
+
+//  override fun onPause() {
+//    mapView.pause()
+//    super.onPause()
+//  }
+//
+//  override fun onResume() {
+//    super.onResume()
+//    mapView.resume()
+//  }
+//
+//  override fun onDestroy() {
+//    mapView.dispose()
+//    super.onDestroy()
+//  }
+
+}
