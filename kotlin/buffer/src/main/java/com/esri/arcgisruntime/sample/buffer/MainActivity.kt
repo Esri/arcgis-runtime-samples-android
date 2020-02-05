77171888ca7a8f07a4dd9fd0783771b415187393
Hudson Miears <hud10837@maskwa.esri.com>
2020-02-05 09:05:29 +0000

    Rewrote buffer in kotlin. Works but needs style review

--- kotlin/buffer/src/main/java/com/esri/arcgisruntime/sample/buffer/MainActivity.kt
+++ kotlin/buffer/src/main/java/com/esri/arcgisruntime/sample/buffer/MainActivity.kt
@@ -17,97 +17,90 @@ package com.esri.arcgisruntime.sample.buffer
 
 import android.graphics.Color
 import android.os.Bundle
-import androidx.appcompat.app.AppCompatActivity
 import android.view.MotionEvent
-import android.widget.Button
-import android.widget.EditText
 import android.widget.Toast
-
-import com.esri.arcgisruntime.geometry.GeodeticCurveType
-import com.esri.arcgisruntime.geometry.Geometry
-import com.esri.arcgisruntime.geometry.GeometryEngine
-import com.esri.arcgisruntime.geometry.LinearUnit
-import com.esri.arcgisruntime.geometry.LinearUnitId
-import com.esri.arcgisruntime.geometry.Point
-import com.esri.arcgisruntime.geometry.SpatialReferences
+import androidx.appcompat.app.AppCompatActivity
+import com.esri.arcgisruntime.geometry.*
 import com.esri.arcgisruntime.mapping.ArcGISMap
 import com.esri.arcgisruntime.mapping.Basemap
 import com.esri.arcgisruntime.mapping.view.DefaultMapViewOnTouchListener
 import com.esri.arcgisruntime.mapping.view.Graphic
 import com.esri.arcgisruntime.mapping.view.GraphicsOverlay
-import com.esri.arcgisruntime.mapping.view.MapView
 import com.esri.arcgisruntime.symbology.SimpleFillSymbol
 import com.esri.arcgisruntime.symbology.SimpleLineSymbol
 import com.esri.arcgisruntime.symbology.SimpleMarkerSymbol
 import com.esri.arcgisruntime.symbology.SimpleRenderer
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlin.math.roundToInt
 
 class MainActivity : AppCompatActivity() {
 
-    private var mMapView: MapView? = null
 
-    @Override
-    protected fun onCreate(savedInstanceState: Bundle) {
+    override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        // inflate views
-        mMapView = findViewById(R.id.mapView)
-        val bufferInput = findViewById(R.id.bufferInput)
+        //UNSURE: I originally had this down below with the graphics overlay stuff, but moved it out
+        // to prevent too much nesting.
+        // the traceutilitynetwork sample has a triple nested apply{apply{apply}}}.
 
         // create a map with the Basemap
-        val map = ArcGISMap(SpatialReferences.getWebMercator())
-        map.setBasemap(Basemap.createTopographic())
-        // set the map to be displayed in this view
-        mMapView!!.setMap(map)
-
-        // create a graphics overlay to contain the buffered geometry graphics
-        val graphicsOverlay = GraphicsOverlay()
-        mMapView!!.getGraphicsOverlays().add(graphicsOverlay)
+        mapView.map = ArcGISMap(SpatialReferences.getWebMercator()).apply {
+            basemap = Basemap.createTopographic() //UNSURE: Is the apply block necessary here? Should I not just separate it into two lines?
+        }
 
         // create a fill symbol for geodesic buffer polygons
-        val geodesicOutlineSymbol = SimpleLineSymbol(SimpleLineSymbol.Style.SOLID, Color.BLACK, 2)
+        val geodesicOutlineSymbol = SimpleLineSymbol(SimpleLineSymbol.Style.SOLID, Color.BLACK, 2F)
         val geodesicBufferFillSymbol = SimpleFillSymbol(SimpleFillSymbol.Style.SOLID, Color.GREEN,
                 geodesicOutlineSymbol)
 
         // create a fill symbol for planar buffer polygons
-        val planarOutlineSymbol = SimpleLineSymbol(SimpleLineSymbol.Style.SOLID, Color.BLACK, 2)
+        val planarOutlineSymbol = SimpleLineSymbol(SimpleLineSymbol.Style.SOLID, Color.BLACK, 2F)
         val planarBufferFillSymbol = SimpleFillSymbol(SimpleFillSymbol.Style.SOLID, Color.RED,
                 planarOutlineSymbol)
 
         // create a marker symbol for tap locations
-        val tapSymbol = SimpleMarkerSymbol(SimpleMarkerSymbol.Style.CROSS, Color.WHITE, 14)
+        val tapSymbol = SimpleMarkerSymbol(SimpleMarkerSymbol.Style.CROSS, Color.WHITE, 14F)
 
-        // create a graphics overlay to display geodesic polygons, set its renderer and add it to the map view.
-        val geodesicGraphicsOverlay = GraphicsOverlay()
-        geodesicGraphicsOverlay.setRenderer(SimpleRenderer(geodesicBufferFillSymbol))
-        geodesicGraphicsOverlay.setOpacity(0.5f)
-        mMapView!!.getGraphicsOverlays().add(geodesicGraphicsOverlay)
+        // create a graphics overlay to display geodesic polygons and set its renderer.
+        val geodesicGraphicsOverlay = GraphicsOverlay().apply {
+            renderer = SimpleRenderer(geodesicBufferFillSymbol)
+            opacity = 0.5f
+        }
 
-        // create a graphics overlay to display planar polygons, set its renderer and add it to the map view.
-        val planarGraphicsOverlay = GraphicsOverlay()
-        planarGraphicsOverlay.setRenderer(SimpleRenderer(planarBufferFillSymbol))
-        planarGraphicsOverlay.setOpacity(0.5f)
-        mMapView!!.getGraphicsOverlays().add(planarGraphicsOverlay)
+        // create a graphics overlay to display planar polygons and set its renderer.
+        val planarGraphicsOverlay = GraphicsOverlay().apply {
+            renderer = SimpleRenderer(planarBufferFillSymbol)
+            opacity = 0.5f
+        }
 
-        // create a graphics overlay to display tap locations for buffers, set its renderer and add it to the map view.
-        val tapLocationsOverlay = GraphicsOverlay()
-        tapLocationsOverlay.setRenderer(SimpleRenderer(tapSymbol))
-        mMapView!!.getGraphicsOverlays().add(tapLocationsOverlay)
+        // create a graphics overlay to display tap locations for buffers and set its renderer.
+        val tapLocationsOverlay = GraphicsOverlay().apply {
+            renderer = SimpleRenderer(tapSymbol)
+        }
+
+        // add overlays to the mapView.
+        mapView.graphicsOverlays.apply {
+            add(geodesicGraphicsOverlay)
+            add(planarGraphicsOverlay)
+            add(tapLocationsOverlay)
+        }
 
         // create a buffer around the clicked location
-        mMapView!!.setOnTouchListener(object : DefaultMapViewOnTouchListener(this, mMapView) {
-            @Override
-            fun onSingleTapConfirmed(motionEvent: MotionEvent): Boolean {
+
+        mapView.onTouchListener = object: DefaultMapViewOnTouchListener(applicationContext, mapView){
+            override fun onSingleTapConfirmed(motionEvent: MotionEvent): Boolean {
 
                 // get the point that was clicked and convert it to a point in the map
-                val screenPoint = android.graphics.Point(Math.round(motionEvent.getX()),
-                        Math.round(motionEvent.getY()))
-                val mapPoint = mMapView!!.screenToLocation(screenPoint)
+                val screenPoint = android.graphics.Point(motionEvent.x.roundToInt(),
+                        motionEvent.y.roundToInt())
+                val mapPoint = mapView.screenToLocation(screenPoint)
 
                 // only draw a buffer if a value was entered
-                if (!bufferInput.getText().toString().isEmpty()) {
+                if (bufferInput.text.toString().isNotEmpty()) {
                     // get the buffer distance (miles) entered in the text box.
-                    val bufferInMiles = Double.valueOf(bufferInput.getText().toString())
+//                    val bufferInMiles = Double.valueOf(bufferInput.getText().toString())
+                    val bufferInMiles = bufferInput.text.toString().toDouble()
 
                     // convert the input distance to meters, 1609.34 meters in one mile
                     val bufferInMeters = bufferInMiles * 1609.34
@@ -125,40 +118,35 @@ class MainActivity : AppCompatActivity() {
                     val locationGraphic = Graphic(mapPoint)
 
                     // add the buffer polygons and tap location graphics to the appropriate graphic overlays.
-                    planarGraphicsOverlay.getGraphics().add(planarBufferGraphic)
-                    geodesicGraphicsOverlay.getGraphics().add(geodesicBufferGraphic)
-                    tapLocationsOverlay.getGraphics().add(locationGraphic)
+                    planarGraphicsOverlay.graphics.add(planarBufferGraphic)
+                    geodesicGraphicsOverlay.graphics.add(geodesicBufferGraphic)
+                    tapLocationsOverlay.graphics.add(locationGraphic)
                 } else {
                     Toast.makeText(this@MainActivity, "Please enter a buffer distance first.", Toast.LENGTH_LONG).show()
                 }
                 return true
             }
-        })
+        }
 
-        // clear all graphics on button click
-        val clearButton = findViewById(R.id.clearButton)
-        clearButton.setOnClickListener({ v ->
-            planarGraphicsOverlay.getGraphics().clear()
-            geodesicGraphicsOverlay.getGraphics().clear()
-            tapLocationsOverlay.getGraphics().clear()
-        })
+        clearButton.setOnClickListener {
+            planarGraphicsOverlay.graphics.clear()
+            geodesicGraphicsOverlay.graphics.clear()
+            tapLocationsOverlay.graphics.clear()
+        }
     }
 
-    @Override
-    protected fun onPause() {
+    override fun onPause() {
         super.onPause()
-        mMapView!!.pause()
+        mapView.pause()
     }
 
-    @Override
-    protected fun onResume() {
+    override fun onResume() {
         super.onResume()
-        mMapView!!.resume()
+        mapView.resume()
     }
 
-    @Override
-    protected fun onDestroy() {
+    override fun onDestroy() {
         super.onDestroy()
-        mMapView!!.dispose()
+        mapView.dispose()
     }
 }
