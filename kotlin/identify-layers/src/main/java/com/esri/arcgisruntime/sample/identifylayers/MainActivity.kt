1458bc757e906010b2b6dc127961366825c50928
Soren Roth <sroth@esri.com>
2020-02-10 12:35:40 -0800

    finish kotlin conversion. move text to strings file. fix up some comments

--- kotlin/identify-layers/src/main/java/com/esri/arcgisruntime/sample/identifylayers/MainActivity.kt
+++ kotlin/identify-layers/src/main/java/com/esri/arcgisruntime/sample/identifylayers/MainActivity.kt
@@ -1,7 +1,7 @@
 /*
  * Copyright 2020 Esri
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
+ * Licensed under the Apache License, Version 2.0 (the "License")
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
@@ -17,9 +17,14 @@
 
 package com.esri.arcgisruntime.sample.identifylayers
 
-import androidx.appcompat.app.AppCompatActivity
+
+import java.util.concurrent.ExecutionException
+import android.widget.Toast
 import android.os.Bundle
 import android.view.MotionEvent
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AlertDialog.Builder
 import com.esri.arcgisruntime.data.ServiceFeatureTable
 import com.esri.arcgisruntime.geometry.Point
 import com.esri.arcgisruntime.geometry.SpatialReference
@@ -29,39 +34,179 @@ import com.esri.arcgisruntime.mapping.ArcGISMap
 import com.esri.arcgisruntime.mapping.Basemap
 import com.esri.arcgisruntime.mapping.Viewpoint
 import com.esri.arcgisruntime.mapping.view.DefaultMapViewOnTouchListener
-import com.esri.arcgisruntime.mapping.view.DefaultSceneViewOnTouchListener
+import com.esri.arcgisruntime.mapping.view.IdentifyLayerResult
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlin.math.roundToInt
+
 
 class MainActivity : AppCompatActivity() {
+
+  private companion object {
+    private val TAG: String = MainActivity::class.java.simpleName
+  }
 
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.activity_main)
 
-
+    // create a feature layer of damaged property data
     val featureTable = ServiceFeatureTable(getString(R.string.damage_assessment))
     val featureLayer = FeatureLayer(featureTable)
 
-    val mapImageLayer = ArcGISMapImageLayer(getString(R.string.world_cities))
+    // create a layer with world cities data
+    val mapImageLayer = ArcGISMapImageLayer(getString(R.string.world_cities)).apply {
+      addDoneLoadingListener {
+        // hide continent and world layers
+        subLayerContents.get(1).isVisible = false
+        subLayerContents.get(2).isVisible = false
+      }
+    }
 
+    // create a topographic map
     val map = ArcGISMap(Basemap.createTopographic()).apply {
+      // add world cities layer
       operationalLayers.add(mapImageLayer)
+
+      // add damaged property data
       operationalLayers.add(featureLayer)
+
+      // set the map's initial viewpoint
       initialViewpoint = Viewpoint(
         Point(-10977012.785807, 4514257.550369, SpatialReference.create(3857)),
         68015210.0
       )
     }
 
-
+    // assign the map to the map view
     mapView.apply {
       this.map = map
+      // add a listener to detect taps on the map view
       onTouchListener = object : DefaultMapViewOnTouchListener(this@MainActivity, this) {
         override fun onSingleTapConfirmed(e: MotionEvent?): Boolean {
-          return super.onSingleTapConfirmed(e)
+          e?.let {
+            val screenPoint = android.graphics.Point(
+              it.x.roundToInt(),
+              it.y.roundToInt()
+            )
+            identifyResult(screenPoint)
+          }
+          return true
         }
       }
-
     }
   }
+
+  /**
+   * Performs an identify on layers at the given screenpoint and calls handleIdentifyResults(...) to process them.
+   *
+   * @param screenPoint in Android graphic coordinates.
+   */
+  private fun identifyResult(screenPoint: android.graphics.Point) {
+
+    val identifyLayerResultsFuture = mapView
+      .identifyLayersAsync(screenPoint, 12.0, false, 10)
+
+    identifyLayerResultsFuture.addDoneListener {
+      try {
+        val identifyLayerResults = identifyLayerResultsFuture.get()
+        handleIdentifyResults(identifyLayerResults)
+      } catch (e: InterruptedException) {
+        "Error identifying results: ${e.message}".also {
+          Toast.makeText(this, it, Toast.LENGTH_LONG).show()
+          Log.e(TAG, it)
+        }
+      } catch (e: ExecutionException) {
+        "Error identifying results: ${e.message}".also {
+          Toast.makeText(this, it, Toast.LENGTH_LONG).show()
+          Log.e(TAG, it)
+        }
+      }
+    }
+  }
+
+  /**
+   * Processes identify results into a string which is passed to showAlertDialog(...).
+   *
+   * @param identifyLayerResults a list of identify results generated in identifyResult().
+   */
+  private fun handleIdentifyResults(identifyLayerResults: List<IdentifyLayerResult>) {
+    val message = StringBuilder()
+    var totalCount = 0
+    for (identifyLayerResult in identifyLayerResults) {
+      val count = geoElementsCountFromResult(identifyLayerResult)
+      val layerName = identifyLayerResult.layerContent.name
+      message.append(layerName).append(": ").append(count)
+
+      // add new line character if not the final element in array
+      if (identifyLayerResult != identifyLayerResults[identifyLayerResults.size - 1]) {
+        message.append("\n")
+      }
+      totalCount += count
+    }
+
+    // if any elements were found show the results, else notify user that no elements were found
+    if (totalCount > 0) {
+      showAlertDialog(message)
+    } else {
+      Toast.makeText(this, "No element found", Toast.LENGTH_SHORT).show()
+      Log.i(TAG, "No element found.")
+    }
+  }
+
+  /**
+   * Gets a count of the GeoElements in the passed result layer.
+   *
+   * @param result from a single layer.
+   * @return the total count of GeoElements.
+   */
+  private fun geoElementsCountFromResult(result: IdentifyLayerResult): Int {
+    // create temp array
+    val tempResults = mutableListOf<IdentifyLayerResult>(result)
+
+    // using Depth First Search approach to handle recursion
+    var count = 0
+    var index = 0
+
+    while (index < tempResults.size) {
+      // get the result object from the array
+      val identifyResult = tempResults.get(index)
+
+      // update count with geoElements from the result
+      count += identifyResult.getElements().size
+
+      // if sublayer has any results, add result objects in the tempResults array after the current result
+      if (identifyResult.sublayerResults.size > 0) {
+        tempResults.add(identifyResult.sublayerResults.get(index))
+      }
+
+      // update the count and repeat
+      index += 1
+    }
+    return count
+  }
+
+  /**
+   * Shows message in an AlertDialog.
+   *
+   * @param message contains identify results processed into a string.
+   */
+  private fun showAlertDialog(message: StringBuilder) {
+    val alertDialogBuilder = Builder(this)
+
+    // set title
+    alertDialogBuilder.setTitle("Number of elements found")
+
+    // set dialog message
+    alertDialogBuilder
+      .setMessage(message)
+      .setCancelable(false)
+      .setPositiveButton(getString(R.string.ok)){ dialog, which -> }
+
+    // create alert dialog
+    val alertDialog = alertDialogBuilder.create()
+
+    // show the alert dialog
+    alertDialog.show()
+  }
+
 }
