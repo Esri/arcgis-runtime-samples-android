e91022e9da81954079bc7f801ae6df30d16d3b1b
hud10837 <hud10837@esri.com>
2020-03-05 11:26:34 +0000

    clean up code

--- kotlin/offline-routing/src/main/java/com/esri/arcgisruntime/sample/offlinerouting/MainActivity.kt
+++ kotlin/offline-routing/src/main/java/com/esri/arcgisruntime/sample/offlinerouting/MainActivity.kt
@@ -21,18 +21,22 @@ import com.esri.arcgisruntime.symbology.TextSymbol
 import com.esri.arcgisruntime.tasks.networkanalysis.RouteParameters
 import com.esri.arcgisruntime.tasks.networkanalysis.RouteTask
 import com.esri.arcgisruntime.tasks.networkanalysis.Stop
-import com.esri.arcgisruntime.tasks.networkanalysis.TravelMode
 import kotlinx.android.synthetic.main.activity_main.*
 import java.util.concurrent.ExecutionException
 import kotlin.math.roundToInt
 
 class MainActivity : AppCompatActivity() {
 
-  val stopsOverlay: GraphicsOverlay by lazy { GraphicsOverlay() }
-  val routeOverlay: GraphicsOverlay by lazy { GraphicsOverlay() }
-  var routeParameters: RouteParameters? = null
-  lateinit var routeTask: RouteTask
-
+  private val stopsOverlay: GraphicsOverlay by lazy { GraphicsOverlay() }
+  private val routeOverlay: GraphicsOverlay by lazy { GraphicsOverlay() }
+  private var routeParameters: RouteParameters? = null
+  private val routeTask: RouteTask by lazy {
+    RouteTask(
+      this,
+      getExternalFilesDir(null)?.path + getString(R.string.geodatabase_path),
+      "Streets_ND"
+    )
+  }
   private val TAG: String = MainActivity::class.java.simpleName
 
   override fun onCreate(savedInstanceState: Bundle?) {
@@ -43,25 +47,20 @@ class MainActivity : AppCompatActivity() {
     //TODO: condense
     val tileCache = TileCache(getExternalFilesDir(null)?.path + getString(R.string.tpk_path))
     val tiledLayer = ArcGISTiledLayer(tileCache)
-    // make a basemap with the tile cache
+    // make a basemap with the tiled layer
     val basemap = Basemap(tiledLayer)
-//    mapView.map = ArcGISMap(Basemap(ArcGISTiledLayer(TileCache(filePath))))
     mapView.map = ArcGISMap(basemap)
-    //TODO: is this needlessly complicated
+
+    // add the graphics overlays to the map view
     mapView.graphicsOverlays.addAll(listOf(stopsOverlay, routeOverlay))
 
-    routeTask = RouteTask(
-      this,
-      getExternalFilesDir(null)?.path + getString(R.string.geodatabase_path),
-      "Streets_ND"
-    )
+    // load the route task
     routeTask.loadAsync()
     routeTask.addDoneLoadingListener {
       if (routeTask.loadStatus == LoadStatus.LOADED) {
         try {
           // create route parameters
           routeParameters = routeTask.createDefaultParametersAsync().get()
-          //todo: do I need to set up travel modes?
         } catch (e: Exception) {
           when (e) {
             is InterruptedException, is ExecutionException -> Log.e(
@@ -75,18 +74,28 @@ class MainActivity : AppCompatActivity() {
       }
     }
 
-    // setup switch.
+    // set up travel mode switch
     modeSwitch.setOnCheckedChangeListener { _, isChecked ->
       //TODO: remove toast
       routeParameters?.travelMode = when (isChecked) {
         true -> routeTask.routeTaskInfo.travelModes[0]
         false -> routeTask.routeTaskInfo.travelModes[1]
       }
-      Toast.makeText(this, routeParameters?.travelMode?.name, Toast.LENGTH_SHORT).show()
+      Toast.makeText(
+        this,
+        "${routeParameters?.travelMode?.name} route selected.",
+        Toast.LENGTH_SHORT
+      ).show()
       updateRoute()
     }
 
-    // add a graphics overlay to show the boundary.
+    // make a clear button to reset the stops and routes
+    clearButton.setOnClickListener {
+      stopsOverlay.graphics.clear()
+      routeOverlay.graphics.clear()
+    }
+
+    // add a graphics overlay to show the boundary
     GraphicsOverlay().let {
       val envelope = Envelope(
         Point(-13045352.223196, 3864910.900750, 0.0, SpatialReferences.getWebMercator()),
@@ -97,56 +106,65 @@ class MainActivity : AppCompatActivity() {
       mapView.graphicsOverlays.add(it)
     }
 
+    // set up the touch listeners on the map view
     createMapGestures()
-
-    clearButton.setOnClickListener {
-      stopsOverlay.graphics.clear()
-      routeOverlay.graphics.clear()
-    }
   }
 
+  /**
+  * Sets up the onTouchListener for the mapView.
+  * For single taps, graphics will be selected.
+  * For double touch drags, graphics will be moved.
+  * */
   private fun createMapGestures() {
-    mapView.setOnTouchListener(object : DefaultMapViewOnTouchListener(this, mapView) {
-      override fun onSingleTapConfirmed(event: MotionEvent): Boolean {
-        // convert screen point to location point
+    mapView.onTouchListener = object : DefaultMapViewOnTouchListener(this, mapView) {
+      override fun onSingleTapConfirmed(motionEvent: MotionEvent): Boolean {
+
         val screenPoint = android.graphics.Point(
-          event.x.roundToInt(),
-          event.y.roundToInt()
+          motionEvent.x.roundToInt(),
+          motionEvent.y.roundToInt()
         )
-        selectGraphic(screenPoint, true)
+        addOrSelectGraphic(screenPoint)
         return true
       }
 
-      override fun onDoubleTouchDrag(event: MotionEvent): Boolean {
-        // convert screen point to location point
+      override fun onDoubleTouchDrag(motionEvent: MotionEvent): Boolean {
+
         val screenPoint = android.graphics.Point(
-          event.x.roundToInt(),
-          event.y.roundToInt()
+          motionEvent.x.roundToInt(),
+          motionEvent.y.roundToInt()
         )
-        selectGraphic(screenPoint, false)
+        // UNSURE: should you be able to select a graphic with double touch drag?
+        // selectGraphic(screenPoint, false)
+
         // move the selected graphic to the new location
         if (stopsOverlay.selectedGraphics.isNotEmpty()) {
           stopsOverlay.selectedGraphics[0]?.geometry = mapView.screenToLocation(screenPoint)
           updateRoute()
         }
-
         // ignore default double touch drag gesture
         return true
       }
 
+      // ignore default double tap gesture
       override fun onDoubleTap(e: MotionEvent?): Boolean {
         return true
       }
-    })
+    }
   }
 
+  /**
+  * Updates the calculated route by calling routeTask.solveRouteAsync().
+  * Creates a graphic to display the route.
+  * */
   private fun updateRoute(){
+    // get a list of stops from the graphics currently on the graphics overlay.
     val stops = stopsOverlay.graphics.map{
       Stop(it.geometry as Point)
     }
 
     routeParameters?.setStops(stops)
 
+    // solve the route
     val results = routeTask.solveRouteAsync(routeParameters)
     results.addDoneListener {
       try {
@@ -169,7 +187,12 @@ class MainActivity : AppCompatActivity() {
     }
   }
 
-  private fun selectGraphic(screenPoint: android.graphics.Point, createNew: Boolean) {
+  /**
+  * Selects a graphic if there is one at the provided tapped location or, if there is none, creates a new graphic.
+  *
+  * @param screenPoint a point in screen space where the user tapped.
+  * */
+  private fun addOrSelectGraphic(screenPoint: android.graphics.Point) {
     // identify the selected graphic
     val results = mapView.identifyGraphicsOverlayAsync(stopsOverlay, screenPoint, 10.0, false)
     results.addDoneListener {
@@ -184,7 +207,7 @@ class MainActivity : AppCompatActivity() {
           val firstGraphic = graphics[0]
           firstGraphic.isSelected = true
         }
-        else if (createNew) {
+        else  { // there is no graphic at this location
           // make a new graphic at the tapped location
           val locationPoint = mapView.screenToLocation(screenPoint)
           val stopLabel = TextSymbol(
@@ -205,4 +228,20 @@ class MainActivity : AppCompatActivity() {
       }
     }
   }
+
+  override fun onPause() {
+    mapView.pause()
+    super.onPause()
+  }
+
+  override fun onResume() {
+    super.onResume()
+    mapView.resume()
+  }
+
+  override fun onDestroy() {
+    mapView.dispose()
+    super.onDestroy()
+  }
+
 }
