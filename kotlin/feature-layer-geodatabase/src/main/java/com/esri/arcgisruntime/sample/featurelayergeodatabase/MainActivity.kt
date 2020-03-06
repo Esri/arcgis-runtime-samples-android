b9685d2e175207f7edfa949da9f4e8db281f92af
hud10837 <hud10837@esri.com>
2020-03-06 16:13:03 +0000

    fix data errors and clean up

--- kotlin/feature-layer-geodatabase/src/main/java/com/esri/arcgisruntime/sample/featurelayergeodatabase/MainActivity.kt
+++ kotlin/feature-layer-geodatabase/src/main/java/com/esri/arcgisruntime/sample/featurelayergeodatabase/MainActivity.kt
@@ -1,4 +1,4 @@
-/* Copyright 2016 Esri
+/* Copyright 2020 Esri
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -32,21 +32,21 @@ import com.esri.arcgisruntime.mapping.Viewpoint
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+  private val TAG =
+    MainActivity::class.java.simpleName
   // permission to read external storage
   private val reqPermission =
     arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE)
+
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.activity_main)
     // create map and add to map view
-    val map = ArcGISMap(Basemap.createStreets())
-    mapView.map = map
+    mapView.map = ArcGISMap(Basemap.createStreets())
     // For API level 23+ request permission at runtime
     if (ContextCompat.checkSelfPermission(
         this@MainActivity,
-        reqPermission[0]
-      ) == PackageManager.PERMISSION_GRANTED
-    ) {
+        reqPermission[0]) == PackageManager.PERMISSION_GRANTED) {
       loadGeodatabase()
     } else { // request permission
       val requestCode = 2
@@ -59,8 +59,7 @@ class MainActivity : AppCompatActivity() {
    */
   private fun loadGeodatabase() { // create path to local geodatabase
     val path: String =
-      (getExternalFilesDir(null)?.path + getString(R.string.config_data_sdcard_offline_dir)
-          + getString(R.string.config_geodb_name))
+      (getExternalFilesDir(null)?.path + getString(R.string.geodb_name))
     // create a new geodatabase from local path
     val geodatabase = Geodatabase(path)
     // load the geodatabase
@@ -75,24 +74,18 @@ class MainActivity : AppCompatActivity() {
         val featureLayer = FeatureLayer(geodatabaseFeatureTable)
         featureLayer.addDoneLoadingListener {
           if (featureLayer.loadStatus == LoadStatus.LOADED) { // set viewpoint to the feature layer's extent
-            mapView!!.setViewpointAsync(Viewpoint(featureLayer.fullExtent))
+            mapView.setViewpointAsync(Viewpoint(featureLayer.fullExtent))
           } else {
             Toast.makeText(this@MainActivity, "Feature Layer failed to load!", Toast.LENGTH_LONG)
               .show()
-            Log.e(
-              TAG,
-              "Feature Layer failed to load!"
-            )
+            Log.e(TAG, "Feature Layer failed to load!")
           }
         }
         // add feature layer to the map
-        mapView!!.map.operationalLayers.add(featureLayer)
+        mapView.map.operationalLayers.add(featureLayer)
       } else {
         Toast.makeText(this@MainActivity, "Geodatabase failed to load!", Toast.LENGTH_LONG).show()
-        Log.e(
-          TAG,
-          "Geodatabase failed to load!"
-        )
+        Log.e(TAG, "Geodatabase failed to load!" + geodatabase.loadError)
       }
     }
   }
@@ -109,29 +102,25 @@ class MainActivity : AppCompatActivity() {
       loadGeodatabase()
     } else { // report to user that permission was denied
       Toast.makeText(
-        this@MainActivity, resources.getString(R.string.write_permission_denied),
+        this@MainActivity,
+        resources.getString(R.string.write_permission_denied),
         Toast.LENGTH_SHORT
       ).show()
     }
   }
 
   override fun onPause() {
+    mapView.pause()
     super.onPause()
-    mapView!!.pause()
   }
 
   override fun onResume() {
     super.onResume()
-    mapView!!.resume()
+    mapView.resume()
   }
 
   override fun onDestroy() {
+    mapView.dispose()
     super.onDestroy()
-    mapView!!.dispose()
-  }
-
-  companion object {
-    private val TAG =
-      MainActivity::class.java.simpleName
   }
 }
