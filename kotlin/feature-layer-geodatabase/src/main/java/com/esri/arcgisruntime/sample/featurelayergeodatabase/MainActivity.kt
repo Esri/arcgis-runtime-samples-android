15b31093e729bcdd98a6b04768b391d52e2d8090
hud10837 <hud10837@esri.com>
2020-03-06 14:09:02 +0000

    copy java files

---
+++ kotlin/feature-layer-geodatabase/src/main/java/com/esri/arcgisruntime/sample/featurelayergeodatabase/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.esri.arcgisruntime.sample.featurelayergeodatabase
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContentView(R.layout.activity_main)
+  }
+}
