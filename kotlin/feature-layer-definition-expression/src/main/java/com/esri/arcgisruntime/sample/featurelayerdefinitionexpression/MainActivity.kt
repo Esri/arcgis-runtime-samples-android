65f569e0e99136a359b643a3e6983b07a737ae10
Rachael Ellen <rellen@esri.com>
2020-03-11 10:55:23 +0000

    set up sample framework

--- kotlin/feature-layer-definition-expression/src/main/java/com/esri/arcgisruntime/sample/featurelayerdefinitionexpression/MainActivity.kt
+++ kotlin/feature-layer-definition-expression/src/main/java/com/esri/arcgisruntime/sample/featurelayerdefinitionexpression/MainActivity.kt
@@ -18,11 +18,36 @@ package com.esri.arcgisruntime.sample.featurelayerdefinitionexpression
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.esri.arcgisruntime.mapping.ArcGISMap
+import com.esri.arcgisruntime.mapping.Basemap
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.activity_main)
+
+    // create a map with the BasemapType topographic
+    val map = ArcGISMap(Basemap.Type.TOPOGRAPHIC, 34.056295, -117.195800, 16)
+    
+    // set the map to be displayed in the layout's MapView
+    mapView.map = map
+
+  }
+
+  override fun onResume() {
+    super.onResume()
+    mapView.resume()
+  }
+
+  override fun onPause() {
+    mapView.pause()
+    super.onPause()
+  }
+
+  override fun onDestroy() {
+    mapView.dispose()
+    super.onDestroy()
   }
 }
