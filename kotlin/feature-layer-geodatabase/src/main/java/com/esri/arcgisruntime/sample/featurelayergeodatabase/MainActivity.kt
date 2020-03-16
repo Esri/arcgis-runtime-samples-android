2aa609f4fbcc673bd35a576862acd55b9e8d6272
hud10837 <hud10837@esri.com>
2020-03-16 14:31:44 +0000

    addres pr comments

--- kotlin/feature-layer-geodatabase/src/main/java/com/esri/arcgisruntime/sample/featurelayergeodatabase/MainActivity.kt
+++ kotlin/feature-layer-geodatabase/src/main/java/com/esri/arcgisruntime/sample/featurelayergeodatabase/MainActivity.kt
@@ -15,14 +15,10 @@
  */
 package com.esri.arcgisruntime.sample.featurelayergeodatabase
 
-import android.Manifest
-import android.content.pm.PackageManager
 import android.os.Bundle
 import android.util.Log
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.app.ActivityCompat
-import androidx.core.content.ContextCompat
 import com.esri.arcgisruntime.data.Geodatabase
 import com.esri.arcgisruntime.layers.FeatureLayer
 import com.esri.arcgisruntime.loadable.LoadStatus
@@ -39,87 +35,35 @@ class MainActivity : AppCompatActivity() {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.activity_main)
     // create map and add to map view
-    mapView.map = ArcGISMap(Basemap.createStreets())
-    // For API level 23+ request permission at runtime
-    if (getPermissionStatus()) loadGeodatabase()
-  }
+    val map = ArcGISMap(Basemap.createStreets())
+    mapView.map = map
 
-  /**
-   * Load a local geodatabase file and add it to the map
-   */
-  private fun loadGeodatabase() { // create path to local geodatabase
-    val path: String =
-      (getExternalFilesDir(null)?.path + getString(R.string.geodb_name))
-    // create a new geodatabase from local path
+    val path: String = getExternalFilesDir(null)?.path + getString(R.string.geodb_name)
     val geodatabase = Geodatabase(path)
-    // load the geodatabase
     geodatabase.loadAsync()
-    // create feature layer from geodatabase and add to the map
     geodatabase.addDoneLoadingListener {
-      if (geodatabase.loadStatus == LoadStatus.LOADED) { // access the geodatabase's feature table Trailheads
-        val geodatabaseFeatureTable =
-          geodatabase.getGeodatabaseFeatureTable("Trailheads")
+      if (geodatabase.loadStatus == LoadStatus.LOADED) {
+        // access the geodatabase's feature table Trailheads
+        val geodatabaseFeatureTable = geodatabase.getGeodatabaseFeatureTable("Trailheads")
         geodatabaseFeatureTable.loadAsync()
         // create a layer from the geodatabase feature table and add to map
         val featureLayer = FeatureLayer(geodatabaseFeatureTable)
         featureLayer.addDoneLoadingListener {
-          if (featureLayer.loadStatus == LoadStatus.LOADED) { // set viewpoint to the feature layer's extent
+          if (featureLayer.loadStatus == LoadStatus.LOADED) {
+            // set viewpoint to the feature layer's extent
             mapView.setViewpointAsync(Viewpoint(featureLayer.fullExtent))
           } else {
-            Toast.makeText(this@MainActivity, "Feature Layer failed to load!", Toast.LENGTH_LONG)
+            Toast.makeText(this, "Feature Layer failed to load!", Toast.LENGTH_LONG)
               .show()
             Log.e(TAG, "Feature Layer failed to load!")
           }
         }
         // add feature layer to the map
-        mapView.map.operationalLayers.add(featureLayer)
+        map.operationalLayers.add(featureLayer)
       } else {
-        Toast.makeText(this@MainActivity, "Geodatabase failed to load!", Toast.LENGTH_LONG).show()
+        Toast.makeText(this, "Geodatabase failed to load!", Toast.LENGTH_LONG).show()
         Log.e(TAG, "Geodatabase failed to load!" + geodatabase.loadError)
       }
-    }
-  }
-
-  /**
-   * Gets the status of permissions for API level 23+.
-   * If the permissions are not granted, requests the permissions.
-   *
-   * @return whether the permissions are granted
-   */
-  private fun getPermissionStatus(): Boolean {
-    // permission to read external storage
-    val reqPermission =
-      arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE)
-    // For API level 23+ request permission at runtime
-    return if (ContextCompat.checkSelfPermission(
-        this@MainActivity,
-        reqPermission[0]
-      ) == PackageManager.PERMISSION_GRANTED
-    ) {
-      true
-    } else { // request permission
-      val requestCode = 2
-      ActivityCompat.requestPermissions(this@MainActivity, reqPermission, requestCode)
-      false
-    }
-  }
-
-  /**
-   * Handle the permissions request response
-   */
-  override fun onRequestPermissionsResult(
-    requestCode: Int,
-    permissions: Array<String>,
-    grantResults: IntArray
-  ) {
-    if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-      loadGeodatabase()
-    } else { // report to user that permission was denied
-      Toast.makeText(
-        this@MainActivity,
-        resources.getString(R.string.write_permission_denied),
-        Toast.LENGTH_SHORT
-      ).show()
     }
   }
 
