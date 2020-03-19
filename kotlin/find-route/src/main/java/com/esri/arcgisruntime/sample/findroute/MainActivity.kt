ac70932cb909e366a84461005efc0a8e5f5b1c68
hud10837 <hud10837@esri.com>
2020-03-19 17:01:23 +0000

    clean up code structure

--- kotlin/find-route/src/main/java/com/esri/arcgisruntime/sample/findroute/MainActivity.kt
+++ kotlin/find-route/src/main/java/com/esri/arcgisruntime/sample/findroute/MainActivity.kt
@@ -15,7 +15,6 @@
  */
 package com.esri.arcgisruntime.sample.findroute
 
-import android.app.ProgressDialog
 import android.content.res.Configuration
 import android.graphics.Color
 import android.graphics.drawable.BitmapDrawable
@@ -26,9 +25,10 @@ import android.view.MenuItem
 import android.view.View
 import android.widget.AdapterView
 import android.widget.ArrayAdapter
-import android.widget.FrameLayout
 import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
+import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.core.content.ContextCompat
 import androidx.drawerlayout.widget.DrawerLayout
 import com.esri.arcgisruntime.geometry.Point
@@ -51,147 +51,145 @@ class MainActivity : AppCompatActivity() {
 
   private val TAG = MainActivity::class.java.simpleName
 
-  private var routeSymbol: SimpleLineSymbol? = null
   private val graphicsOverlay: GraphicsOverlay by lazy {
+    // create a graphics overlay and add it to the map view
     GraphicsOverlay().also {
-      // add the overlay to the map view
       mapView.graphicsOverlays.add(it)
     }
   }
-  private var drawerToggle: ActionBarDrawerToggle? = null
+  private val drawerToggle: ActionBarDrawerToggle by lazy { setupDrawer() }
 
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.activity_main)
-    // create new Vector Tiled Layer from service url
-    val vectorTiledLayer = ArcGISVectorTiledLayer(
-      resources.getString(R.string.navigation_vector)
+    // create new basemap with the vector tiled layer from a service url
+    val basemap = Basemap(
+      ArcGISVectorTiledLayer(resources.getString(R.string.navigation_vector))
     )
-    // create a map with the tiled layer basemap
-    ArcGISMap(Basemap(vectorTiledLayer)).let { map ->
+    // create a map with the basemap
+    ArcGISMap(basemap).let { map ->
       // set initial viewpoint to San Diego
       map.initialViewpoint = Viewpoint(32.7157, -117.1611, 200000.0)
       // set the map to be displayed in this view
       mapView.map = map
     }
     // update UI when attribution view changes
-    val params = directionFab.layoutParams as FrameLayout.LayoutParams
+    val params = directionFab.layoutParams as ConstraintLayout.LayoutParams
     mapView.addAttributionViewLayoutChangeListener { _, _, _, _, bottom, _, _, _, oldBottom ->
       val heightDelta = bottom - oldBottom
       params.bottomMargin += heightDelta
     }
-    setupDrawer()
+
     setupSymbols()
-    val progressDialog = ProgressDialog(this)
-    progressDialog.setTitle(getString(R.string.progress_title))
-    progressDialog.setMessage(getString(R.string.progress_message))
+
     directionFab.setOnClickListener {
-      progressDialog.show()
+
+      //TODO: figure out what this does
       supportActionBar?.apply {
         setDisplayHomeAsUpEnabled(true)
         setHomeButtonEnabled(true)
         title = getString(R.string.app_name)
       }
-      drawer_layout.setDrawerLockMode(DrawerLayout.LOCK_MODE_UNLOCKED)
-      // create RouteTask instance
-      val routeTask = RouteTask(this, getString(R.string.routing_service))
-      val listenableFuture = routeTask.createDefaultParametersAsync()
-      listenableFuture.addDoneListener {
-        try {
-          if (listenableFuture.isDone) {
-            val routeParams = listenableFuture.get()
-            // create stops
-            val stop1 =
-              Stop(
-                Point(
-                  -117.15083257944445,
-                  32.741123367963446,
-                  SpatialReferences.getWgs84()
-                )
-              )
-            val stop2 =
-              Stop(
-                Point(
-                  -117.15557279683529,
-                  32.703360305883045,
-                  SpatialReferences.getWgs84()
-                )
-              )
-            val routeStops: MutableList<Stop> = arrayListOf(stop1, stop2)
-            routeParams.setStops(routeStops)
+
+      solveRoute()
+    }
+  }
+
+  private fun solveRoute() {
+    // create a route task instance
+    val routeTask = RouteTask(this, getString(R.string.routing_service))
+    // create an alert dialog for showing load progress
+    val progressDialog = createProgressDialog(routeTask)
+    progressDialog.show()
+
+    val listenableFuture = routeTask.createDefaultParametersAsync()
+    listenableFuture.addDoneListener {
+      try {
+        if (listenableFuture.isDone) {
+          val routeParams = listenableFuture.get()
+          // create stops
+          val stops = arrayListOf(
+            Stop(Point(-117.15083257944445, 32.741123367963446, SpatialReferences.getWgs84())),
+            Stop(Point(-117.15557279683529, 32.703360305883045, SpatialReferences.getWgs84()))
+          )
+          routeParams.apply {
+            setStops(stops)
             // set return directions as true to return turn-by-turn directions in the result of
             // getDirectionManeuvers().
-            routeParams.isReturnDirections = true
-            // solve
-            val result = routeTask.solveRouteAsync(routeParams).get()
-            val routes: List<*> = result.routes
-            val route = routes[0] as Route
-            // create a mRouteSymbol graphic
-            val routeGraphic = Graphic(route.routeGeometry, routeSymbol)
-            // add mRouteSymbol graphic to the map
-            graphicsOverlay.graphics.add(routeGraphic)
-            // get directions
-            // NOTE: to get turn-by-turn directions Route Parameters should set returnDirection flag as true
-            val directions = route.directionManeuvers
-//            val directionsArray = arrayOfNulls<String>(directions.size)
-            val directionsArray = Array<String>(directions.size) { i ->
-              directions[i].directionText
-            }
-
-            Log.d(TAG, directions[0].geometry.extent.xMin.toString() + "")
-            Log.d(TAG, directions[0].geometry.extent.yMin.toString() + "")
-            // Set the adapter for the list view
-            left_drawer.adapter = ArrayAdapter(
-              applicationContext,
-              R.layout.directions_layout,
-              directionsArray
-            )
-            if (progressDialog.isShowing) {
-              progressDialog.dismiss()
-            }
-            left_drawer.onItemClickListener =
-              AdapterView.OnItemClickListener { _, _, position, _ ->
-                if (graphicsOverlay.graphics.size > 3) {
-                  graphicsOverlay.graphics
-                    .removeAt(graphicsOverlay.graphics.size - 1)
-                }
-                drawer_layout.closeDrawers()
-                val directionManeuver = directions[position]
-                val geometry = directionManeuver.geometry
-                val viewpoint = Viewpoint(geometry.extent, 20.0)
-                mapView.setViewpointAsync(viewpoint, 3f)
-                val selectedRouteSymbol = SimpleLineSymbol(
-                  SimpleLineSymbol.Style.SOLID,
-                  Color.GREEN, 5f
-                )
-                val selectedRouteGraphic = Graphic(
-                  directions[position].geometry,
-                  selectedRouteSymbol
-                )
-                graphicsOverlay.graphics.add(selectedRouteGraphic)
-              }
+            isReturnDirections = true
           }
-        } catch (e: Exception) {
-          Log.e(
-            TAG,
-            e.message ?: ""
+
+          // solve
+          val result = routeTask.solveRouteAsync(routeParams).get()
+          val route = result.routes[0] as Route
+          // create a simple line symbol for the route
+          val routeSymbol = SimpleLineSymbol(SimpleLineSymbol.Style.SOLID, Color.BLUE, 5f)
+          // create a graphic for the route
+          Graphic(route.routeGeometry, routeSymbol).also {
+            // add the graphic to the map
+            graphicsOverlay.graphics.add(it)
+          }
+
+          // get directions
+          // NOTE: to get turn-by-turn directions Route Parameters should set returnDirection flag as true
+          val directions = route.directionManeuvers
+          val directionsArray = Array<String>(directions.size) { i ->
+            directions[i].directionText
+          }
+
+          if (progressDialog.isShowing) {
+            progressDialog.dismiss()
+          }
+
+          left_drawer.apply {
+          // Set the adapter for the list view
+          adapter = ArrayAdapter(
+            applicationContext,
+            R.layout.directions_layout,
+            directionsArray
           )
+
+          onItemClickListener =
+            AdapterView.OnItemClickListener { _, _, position, _ ->
+              if (graphicsOverlay.graphics.size > 3) {
+                graphicsOverlay.graphics.removeAt(graphicsOverlay.graphics.size - 1)
+              }
+
+              drawer_layout.closeDrawers()
+
+              val geometry = directions[position].geometry
+              mapView.setViewpointAsync(
+                Viewpoint(geometry.extent, 20.0),
+                3f
+              )
+              // create a graphic with a symbol for the route and add it to the graphics overlay
+              val selectedRouteSymbol = SimpleLineSymbol(
+                SimpleLineSymbol.Style.SOLID,
+                Color.GREEN, 5f
+              )
+              Graphic(geometry, selectedRouteSymbol).also {
+                graphicsOverlay.graphics.add(it)
+              }
+            }
         }
+        } //TODO: should there be an else here?
+      } catch (e: Exception) {
+        Log.e(TAG, "${e.message}")
       }
     }
   }
 
+
   /**
-   * Set up the Source, Destination and mRouteSymbol graphics symbol
+   * Set up the Source, Destination and routeSymbol graphics symbol
    */
   private fun setupSymbols() {
     //[DocRef: Name=Picture Marker Symbol Drawable-android, Category=Fundamentals, Topic=Symbols and Renderers]
-//Create a picture marker symbol from an app resource
-    val startDrawable =
-      ContextCompat.getDrawable(this, R.drawable.ic_source) as BitmapDrawable?
-    val pinSourceSymbol: PictureMarkerSymbol
+    // Create a picture marker symbol from an app resource
     try {
-      pinSourceSymbol = PictureMarkerSymbol.createAsync(startDrawable).get()
+      val startDrawable =
+        ContextCompat.getDrawable(this, R.drawable.ic_source) as BitmapDrawable?
+      val pinSourceSymbol = PictureMarkerSymbol.createAsync(startDrawable).get()
       pinSourceSymbol.loadAsync()
       pinSourceSymbol.addDoneLoadingListener {
         // add a new graphic as start point
@@ -200,8 +198,9 @@ class MainActivity : AppCompatActivity() {
           32.741123367963446,
           SpatialReferences.getWgs84()
         )
-        val pinSourceGraphic = Graphic(sourcePoint, pinSourceSymbol)
-        graphicsOverlay.graphics.add(pinSourceGraphic)
+        Graphic(sourcePoint, pinSourceSymbol).also {
+          graphicsOverlay.graphics.add(it)
+        }
       }
       pinSourceSymbol.offsetY = 20f
     } catch (e: InterruptedException) {
@@ -210,21 +209,21 @@ class MainActivity : AppCompatActivity() {
       e.printStackTrace()
     }
     //[DocRef: END]
-    val endDrawable =
-      ContextCompat.getDrawable(this, R.drawable.ic_destination) as BitmapDrawable?
-    val pinDestinationSymbol: PictureMarkerSymbol
     try {
-      pinDestinationSymbol = PictureMarkerSymbol.createAsync(endDrawable).get()
+      val endDrawable =
+        ContextCompat.getDrawable(this, R.drawable.ic_destination) as BitmapDrawable?
+      val pinDestinationSymbol = PictureMarkerSymbol.createAsync(endDrawable).get()
       pinDestinationSymbol.loadAsync()
       pinDestinationSymbol.addDoneLoadingListener {
-        //add a new graphic as end point
+        // add a new graphic as end point
         val destinationPoint = Point(
           -117.15557279683529,
           32.703360305883045,
           SpatialReferences.getWgs84()
         )
-        val destinationGraphic = Graphic(destinationPoint, pinDestinationSymbol)
-        graphicsOverlay.graphics.add(destinationGraphic)
+        Graphic(destinationPoint, pinDestinationSymbol).also {
+          graphicsOverlay.graphics.add(it)
+        }
       }
       pinDestinationSymbol.offsetY = 20f
     } catch (e: InterruptedException) {
@@ -233,29 +232,13 @@ class MainActivity : AppCompatActivity() {
       e.printStackTrace()
     }
     //[DocRef: END]
-    routeSymbol = SimpleLineSymbol(SimpleLineSymbol.Style.SOLID, Color.BLUE, 5f)
-  }
-
-  override fun onPause() {
-    super.onPause()
-    mapView.pause()
-  }
-
-  override fun onResume() {
-    super.onResume()
-    mapView.resume()
-  }
-
-  override fun onDestroy() {
-    super.onDestroy()
-    mapView.dispose()
   }
 
   /**
    * set up the drawer
    */
-  private fun setupDrawer() {
-    drawerToggle = object : ActionBarDrawerToggle(
+  private fun setupDrawer() =
+    object : ActionBarDrawerToggle(
       this,
       drawer_layout,
       R.string.drawer_open,
@@ -272,22 +255,42 @@ class MainActivity : AppCompatActivity() {
         super.onDrawerClosed(view)
         invalidateOptionsMenu() // creates call to onPrepareOptionsMenu()
       }
+    }.apply {
+      isDrawerIndicatorEnabled = true
+      drawer_layout.addDrawerListener(this)
+      drawer_layout.setDrawerLockMode(DrawerLayout.LOCK_MODE_UNLOCKED)
     }
-    drawerToggle?.isDrawerIndicatorEnabled = true
-    drawer_layout.addDrawerListener(drawerToggle as ActionBarDrawerToggle)
-    drawer_layout.setDrawerLockMode(DrawerLayout.LOCK_MODE_LOCKED_CLOSED)
+
+
+
+  /** Create a progress dialog box for tracking the route task.
+   *
+   * @param routeTask the route task progress to be tracked
+   * @return an AlertDialog set with the dialog layout view
+   */
+  private fun createProgressDialog(routeTask: RouteTask): AlertDialog {
+    val builder = AlertDialog.Builder(this@MainActivity).apply {
+      setTitle("Solving route...")
+      // provide a cancel button on the dialog
+      setNeutralButton("Cancel") { _, _ ->
+        routeTask.cancelLoad()
+      }
+      setCancelable(false)
+      setView(R.layout.dialog_layout)
+    }
+    return builder.create()
   }
 
   override fun onPostCreate(savedInstanceState: Bundle?) {
     super.onPostCreate(savedInstanceState)
     // Sync the toggle state after onRestoreInstanceState has occurred.
-    drawerToggle?.syncState()
+    drawerToggle.syncState()
   }
 
   override fun onConfigurationChanged(newConfig: Configuration) {
 //    requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE
     super.onConfigurationChanged(newConfig)
-    drawerToggle?.onConfigurationChanged(newConfig)
+    drawerToggle.onConfigurationChanged(newConfig)
   }
 
   override fun onCreateOptionsMenu(menu: Menu): Boolean { // Inflate the menu; this adds items to the action bar if it is present.
@@ -299,7 +302,21 @@ class MainActivity : AppCompatActivity() {
 // automatically handle clicks on the Home/Up button, so long
 // as you specify a parent activity in AndroidManifest.xml.
 // Activate the navigation drawer toggle
-    return drawerToggle?.onOptionsItemSelected(item) ?: super.onOptionsItemSelected(item)
+    return drawerToggle.onOptionsItemSelected(item) || super.onOptionsItemSelected(item)
   }
 
+  override fun onPause() {
+    mapView.pause()
+    super.onPause()
+  }
+
+  override fun onResume() {
+    super.onResume()
+    mapView.resume()
+  }
+
+  override fun onDestroy() {
+    mapView.dispose()
+    super.onDestroy()
+  }
 }
