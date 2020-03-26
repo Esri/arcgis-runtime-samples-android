d51fef9405ba1bb304f1edc70d82502b087bca04
Rachael Ellen <rellen@esri.com>
2020-03-26 13:21:47 +0000

    tidy up sample

--- kotlin/feature-layer-definition-expression/src/main/java/com/esri/arcgisruntime/sample/featurelayerdefinitionexpression/MainActivity.kt
+++ kotlin/feature-layer-definition-expression/src/main/java/com/esri/arcgisruntime/sample/featurelayerdefinitionexpression/MainActivity.kt
@@ -16,24 +16,76 @@
  */
 package com.esri.arcgisruntime.sample.featurelayerdefinitionexpression
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.esri.arcgisruntime.data.ServiceFeatureTable
+import com.esri.arcgisruntime.geometry.Point
+import com.esri.arcgisruntime.geometry.SpatialReferences
+import com.esri.arcgisruntime.layers.FeatureLayer
 import com.esri.arcgisruntime.mapping.ArcGISMap
 import com.esri.arcgisruntime.mapping.Basemap
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
+  private lateinit var featureLayer: FeatureLayer
+  var applyActive: Boolean = false
+
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.activity_main)
 
-    // create a map with the BasemapType topographic
-    val map = ArcGISMap(Basemap.Type.TOPOGRAPHIC, 34.056295, -117.195800, 16)
-    
-    // set the map to be displayed in the layout's MapView
-    mapView.map = map
+    // set up the bottom toolbar
+    createBottomToolbar()
 
+    // create a service feature table from a URL
+    val serviceFeatureTable = ServiceFeatureTable(getString(R.string.sample_service_url))
+    // create a feature layer using the service feature table
+    featureLayer = FeatureLayer(serviceFeatureTable)
+
+    // create a map with a topographic base map
+    ArcGISMap(Basemap.createTopographic()).let {
+      // add the feature layer to the map
+      it.operationalLayers.add(featureLayer)
+      // set the map to be displayed in the layout's map view
+      mapView.map = it
+    }
+
+    // set a viewpoint on the map view to center on San Francisco
+    mapView.setViewpointCenterAsync(
+      Point(-13630845.0, 4544861.0, SpatialReferences.getWebMercator()), 600000.0
+    )
+  }
+
+  /**
+   * Create and configure bottom tool bar for applying the definition expression to the feature layer.
+   */
+  private fun createBottomToolbar() {
+
+    bottomToolbar.inflateMenu(R.menu.menu_main)
+    // handle action bar item clicks
+    bottomToolbar.setOnMenuItemClickListener { item ->
+      val itemId = item.itemId
+      // if statement is used because this sample is used elsewhere as a Library module
+      if (itemId == R.id.action_def_exp) {
+        // check the state of the menu item
+        if (!applyActive) {
+          // apply a definition expression on the feature layer
+          // if this is called before the layer is loaded, it will be applied to the loaded layer
+          featureLayer.definitionExpression = "req_Type = 'Tree Maintenance or Damage'"
+          // change the text to reset
+          applyActive = true
+          item.setTitle(R.string.action_reset)
+        } else {
+          // set the definition expression to nothing (empty string, null also works)
+          featureLayer.definitionExpression = ""
+          // change the text to apply
+          applyActive = false
+          item.setTitle(R.string.action_def_exp)
+        }
+      }
+      true
+    }
   }
 
   override fun onResume() {
