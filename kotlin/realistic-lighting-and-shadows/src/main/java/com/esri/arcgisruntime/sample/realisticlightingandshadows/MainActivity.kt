124a65247f90a0d917c056e3e8b4e49ece6fdda2
hud10837 <hud10837@esri.com>
2020-07-07 11:26:24 +0100

    working sample

--- kotlin/realistic-lighting-and-shadows/src/main/java/com/esri/arcgisruntime/sample/realisticlightingandshadows/MainActivity.kt
+++ kotlin/realistic-lighting-and-shadows/src/main/java/com/esri/arcgisruntime/sample/realisticlightingandshadows/MainActivity.kt
@@ -17,13 +17,84 @@
 
 package com.esri.arcgisruntime.sample.realisticlightingandshadows
 
+import android.app.Dialog
+import android.app.TimePickerDialog
+import android.icu.util.GregorianCalendar
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.format.DateFormat
+import android.transition.Scene
+import android.view.View
+import android.widget.TimePicker
+import androidx.fragment.app.DialogFragment
+import com.esri.arcgisruntime.layers.ArcGISSceneLayer
+import com.esri.arcgisruntime.mapping.ArcGISScene
+import com.esri.arcgisruntime.mapping.ArcGISTiledElevationSource
+import com.esri.arcgisruntime.mapping.Basemap
+import com.esri.arcgisruntime.mapping.Surface
+import com.esri.arcgisruntime.mapping.view.AtmosphereEffect
+import com.esri.arcgisruntime.mapping.view.Camera
+import com.esri.arcgisruntime.mapping.view.LightingMode
+import kotlinx.android.synthetic.main.activity_main.*
+import java.util.Calendar
 
 class MainActivity : AppCompatActivity() {
+
+  private val calendar = Calendar.getInstance()
 
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.activity_main)
+
+    val scene = ArcGISScene().apply{
+      basemap = Basemap.createTopographic()
+    }
+    sceneView.scene = scene
+
+    val surface = Surface()
+    val elevationSurface = ArcGISTiledElevationSource("http://elevation3d.arcgis.com/arcgis/rest/services/WorldElevation3D/Terrain3D/ImageServer")
+    surface.elevationSources.add(elevationSurface)
+    sceneView.scene.baseSurface = surface
+
+    val sceneLayer = ArcGISSceneLayer("http://tiles.arcgis.com/tiles/P3ePLMYs2RVChkJx/arcgis/rest/services/Buildings_Brest/SceneServer/layers/0")
+    scene.operationalLayers.add(sceneLayer)
+
+    val camera = Camera(48.37, -4.50, 1000.0, 10.0, 70.0, 0.0)
+    sceneView.setViewpointCamera(camera)
+
+    sceneView.atmosphereEffect = AtmosphereEffect.REALISTIC
+
+    sceneView.sunTime = calendar
+    sceneView.sunLighting = LightingMode.LIGHT_AND_SHADOWS
+
+    dateTextView.text = calendar.time.toString().substring(0,16)
+  }
+
+  fun showTimePickerDialog(v: View) {
+    val updateSunPositionCallback = { calendar: Calendar ->
+      dateTextView.text = calendar.time.toString().substring(0,16)
+    sceneView.sunTime = calendar
+    }
+    TimePickerFragment(calendar, updateSunPositionCallback).show(supportFragmentManager, "timePicker")
   }
 }
+
+class TimePickerFragment(private val calendar: Calendar, val callback: (Calendar)->Unit) : DialogFragment(), TimePickerDialog.OnTimeSetListener {
+
+  override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+    // Use the current time as the default values for the picker
+    val hour = calendar.get(Calendar.HOUR_OF_DAY)
+    val minute = calendar.get(Calendar.MINUTE)
+
+    // Create a new instance of TimePickerDialog and return it
+    return TimePickerDialog(activity, this, hour, minute, DateFormat.is24HourFormat(activity))
+  }
+
+  override fun onTimeSet(view: TimePicker, hourOfDay: Int, minute: Int) {
+    // Do something with the time chosen by the user
+    calendar.set(calendar.get(Calendar.YEAR), calendar.get(Calendar.MONTH), calendar.get(Calendar.DATE), hourOfDay, minute)
+    callback(calendar)
+//    dateTextView.text =
+  }
+}
+
