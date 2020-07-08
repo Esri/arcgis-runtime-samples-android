6a3ff5c57f42a3e8e8e2056e5480e5a79e52b8ae
hud10837 <hud10837@esri.com>
2020-07-08 16:55:20 +0100

    update ui

--- kotlin/realistic-lighting-and-shadows/src/main/java/com/esri/arcgisruntime/sample/realisticlightingandshadows/MainActivity.kt
+++ kotlin/realistic-lighting-and-shadows/src/main/java/com/esri/arcgisruntime/sample/realisticlightingandshadows/MainActivity.kt
@@ -17,14 +17,13 @@
 
 package com.esri.arcgisruntime.sample.realisticlightingandshadows
 
-import android.app.Dialog
-import android.app.TimePickerDialog
 import android.os.Bundle
-import android.text.format.DateFormat
 import android.view.View
-import android.widget.TimePicker
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.SeekBar
 import androidx.appcompat.app.AppCompatActivity
-import androidx.fragment.app.DialogFragment
+import androidx.coordinatorlayout.widget.CoordinatorLayout
 import com.esri.arcgisruntime.layers.ArcGISSceneLayer
 import com.esri.arcgisruntime.mapping.ArcGISScene
 import com.esri.arcgisruntime.mapping.ArcGISTiledElevationSource
@@ -35,14 +34,19 @@ import com.esri.arcgisruntime.mapping.view.Camera
 import com.esri.arcgisruntime.mapping.view.LightingMode
 import kotlinx.android.synthetic.main.activity_main.*
 import java.util.Calendar
+import kotlin.math.floor
 
 class MainActivity : AppCompatActivity() {
 
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.activity_main)
-    
+
     val calendar = Calendar.getInstance()
+    calendar.apply {
+      set(Calendar.HOUR_OF_DAY, 12)
+      set(Calendar.MINUTE, 0)
+    }
 
     sceneView.apply {
       setViewpointCamera(Camera(48.37, -4.50, 1000.0, 10.0, 70.0, 0.0))
@@ -59,43 +63,64 @@ class MainActivity : AppCompatActivity() {
     }
 
     dateTextView.text = calendar.time.toString().substring(0, 16)
-  }
 
-  fun showTimePickerDialog(v: View) {
-    val updateSunPositionCallback = { calendar: Calendar ->
-      dateTextView.text = calendar.time.toString().substring(0, 16)
-      sceneView.sunTime = calendar
+    seekBar.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
+      override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
+        val hours = floor((progress / 60.0)).toInt()
+        val minutes = progress % 60
+        calendar.apply {
+          set(Calendar.HOUR_OF_DAY, hours)
+          set(Calendar.MINUTE, minutes)
+        }
+        dateTextView.text = calendar.time.toString().substring(0, 16)
+        sceneView.sunTime = calendar
+      }
+
+      override fun onStartTrackingTouch(seekBar: SeekBar?) {}
+      override fun onStopTrackingTouch(seekBar: SeekBar?) {}
+    })
+
+    // create a spinner adapter to select lighting modes
+    ArrayAdapter.createFromResource(
+      this,
+      R.array.lighting_modes,
+      android.R.layout.simple_spinner_item
+    ).also { adapter ->
+      adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+      spinner.adapter = adapter
     }
-    TimePickerFragment(Calendar.getInstance(), updateSunPositionCallback).show(
-      supportFragmentManager,
-      "timePicker"
-    )
+
+    // change the lighting mode when a spinner item is selected
+    spinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener,
+      AdapterView.OnItemClickListener {
+      override fun onItemSelected(parent: AdapterView<*>, view: View?, position: Int, id: Long) {
+        val mode = parent.getItemAtPosition(position) as String
+        sceneView.sunLighting = when (mode) {
+          "No sun" -> LightingMode.NO_LIGHT
+          "Sun only" -> LightingMode.LIGHT
+          "Sun and shadows" -> LightingMode.LIGHT_AND_SHADOWS
+          else -> LightingMode.LIGHT_AND_SHADOWS
+        }
+      }
+      override fun onNothingSelected(parent: AdapterView<*>?) {}
+      override fun onItemClick(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {}
+    }
+
+    sceneView.apply {
+      // make sure the fab doesn't obscure the attribution bar
+      addAttributionViewLayoutChangeListener { _, _, _, _, bottom, _, _, _, oldBottom ->
+        val layoutParams = fab.layoutParams as CoordinatorLayout.LayoutParams
+        layoutParams.bottomMargin += bottom - oldBottom
+      }
+      // close the options when the scene is tapped
+      setOnTouchListener { _, _ ->
+        if (fab.isExpanded) {
+          fab.isExpanded = false
+        }
+        true
+      }
+    }
+    // open and close the options with the fab
+    fab.setOnClickListener { fab.isExpanded = !fab.isExpanded }
   }
 }
-
-class TimePickerFragment(
-  private val calendar: Calendar,
-  val updateSunPositionCallback: (Calendar) -> Unit
-) : DialogFragment(), TimePickerDialog.OnTimeSetListener {
-
-  override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
-    // Use the current time as the default values for the picker
-    val hour = calendar.get(Calendar.HOUR_OF_DAY)
-    val minute = calendar.get(Calendar.MINUTE)
-
-    // Create a new instance of TimePickerDialog and return it
-    return TimePickerDialog(activity, this, hour, minute, DateFormat.is24HourFormat(activity))
-  }
-
-  override fun onTimeSet(view: TimePicker, hourOfDay: Int, minute: Int) {
-    calendar.set(
-      calendar.get(Calendar.YEAR),
-      calendar.get(Calendar.MONTH),
-      calendar.get(Calendar.DATE),
-      hourOfDay,
-      minute
-    )
-    updateSunPositionCallback(calendar)
-  }
-}
-
