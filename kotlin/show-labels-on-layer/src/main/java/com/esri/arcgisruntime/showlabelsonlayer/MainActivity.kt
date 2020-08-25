abe7f90724ad715846e0ef42ea6de3fbbe43d014
trev8939 <tdraeseke@esri.com>
2020-08-25 15:02:04 +0100

    update sample to congressional districts

--- kotlin/show-labels-on-layer/src/main/java/com/esri/arcgisruntime/showlabelsonlayer/MainActivity.kt
+++ kotlin/show-labels-on-layer/src/main/java/com/esri/arcgisruntime/showlabelsonlayer/MainActivity.kt
@@ -1,4 +1,5 @@
-/* Copyright 2018 Esri
+/*
+ * Copyright 2020 Esri
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -16,14 +17,13 @@
 
 package com.esri.arcgisruntime.showlabelsonlayer
 
+import android.graphics.Color
 import android.os.Bundle
 import android.util.Log
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.esri.arcgisruntime.arcgisservices.LabelDefinition
 import com.esri.arcgisruntime.data.ServiceFeatureTable
-import com.esri.arcgisruntime.geometry.Point
-import com.esri.arcgisruntime.geometry.SpatialReferences
 import com.esri.arcgisruntime.layers.FeatureLayer
 import com.esri.arcgisruntime.loadable.LoadStatus
 import com.esri.arcgisruntime.mapping.ArcGISMap
@@ -35,7 +35,6 @@ import com.google.gson.JsonParser
 import com.google.gson.JsonPrimitive
 import kotlinx.android.synthetic.main.activity_main.*
 
-
 class MainActivity : AppCompatActivity() {
 
   private val TAG = MainActivity::class.java.simpleName
@@ -44,10 +43,11 @@ class MainActivity : AppCompatActivity() {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.activity_main)
 
+    // create a map view and set a map
     val map = ArcGISMap(Basemap.createLightGrayCanvas())
     mapView.map = map
 
-    // create a feature layer from an online feature service of US Highways and add it to the map
+    // create a feature layer from an online feature service of US Congressional Districts
     val serviceFeatureTable = ServiceFeatureTable(getString(R.string.congressional_districts_url))
     val featureLayer = FeatureLayer(serviceFeatureTable)
     map.operationalLayers.add(featureLayer)
@@ -55,54 +55,72 @@ class MainActivity : AppCompatActivity() {
     // zoom to the layer when it's done loading
     featureLayer.addDoneLoadingListener {
       if (featureLayer.loadStatus == LoadStatus.LOADED) {
-        // set viewpoint to the center of the US
-        mapView.setViewpointAsync(
-          Viewpoint(
-            featureLayer.fullExtent)
-        )
+        // set viewpoint to feature layer extent
+        mapView.setViewpointAsync(Viewpoint(featureLayer.fullExtent))
       } else {
-        Toast.makeText(
-            this,
-            getString(R.string.error_message) + featureLayer.loadError.message,
-            Toast.LENGTH_LONG
-          )
-          .show()
-        Log.e(TAG, getString(R.string.error_message) + featureLayer.loadError.message)
+        val error = "Error loading feature layer :" + featureLayer.loadError.cause
+        Toast.makeText(this, error, Toast.LENGTH_LONG).show()
+        Log.e(TAG, error)
       }
     }
 
-    // use large blue text with a yellow halo for the labels
-    val textSymbol = TextSymbol().apply {
-      size = 20f
-      color = -0xffff01
-      haloColor = -0x100
+    // use red text with white halo for republican district labels
+    val republicanTextSymbol = TextSymbol().apply {
+      size = 10f
+      color = Color.RED
+      haloColor = Color.WHITE
       haloWidth = 2f
+    }
+
+    // use blue text with white halo for democrat district labels
+    val democratTextSymbol = TextSymbol().apply {
+      size = 10f
+      color = Color.BLUE
+      haloColor = Color.WHITE
+      haloWidth = 2f
+    }
+
+    // use a custom label expression combining some of the feature's fields
+    val expressionInfo = JsonObject().apply {
+      add(
+        "expression",
+        JsonPrimitive("\$feature.NAME + \" (\" + left(\$feature.PARTY,1) + \")\\nDistrict \" + \$feature.CDFIPS")
+      )
     }
 
     // construct the label definition json
     val json = JsonObject().apply {
-      // prepend 'I - ' (for Interstate) to the route number for the label
-      val expressionInfo = JsonObject()
-      expressionInfo.add("expression", JsonPrimitive("'I -' + \$feature.rte_num1"))
       add("labelExpressionInfo", expressionInfo)
-      // position the label above and along the direction of the road
-      add("labelPlacement", JsonPrimitive("esriServerLinePlacementAboveAlong"))
-      // only show labels on the interstate highways (others have an empty rte_num1 attribute)
-      add("where", JsonPrimitive("\$feature.rte_num1 <> ' '"))
-      // set the text symbol as the label symbol
-      add("symbol", JsonParser().parse(textSymbol.toJson()))
+      // position the label in the center of the feature
+      add("labelPlacement", JsonPrimitive("esriServerPolygonPlacementAlwaysHorizontal"))
     }
 
-    // create a label definition from the JSON string
-    val labelDefinition = LabelDefinition.fromJson(json.toString())
-    // add the definition to the feature layer and enable labels on it
-    featureLayer.labelDefinitions.add(labelDefinition)
-    featureLayer.isLabelsEnabled = true
+    // create a copy of the json with a custom where clause and symbol only for republican districts
+    val republicanJson = json.deepCopy().apply {
+      add("where", JsonPrimitive("PARTY = 'Republican'"))
+      add("symbol", JsonParser.parseString(republicanTextSymbol.toJson()))
+    }
+
+    // create a copy of the json with a custom where clause and symbol only for democrat districts
+    val democratJson = json.deepCopy().apply {
+      add("where", JsonPrimitive("PARTY = 'Democrat'"))
+      add("symbol", JsonParser.parseString(democratTextSymbol.toJson()))
+    }
+
+    // create label definitions from the JSON strings
+    val republicanLabelDefinition = LabelDefinition.fromJson(republicanJson.toString())
+    val democratLabelDefinition = LabelDefinition.fromJson(democratJson.toString())
+    featureLayer.apply {
+      // add the definitions to the feature layer
+      labelDefinitions.addAll(listOf(republicanLabelDefinition, democratLabelDefinition))
+      // enable labels
+      isLabelsEnabled = true
+    }
   }
 
   override fun onPause() {
-    super.onPause()
     mapView.pause()
+    super.onPause()
   }
 
   override fun onResume() {
@@ -111,7 +129,7 @@ class MainActivity : AppCompatActivity() {
   }
 
   override fun onDestroy() {
-    super.onDestroy()
     mapView.dispose()
+    super.onDestroy()
   }
 }
