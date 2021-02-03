9bd58578c278a0a91ccc0d8887f01397d4c544b6
TADraeseke <tdraeseke@esri.com>
2021-02-03 12:41:05 +0000

    Trev8939/change basemap fixes (#867)
    * java basemap sample fixes
    
    * update kotlin change basemaps

--- kotlin/change-basemaps/src/main/java/com/esri/arcgisruntime/sample/changebasemaps/MainActivity.kt
+++ kotlin/change-basemaps/src/main/java/com/esri/arcgisruntime/sample/changebasemaps/MainActivity.kt
@@ -33,160 +33,118 @@ import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
-  private lateinit var mNavigationDrawerItemTitles: Array<String>
+    private lateinit var mNavigationDrawerItemTitles: Array<String>
 
-  private val mDrawerToggle: ActionBarDrawerToggle by lazy { setupDrawer() }
+    private val mDrawerToggle: ActionBarDrawerToggle by lazy { setupDrawer() }
 
-  override fun onCreate(savedInstanceState: Bundle?) {
-    super.onCreate(savedInstanceState)
-    setContentView(R.layout.activity_main)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
 
-    // authentication with an API key or named user is required to access basemaps and other 
-    // location services
-    ArcGISRuntimeEnvironment.setApiKey(BuildConfig.API_KEY)
+        // authentication with an API key or named user is required to access basemaps and other
+        // location services
+        ArcGISRuntimeEnvironment.setApiKey(BuildConfig.API_KEY)
 
-    // inflate navigation drawer with all basemap types in a human readable format
-    mNavigationDrawerItemTitles =
-      BasemapStyle.values().map { it.name.replace("_", " ").toLowerCase().capitalize() }
-        .toTypedArray()
+        // inflate navigation drawer with all basemap types in a human readable format
+        mNavigationDrawerItemTitles =
+            BasemapStyle.values().map { it.name.replace("_", " ") }
+                .toTypedArray()
 
-    addDrawerItems()
-    drawerLayout.addDrawerListener(mDrawerToggle)
+        addDrawerItems()
+        drawerLayout.addDrawerListener(mDrawerToggle)
 
-    supportActionBar?.apply {
-      setDisplayHomeAsUpEnabled(true)
-      setHomeButtonEnabled(true)
-      // set opening basemap title to Topographic
-      title = mNavigationDrawerItemTitles[2]
+        supportActionBar?.apply {
+            setDisplayHomeAsUpEnabled(true)
+            setHomeButtonEnabled(true)
+            // set opening basemap title to ARCGIS IMAGERY
+            title = mNavigationDrawerItemTitles[0]
+        }
+
+        // create a map with the imagery Basemap and set it to the map
+        mapView.map = ArcGISMap(BasemapStyle.ARCGIS_IMAGERY)
+        mapView.setViewpoint(Viewpoint(47.6047, -122.3334, 10000000.0))
     }
 
-    // create a map with Topographic Basemap and set it to the map
-    mapView.map = ArcGISMap(BasemapStyle.ARCGIS_TOPOGRAPHIC)
-    mapView.setViewpoint(Viewpoint(47.6047, -122.3334, 10000000.0))
-  }
-
-  /**
-   * Add navigation drawer items
-   */
-  private fun addDrawerItems() {
-    ArrayAdapter(this, android.R.layout.simple_list_item_1, mNavigationDrawerItemTitles).apply {
-      drawerList.adapter = this
-      drawerList.onItemClickListener =
-        AdapterView.OnItemClickListener { _, _, position, _ -> selectBasemap(position) }
-    }
-  }
-
-  /**
-   * Set up the navigation drawer
-   */
-  private fun setupDrawer() =
-    object :
-      ActionBarDrawerToggle(this, drawerLayout, R.string.drawer_open, R.string.drawer_close) {
-
-      override fun isDrawerIndicatorEnabled() = true
-
-      /** Called when a drawer has settled in a completely open state.  */
-      override fun onDrawerOpened(drawerView: View) {
-        super.onDrawerOpened(drawerView)
-        supportActionBar?.title = title
-        invalidateOptionsMenu() // creates call to onPrepareOptionsMenu()
-      }
-
-      /** Called when a drawer has settled in a completely closed state.  */
-      override fun onDrawerClosed(view: View) {
-        super.onDrawerClosed(view)
-        invalidateOptionsMenu() // creates call to onPrepareOptionsMenu()
-      }
+    /**
+     * Add navigation drawer items
+     */
+    private fun addDrawerItems() {
+        ArrayAdapter(this, android.R.layout.simple_list_item_1, mNavigationDrawerItemTitles).apply {
+            drawerList.adapter = this
+            drawerList.onItemClickListener =
+                AdapterView.OnItemClickListener { _, _, position, _ -> selectBasemap(position) }
+        }
     }
 
-  /**
-   * Select the Basemap item based on position in the navigation drawer
-   *
-   * @param position order int in navigation drawer
-   */
-  private fun selectBasemap(position: Int) {
-    // update selected item and title, then close the drawer
-    drawerList.setItemChecked(position, true)
-    drawerLayout.closeDrawer(drawerList)
+    /**
+     * Set up the navigation drawer
+     */
+    private fun setupDrawer() =
+        object :
+            ActionBarDrawerToggle(this, drawerLayout, R.string.drawer_open, R.string.drawer_close) {
 
-    // get basemap title by position
-    val baseMapTitle = mNavigationDrawerItemTitles[position]
-    supportActionBar?.title = baseMapTitle
+            override fun isDrawerIndicatorEnabled() = true
 
-    // select basemap by title
-    mapView.map.basemap =
-      when (BasemapStyle.valueOf(baseMapTitle.replace(" ", "_").toUpperCase())) {
-        BasemapStyle.ARCGIS_CHARTED_TERRITORY -> Basemap(BasemapStyle.ARCGIS_CHARTED_TERRITORY)
-        BasemapStyle.ARCGIS_COLORED_PENCIL -> Basemap(BasemapStyle.ARCGIS_COLORED_PENCIL)
-        BasemapStyle.ARCGIS_COMMUNITY -> Basemap(BasemapStyle.ARCGIS_COMMUNITY)
-        BasemapStyle.ARCGIS_DARK_GRAY -> Basemap(BasemapStyle.ARCGIS_DARK_GRAY)
-        BasemapStyle.ARCGIS_DARK_GRAY_BASE -> Basemap(BasemapStyle.ARCGIS_DARK_GRAY_BASE)
-        BasemapStyle.ARCGIS_DARK_GRAY_LABELS -> Basemap(BasemapStyle.ARCGIS_DARK_GRAY_LABELS)
-        BasemapStyle.ARCGIS_HILLSHADE_DARK -> Basemap(BasemapStyle.ARCGIS_HILLSHADE_DARK)
-        BasemapStyle.ARCGIS_HILLSHADE_LIGHT -> Basemap(BasemapStyle.ARCGIS_HILLSHADE_LIGHT)
-        BasemapStyle.ARCGIS_IMAGERY -> Basemap(BasemapStyle.ARCGIS_IMAGERY)
-        BasemapStyle.ARCGIS_IMAGERY_LABELS -> Basemap(BasemapStyle.ARCGIS_IMAGERY_LABELS)
-        BasemapStyle.ARCGIS_IMAGERY_STANDARD -> Basemap(BasemapStyle.ARCGIS_IMAGERY_STANDARD)
-        BasemapStyle.ARCGIS_LIGHT_GRAY -> Basemap(BasemapStyle.ARCGIS_LIGHT_GRAY)
-        BasemapStyle.ARCGIS_LIGHT_GRAY_BASE -> Basemap(BasemapStyle.ARCGIS_LIGHT_GRAY_BASE)
-        BasemapStyle.ARCGIS_LIGHT_GRAY_LABELS -> Basemap(BasemapStyle.ARCGIS_LIGHT_GRAY_LABELS)
-        BasemapStyle.ARCGIS_MIDCENTURY -> Basemap(BasemapStyle.ARCGIS_MIDCENTURY)
-        BasemapStyle.ARCGIS_MODERN_ANTIQUE -> Basemap(BasemapStyle.ARCGIS_MODERN_ANTIQUE)
-        BasemapStyle.ARCGIS_NAVIGATION -> Basemap(BasemapStyle.ARCGIS_NAVIGATION)
-        BasemapStyle.ARCGIS_NAVIGATION_NIGHT -> Basemap(BasemapStyle.ARCGIS_NAVIGATION_NIGHT)
-        BasemapStyle.ARCGIS_NEWSPAPER -> Basemap(BasemapStyle.ARCGIS_NEWSPAPER)
-        BasemapStyle.ARCGIS_NOVA -> Basemap(BasemapStyle.ARCGIS_NOVA)
-        BasemapStyle.ARCGIS_OCEANS -> Basemap(BasemapStyle.ARCGIS_OCEANS)
-        BasemapStyle.ARCGIS_OCEANS_BASE -> Basemap(BasemapStyle.ARCGIS_OCEANS_BASE)
-        BasemapStyle.ARCGIS_OCEANS_LABELS -> Basemap(BasemapStyle.ARCGIS_OCEANS_LABELS)
-        BasemapStyle.ARCGIS_STREETS -> Basemap(BasemapStyle.ARCGIS_STREETS)
-        BasemapStyle.ARCGIS_STREETS_NIGHT -> Basemap(BasemapStyle.ARCGIS_STREETS_NIGHT)
-        BasemapStyle.ARCGIS_STREETS_RELIEF -> Basemap(BasemapStyle.ARCGIS_STREETS_RELIEF)
-        BasemapStyle.ARCGIS_TERRAIN -> Basemap(BasemapStyle.ARCGIS_TERRAIN)
-        BasemapStyle.ARCGIS_TERRAIN_BASE -> Basemap(BasemapStyle.ARCGIS_TERRAIN_BASE)
-        BasemapStyle.ARCGIS_TERRAIN_DETAIL -> Basemap(BasemapStyle.ARCGIS_TERRAIN_DETAIL)
-        BasemapStyle.ARCGIS_TOPOGRAPHIC -> Basemap(BasemapStyle.ARCGIS_TOPOGRAPHIC)
-        BasemapStyle.OSM_DARK_GRAY -> Basemap(BasemapStyle.OSM_DARK_GRAY)
-        BasemapStyle.OSM_DARK_GRAY_BASE -> Basemap(BasemapStyle.OSM_DARK_GRAY_BASE)
-        BasemapStyle.OSM_DARK_GRAY_LABELS -> Basemap(BasemapStyle.OSM_DARK_GRAY_LABELS)
-        BasemapStyle.OSM_LIGHT_GRAY -> Basemap(BasemapStyle.OSM_LIGHT_GRAY)
-        BasemapStyle.OSM_LIGHT_GRAY_BASE -> Basemap(BasemapStyle.OSM_LIGHT_GRAY_BASE)
-        BasemapStyle.OSM_LIGHT_GRAY_LABELS -> Basemap(BasemapStyle.OSM_LIGHT_GRAY_LABELS)
-        BasemapStyle.OSM_STANDARD -> Basemap(BasemapStyle.OSM_STANDARD)
-        BasemapStyle.OSM_STANDARD_RELIEF -> Basemap(BasemapStyle.OSM_STANDARD_RELIEF)
-        BasemapStyle.OSM_STANDARD_RELIEF_BASE -> Basemap(BasemapStyle.OSM_STANDARD_RELIEF_BASE)
-        BasemapStyle.OSM_STREETS -> Basemap(BasemapStyle.OSM_STREETS)
-        BasemapStyle.OSM_STREETS_RELIEF -> Basemap(BasemapStyle.OSM_STREETS_RELIEF)
-      }
-  }
+            /** Called when a drawer has settled in a completely open state.  */
+            override fun onDrawerOpened(drawerView: View) {
+                super.onDrawerOpened(drawerView)
+                supportActionBar?.title = title
+                invalidateOptionsMenu() // creates call to onPrepareOptionsMenu()
+            }
 
-  override fun onOptionsItemSelected(item: MenuItem): Boolean {
-    // Activate the navigation drawer toggle
-    return mDrawerToggle.onOptionsItemSelected(item) || super.onOptionsItemSelected(item)
-  }
+            /** Called when a drawer has settled in a completely closed state.  */
+            override fun onDrawerClosed(view: View) {
+                super.onDrawerClosed(view)
+                invalidateOptionsMenu() // creates call to onPrepareOptionsMenu()
+            }
+        }
 
-  override fun onPostCreate(savedInstanceState: Bundle?) {
-    super.onPostCreate(savedInstanceState)
-    mDrawerToggle.syncState()
-  }
+    /**
+     * Select the Basemap item based on position in the navigation drawer
+     *
+     * @param position order int in navigation drawer
+     */
+    private fun selectBasemap(position: Int) {
+        // update selected item and title, then close the drawer
+        drawerList.setItemChecked(position, true)
+        drawerLayout.closeDrawer(drawerList)
 
-  override fun onConfigurationChanged(newConfig: Configuration) {
-    super.onConfigurationChanged(newConfig)
-    mDrawerToggle.onConfigurationChanged(newConfig)
-  }
+        // get basemap title by position
+        val baseMapTitle = mNavigationDrawerItemTitles[position]
+        supportActionBar?.title = baseMapTitle
 
-  override fun onResume() {
-    super.onResume()
-    mapView.resume()
-  }
+        // create a new Basemap(BasemapStyle.THE_ENUM_SELECTED)
+        mapView.map.basemap =
+            Basemap(BasemapStyle.valueOf(baseMapTitle.replace(" ", "_")))
+    }
 
-  override fun onPause() {
-    super.onPause()
-    mapView.pause()
-  }
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // Activate the navigation drawer toggle
+        return mDrawerToggle.onOptionsItemSelected(item) || super.onOptionsItemSelected(item)
+    }
 
-  override fun onDestroy() {
-    super.onDestroy()
-    mapView.dispose()
-  }
+    override fun onPostCreate(savedInstanceState: Bundle?) {
+        super.onPostCreate(savedInstanceState)
+        mDrawerToggle.syncState()
+    }
+
+    override fun onConfigurationChanged(newConfig: Configuration) {
+        super.onConfigurationChanged(newConfig)
+        mDrawerToggle.onConfigurationChanged(newConfig)
+    }
+
+    override fun onResume() {
+        super.onResume()
+        mapView.resume()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        mapView.pause()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        mapView.dispose()
+    }
 }
