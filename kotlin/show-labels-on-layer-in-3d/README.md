cb2c9604fff2168d874c53b946449f91e73b198d
trev8939 <tdraeseke@esri.com>
2021-02-23 14:55:41 +0000

    still need to fix viewbinding and label expression

---
+++ kotlin/show-labels-on-layer-in-3d/README.md
@@ -0,0 +1,43 @@
+# Show labels on layer
+
+Display custom labels on a feature layer.
+
+![Image of show labels on layer](show-labels-on-layer.png)
+
+## Use case
+
+Labeling features is useful to visually display a key piece of information or attribute of a feature on a map. For example, you may want to label rivers or streets with their names. 
+
+## How to use the sample
+
+Pan and zoom around the United States. Labels for US highways will be shown. 
+
+## How it works
+
+1. Create a `ServiceFeatureTable` using a feature service URL.
+2. Create a `FeatureLayer` from the service feature table.
+3. Create a `TextSymbol` to use for displaying the label text.
+4. Create a JSON string for the label definition.
+    * Set the "LabelExpressionInfo.expression" key to express what the text the label should display. You can use fields of the feature by using `$feature.field_name` in the expression.
+    * To use the text symbol, set the "symbol" key to the symbol's JSON representation using `textSymbol.toJson()`.
+5. Create a label definition from the JSON using `LabelDefinition.fromJson(jsonString)`.
+6. Add the definition to the feature layer with `featureLayer.labelDefinitions.add(labelDefinition)` .
+7. Lastly, enable labels on the layer using `featureLayer.isLabelsEnabled`.
+
+## Relevant API
+
+* FeatureLayer
+* LabelDefinition
+* TextSymbol
+
+## About the data
+
+This sample uses the [US Highways](http://sampleserver6.arcgisonline.com/arcgis/rest/services/USA/MapServer/1) feature service hosted on ArcGIS Online.
+
+## Additional information
+
+Help regarding the JSON syntax for defining the `LabelDefinition.FromJson` syntax can be found in [labeling info](https://developers.arcgis.com/web-map-specification/objects/labelingInfo/) in the *Web map specification*.
+
+## Tags
+
+attribute, deconfliction, label, labeling, string, symbol, text, visualization
---
+++ kotlin/show-labels-on-layer-in-3d/src/main/java/com/esri/arcgisruntime/showlabelsonlayerin3d/MainActivity.kt
@@ -0,0 +1,121 @@
+/*
+ * Copyright 2020 Esri
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package com.esri.arcgisruntime.showlabelsonlayerin3d
+
+import android.graphics.Color
+import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.esri.arcgisruntime.ArcGISRuntimeEnvironment
+import com.esri.arcgisruntime.arcgisservices.LabelDefinition
+import com.esri.arcgisruntime.layers.FeatureLayer
+import com.esri.arcgisruntime.mapping.ArcGISScene
+import com.esri.arcgisruntime.mapping.BasemapStyle
+import com.esri.arcgisruntime.mapping.view.SceneView
+import com.esri.arcgisruntime.portal.Portal
+import com.esri.arcgisruntime.portal.PortalItem
+import com.esri.arcgisruntime.showlabelsonlayerin3d.databinding.ActivityMainBinding
+import com.esri.arcgisruntime.symbology.TextSymbol
+import com.google.gson.JsonObject
+import com.google.gson.JsonParser
+import com.google.gson.JsonPrimitive
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    private val TAG = MainActivity::class.java.simpleName
+
+  /*  private val activityMainBinding: ActivityMainBinding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
+
+    private val sceneView: SceneView by lazy {
+        activityMainBinding.sceneView
+    }*/
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        ArcGISRuntimeEnvironment.setApiKey(BuildConfig.API_KEY)
+
+        val portal = Portal("https://www.arcgis.com")
+        val portalItem = PortalItem(portal, "850dfee7d30f4d9da0ebca34a533c169")
+
+        val scene = ArcGISScene(portalItem)
+        scene.addDoneLoadingListener {
+            Toast.makeText(applicationContext, scene.loadStatus.name, Toast.LENGTH_LONG).show()
+            (scene.operationalLayers.filter { it.name == "Gas" } as? FeatureLayer)?.let { gasFeatureLayer ->
+                gasFeatureLayer.apply {
+                    // add the label definition to the feature layer
+                    labelDefinitions.add(makeLabelDefinition())
+                    // enable layers on the feature layer
+                    isLabelsEnabled = true
+                }
+            }
+        }
+
+        sceneView.scene = scene
+    }
+
+    private fun makeLabelDefinition(): LabelDefinition {
+        // make and stylize the text symbol
+        val textSymbol = TextSymbol().apply {
+            angle = 0f
+            backgroundColor = Color.TRANSPARENT
+            outlineColor = Color.WHITE
+            color = Color.RED
+            haloColor = Color.WHITE
+            haloWidth = 2f
+            horizontalAlignment = TextSymbol.HorizontalAlignment.CENTER
+            verticalAlignment = TextSymbol.VerticalAlignment.MIDDLE
+            offsetX = 0f
+            offsetY = 0f
+            fontDecoration = TextSymbol.FontDecoration.NONE
+            size = 14f
+            fontStyle = TextSymbol.FontStyle.NORMAL
+            fontWeight = TextSymbol.FontWeight.NORMAL
+        }
+
+        // make a JSON object
+        val labelJSONObject = JsonObject().apply {
+            add("expression", JsonPrimitive("\$feature.INSTALLATIONDATE"))
+            add("labelPlacement", JsonPrimitive("esriServerLinePlacementAboveAlong"))
+            add("useCodedValues", JsonPrimitive(true))
+            add("symbol", JsonParser.parseString(textSymbol.toJson()))
+        }
+
+        // create and return a label definition from the JSON object
+        return LabelDefinition.fromJson(labelJSONObject.toString())
+    }
+
+    override fun onPause() {
+        sceneView.pause()
+        super.onPause()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        sceneView.resume()
+    }
+
+    override fun onDestroy() {
+        sceneView.dispose()
+        super.onDestroy()
+    }
+}
