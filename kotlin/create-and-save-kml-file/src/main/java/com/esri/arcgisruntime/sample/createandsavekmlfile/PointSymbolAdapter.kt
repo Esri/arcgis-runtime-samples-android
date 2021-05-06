bc009b3fcdc76ec8123ddcab73d0aee27a1a04c4
trev8939 <tdraeseke@esri.com>
2021-05-06 10:53:58 +0100

    unsuccessful create and save kml conversion

--- kotlin/create-and-save-kml-file/src/main/java/com/esri/arcgisruntime/sample/createandsavekmlfile/PointSymbolAdapter.kt
+++ kotlin/create-and-save-kml-file/src/main/java/com/esri/arcgisruntime/sample/createandsavekmlfile/PointSymbolAdapter.kt
@@ -5,27 +5,28 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
-import kotlinx.android.synthetic.main.point_symbol.view.*
+import com.esri.arcgisruntime.sample.createandsavekmlfile.databinding.PointSymbolBinding
 
 
-class PointSymbolAdapter(context: Context, pointSymbolUrls: List<Int>) : ArrayAdapter<Int>(context, R.layout.point_symbol, pointSymbolUrls) {
+class PointSymbolAdapter(context: Context, pointSymbolUrls: List<Int>) :
+    ArrayAdapter<Int>(context, R.layout.point_symbol, pointSymbolUrls) {
 
-  override fun getView(position: Int, recycledView: View?, parent: ViewGroup): View {
-    return this.createView(position, recycledView, parent)
-  }
+    private val pointSymbolBinding by lazy {
+        PointSymbolBinding.inflate(LayoutInflater.from(context))
+    }
 
-  override fun getDropDownView(position: Int, recycledView: View?, parent: ViewGroup): View {
-    return this.createView(position, recycledView, parent)
-  }
+    override fun getView(position: Int, recycledView: View?, parent: ViewGroup): View {
+        return this.createView(position, recycledView, parent)
+    }
 
-  private fun createView(position: Int, recycledView: View?, parent: ViewGroup): View {
-    val pointDrawable = getItem(position)!!
-    val view = recycledView ?: LayoutInflater.from(context).inflate(
-      R.layout.point_symbol,
-      parent,
-      false
-    )
-    view.pointSymbol.setImageResource(pointDrawable)
-    return view
-  }
+    override fun getDropDownView(position: Int, recycledView: View?, parent: ViewGroup): View {
+        return this.createView(position, recycledView, parent)
+    }
+
+    private fun createView(position: Int, recycledView: View?, parent: ViewGroup): View {
+        val pointDrawable = getItem(position)!!
+        val view = recycledView ?: pointSymbolBinding.root
+        pointSymbolBinding.pointSymbol.setImageResource(pointDrawable)
+        return view
+    }
 }
