053743764fb43e6afa37e60d03b2ed4cef7e2188
Shubham Sharma <shubham71098@gmail.com>
2021-08-03 22:21:20 -0700

    Update MainActivity.kt

--- kotlin/generate-geodatabase/src/main/java/com/esri/arcgisruntime/sample/generategeodatabase/MainActivity.kt
+++ kotlin/generate-geodatabase/src/main/java/com/esri/arcgisruntime/sample/generategeodatabase/MainActivity.kt
@@ -198,10 +198,8 @@ class MainActivity : AppCompatActivity() {
                 generateGeodatabaseJob.cancel()
             }
             setCancelable(false)
-            setView(
-                LayoutInflater.from(this@MainActivity)
-                    .inflate(R.layout.dialog_layout, null)
-            )
+            val dialogLayoutBinding = DialogLayoutBinding.inflate(layoutInflater)
+            setView(dialogLayoutBinding.root)
         }
         return builder.create()
     }
