d982c7ecfc4fb568db1f3f1b59056dcebad39746
Shubham Sharma <shubhamsharma@esri.com>
2022-02-03 16:49:39 -0800

    Update MainActivity.kt

--- kotlin/dictionary-renderer-with-graphics-overlay/src/main/java/com/esri/arcgisruntime/sample/dictionaryrendererwithgraphicsoverlay/MainActivity.kt
+++ kotlin/dictionary-renderer-with-graphics-overlay/src/main/java/com/esri/arcgisruntime/sample/dictionaryrendererwithgraphicsoverlay/MainActivity.kt
@@ -17,16 +17,45 @@
 package com.esri.arcgisruntime.sample.dictionaryrendererwithgraphicsoverlay
 
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.esri.arcgisruntime.ArcGISRuntimeEnvironment
+import com.esri.arcgisruntime.loadable.LoadStatus
 import com.esri.arcgisruntime.mapping.ArcGISMap
 import com.esri.arcgisruntime.mapping.BasemapStyle
 import com.esri.arcgisruntime.mapping.Viewpoint
+import com.esri.arcgisruntime.mapping.view.Graphic
+import com.esri.arcgisruntime.mapping.view.GraphicsOverlay
 import com.esri.arcgisruntime.mapping.view.MapView
+import com.esri.arcgisruntime.portal.Portal
+import com.esri.arcgisruntime.portal.PortalItem
 import com.esri.arcgisruntime.sample.dictionaryrendererwithgraphicsoverlay.databinding.ActivityMainBinding
+import com.esri.arcgisruntime.symbology.DictionaryRenderer
+import com.esri.arcgisruntime.symbology.DictionarySymbolStyle
+import org.w3c.dom.Document
+import org.w3c.dom.Node
+import org.w3c.dom.NodeList
+import java.io.File
+import javax.xml.parsers.DocumentBuilder
+import javax.xml.parsers.DocumentBuilderFactory
+import com.esri.arcgisruntime.geometry.Multipoint
+
+import com.esri.arcgisruntime.geometry.PointCollection
+
+import com.esri.arcgisruntime.geometry.SpatialReference
+
+
+
 
 
 class MainActivity : AppCompatActivity() {
+
+    private val TAG = MainActivity::class.java.simpleName
+
+    private val graphicsOverlay: GraphicsOverlay = GraphicsOverlay()
+
+    private val itemID = "d815f3bdf6e6452bb8fd153b654c94ca"
 
     private val activityMainBinding by lazy {
         ActivityMainBinding.inflate(layoutInflater)
@@ -45,13 +74,114 @@ class MainActivity : AppCompatActivity() {
         ArcGISRuntimeEnvironment.setApiKey(BuildConfig.API_KEY)
 
         // create a map with the BasemapType topographic
-        val map = ArcGISMap(BasemapStyle.ARCGIS_NAVIGATION_NIGHT)
+        val map = ArcGISMap(BasemapStyle.ARCGIS_TOPOGRAPHIC)
+
+        // graphics no longer show after zooming passed this scale
+        graphicsOverlay.minScale = 1000000.0
+        mapView.graphicsOverlays.add(graphicsOverlay)
+
+        // create the dictionary symbol style from the Joint Military Symbology MIL-STD-2525D portal item
+        val portalItem = PortalItem(Portal("https://www.arcgis.com/", false), itemID)
+        val dictionarySymbolStyle = DictionarySymbolStyle(portalItem)
+
+        // add done loading listeners to the map and dictionary symbol style and check they have loaded
+        map.addDoneLoadingListener {
+            if (map.loadStatus == LoadStatus.LOADED) {
+                dictionarySymbolStyle.addDoneLoadingListener {
+                    if (dictionarySymbolStyle.loadStatus == LoadStatus.LOADED) {
+                        // find the first configuration setting which has the property name "model",
+                        // and set its value to "ORDERED ANCHOR POINT"
+                        dictionarySymbolStyle.configurations
+                            .first { it.name.equals("model") }.value = "ORDERED ANCHOR POINT"
+
+                        // create a new dictionary renderer from the dictionary symbol style to render graphics
+                        // with symbol dictionary attributes and set it to the graphics overlay renderer
+                        val dictionaryRenderer = DictionaryRenderer(dictionarySymbolStyle)
+                        graphicsOverlay.renderer = dictionaryRenderer
+
+                        // parse graphic attributes from an XML file following the mil2525d specification
+                        try {
+                            val messages: List<Map<String, Any>> = parseMessages()
+                            // create graphics with attributes and add to graphics overlay
+                            val graphics: List<Graphic> = emptyList()
+
+                            //TODO CHECK THIS
+                            graphicsOverlay.graphics.addAll(graphics)
+
+                            // set the viewpoint to the extent of the graphics overlay
+                            mapView.setViewpointGeometryAsync(graphicsOverlay.extent)
+                        } catch (e: Exception) {
+                            displayError("Error parsing messages: ${e.message}")
+                        }
+                    } else {
+                        displayError("Failed to load symbol style: ${dictionarySymbolStyle.loadError.cause?.message}")
+                    }
+                }
+            } else {
+                displayError("Map failed to load: ${map.loadError.cause?.message} ")
+            }
+            // load the dictionary symbol style once the map has loaded
+            dictionarySymbolStyle.loadAsync();
+        }
 
         // set the map to be displayed in the layout's MapView
         mapView.map = map
-
-        mapView.setViewpoint(Viewpoint(34.056295, -117.195800, 10000.0))
     }
+
+    /**
+     * Function to log the error message and display it as a toast.
+     */
+    private fun displayError(message: String) {
+        Log.e(TAG, message)
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
+    }
+
+    /**
+     * Parses a XML file following the mil2525d specification and creates a message for each block of attributes found.
+     */
+    private fun parseMessages(): List<Map<String, Any>> {
+        val mil2525dFile =
+            File(getExternalFilesDir(null).toString() + "/Mil2525DMessages.xml")
+
+        val documentBuilderFactory: DocumentBuilderFactory? = DocumentBuilderFactory.newInstance()
+        val documentBuilder: DocumentBuilder? = documentBuilderFactory?.newDocumentBuilder()
+        val document: Document? = documentBuilder?.parse(mil2525dFile)
+        document?.documentElement?.normalize()
+
+        val messages: MutableList<Map<String, Any>> = ArrayList()
+
+        for (i in 0 until document!!.getElementsByTagName("message").length) {
+            val message: Node = document.getElementsByTagName("message").item(i)
+            val attributes: MutableMap<String, Any> = HashMap()
+            val childNodes: NodeList = message.childNodes
+            for (j in 0 until childNodes.length) {
+                attributes[childNodes.item(j).nodeName] = childNodes.item(j).textContent
+            }
+            messages.add(attributes)
+        }
+        return messages
+    }
+
+    /**
+     * Creates a graphic using a symbol dictionary and the attributes that were passed.
+     *
+     * @param attributes tells symbol dictionary what symbol to apply to graphic
+     */
+    private fun createGraphic(attributes: Map<String, Any>): Graphic {
+
+        // get spatial reference
+        val wkid: Int = (attributes["_wkid"] as String).toInt ()
+        val sr = SpatialReference.create(wkid)
+        // get points from the coordinate string in the "_control_points" attribute (delimited with ';')
+        val points = PointCollection(sr)
+        val coordinates = (attributes["_control_points"] as String).split(";").toTypedArray()
+
+        //TODO add stream here
+
+        // return a graphic with multipoint geometry
+        return Graphic(Multipoint(points), attributes)
+    }
+
 
     override fun onPause() {
         mapView.pause()
