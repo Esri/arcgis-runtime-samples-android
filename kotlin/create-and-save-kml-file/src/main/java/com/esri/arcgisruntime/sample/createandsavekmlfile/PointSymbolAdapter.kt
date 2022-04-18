243273bf8e9ea692b449de292b91c24023639823
Shubham Sharma <shubhamsharma@esri.com>
2022-04-18 13:45:50 -0700

    point symbols now loading

--- kotlin/create-and-save-kml-file/src/main/java/com/esri/arcgisruntime/sample/createandsavekmlfile/PointSymbolAdapter.kt
+++ kotlin/create-and-save-kml-file/src/main/java/com/esri/arcgisruntime/sample/createandsavekmlfile/PointSymbolAdapter.kt
@@ -11,22 +11,20 @@ import com.esri.arcgisruntime.sample.createandsavekmlfile.databinding.PointSymbo
 class PointSymbolAdapter(context: Context, pointSymbolUrls: List<Int>) :
     ArrayAdapter<Int>(context, R.layout.point_symbol, pointSymbolUrls) {
 
-    private val pointSymbolBinding by lazy {
-        PointSymbolBinding.inflate(LayoutInflater.from(context))
-    }
-
     override fun getView(position: Int, recycledView: View?, parent: ViewGroup): View {
-        return this.createView(position, recycledView, parent)
+        val binding = PointSymbolBinding.inflate(
+            LayoutInflater.from(parent.context),
+            parent,
+            false
+        )
+        getItem(position)?.let { binding.pointSymbol.setImageResource(it) }
+        return binding.root
     }
 
-    override fun getDropDownView(position: Int, recycledView: View?, parent: ViewGroup): View {
-        return this.createView(position, recycledView, parent)
-    }
+    override fun getDropDownView(
+        position: Int,
+        convertView: View?,
+        parent: ViewGroup
+    ): View = getView(position, convertView, parent)
 
-    private fun createView(position: Int, recycledView: View?, parent: ViewGroup): View {
-        val pointDrawable = getItem(position)!!
-        val view = recycledView ?: pointSymbolBinding.root
-        pointSymbolBinding.pointSymbol.setImageResource(pointDrawable)
-        return view
-    }
 }
