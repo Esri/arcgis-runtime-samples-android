a270f4b9a3af651e614a4864ae5be36ab387850d
Shubham Sharma <shubhamsharma@esri.com>
2022-06-29 15:33:14 -0700

    alternate symbols rendering

--- kotlin/apply-unique-values-with-alternate-symbols/src/main/java/com/esri/arcgisruntime/sample/applyuniquevalueswithalternatesymbols/MainActivity.kt
+++ kotlin/apply-unique-values-with-alternate-symbols/src/main/java/com/esri/arcgisruntime/sample/applyuniquevalueswithalternatesymbols/MainActivity.kt
@@ -16,14 +16,23 @@
 
 package com.esri.arcgisruntime.sample.applyuniquevalueswithalternatesymbols
 
+import android.graphics.Color
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.esri.arcgisruntime.ArcGISRuntimeEnvironment
+import com.esri.arcgisruntime.data.ServiceFeatureTable
+import com.esri.arcgisruntime.geometry.Point
+import com.esri.arcgisruntime.geometry.SpatialReferences
+import com.esri.arcgisruntime.layers.FeatureLayer
 import com.esri.arcgisruntime.mapping.ArcGISMap
 import com.esri.arcgisruntime.mapping.BasemapStyle
 import com.esri.arcgisruntime.mapping.Viewpoint
 import com.esri.arcgisruntime.mapping.view.MapView
 import com.esri.arcgisruntime.sample.applyuniquevalueswithalternatesymbols.databinding.ActivityMainBinding
+import com.esri.arcgisruntime.symbology.SimpleMarkerSymbol
+import com.esri.arcgisruntime.symbology.Symbol
+import com.esri.arcgisruntime.symbology.SymbolReferenceProperties
+import com.esri.arcgisruntime.symbology.UniqueValueRenderer
 
 
 class MainActivity : AppCompatActivity() {
@@ -46,13 +55,81 @@ class MainActivity : AppCompatActivity() {
         // location services
         ArcGISRuntimeEnvironment.setApiKey(BuildConfig.API_KEY)
 
+        // create a feature layer using the feature table
+        val featureTable =
+            ServiceFeatureTable("https://sampleserver6.arcgisonline.com/arcgis/rest/services/SF311/FeatureServer/0")
+        val featureLayer = FeatureLayer(featureTable)
+
+        // create a symbol for a specific scale range
+        val triangleMultilayerSymbol = SimpleMarkerSymbol(
+            SimpleMarkerSymbol.Style.TRIANGLE,
+            Color.RED,
+            30F
+        ).toMultilayerSymbol().apply {
+            referenceProperties = SymbolReferenceProperties(5000.0, 0.0)
+        }
+
+        // create alternate symbols for use at different scale ranges
+        val blueAlternateSymbol = SimpleMarkerSymbol(
+            SimpleMarkerSymbol.Style.SQUARE,
+            Color.BLUE,
+            30F
+        ).toMultilayerSymbol().apply {
+            referenceProperties = SymbolReferenceProperties(10000.0, 5000.0)
+        }
+
+        val yellowAlternateSymbol = SimpleMarkerSymbol(
+            SimpleMarkerSymbol.Style.DIAMOND,
+            Color.YELLOW,
+            30F
+        ).toMultilayerSymbol().apply {
+            referenceProperties = SymbolReferenceProperties(20000.0, 10000.0)
+        }
+
+        val alternateSymbols = listOf<Symbol>(blueAlternateSymbol, yellowAlternateSymbol)
+
+        // create a unique value with the triangle symbol and the alternate symbols
+        val uniqueValue = UniqueValueRenderer.UniqueValue(
+            "unique value",
+            "unique values based on request type",
+            triangleMultilayerSymbol,
+            listOf("Damaged Property"),
+            alternateSymbols
+        )
+
+        // create a unique value renderer
+        val uniqueValueRenderer = UniqueValueRenderer().apply {
+            uniqueValues.add(uniqueValue)
+            fieldNames.add("req_type")
+        }
+
+        // set a default symbol for the unique value renderer.
+        // This will be use for features that aren't "Damaged Property"
+        // or when out of range of the UniqueValue symbols.
+        uniqueValueRenderer.defaultSymbol = SimpleMarkerSymbol(
+            SimpleMarkerSymbol.Style.DIAMOND,
+            Color.MAGENTA,
+            15F
+        ).toMultilayerSymbol()
+
+        // set the unique value renderer on the feature layer
+        featureLayer.renderer = uniqueValueRenderer
+
         // create a map with the BasemapType topographic
-        val map = ArcGISMap(BasemapStyle.ARCGIS_NAVIGATION_NIGHT)
+        val map = ArcGISMap(BasemapStyle.ARCGIS_TOPOGRAPHIC)
 
         // set the map to be displayed in the layout's MapView
         mapView.map = map
 
-        mapView.setViewpoint(Viewpoint(34.056295, -117.195800, 10000.0))
+        val centerPoint = Point(
+            -13631205.660131,
+            4546829.846004,
+            SpatialReferences.getWebMercator()
+        )
+        mapView.setViewpoint(
+            Viewpoint(centerPoint, 25000.0)
+        )
+        mapView.map.operationalLayers.add(featureLayer)
     }
 
     override fun onPause() {
