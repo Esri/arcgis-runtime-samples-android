6cc7827903edecb6a1326642fd21da2e139eaea1
Shubham Sharma <shubhamsharma@esri.com>
2022-07-06 13:38:45 -0700

    Update MainActivity.kt

--- kotlin/set-up-location-driven-geotriggers/src/main/java/com/esri/arcgisruntime/sample/setuplocationdrivengeotriggers/MainActivity.kt
+++ kotlin/set-up-location-driven-geotriggers/src/main/java/com/esri/arcgisruntime/sample/setuplocationdrivengeotriggers/MainActivity.kt
@@ -214,12 +214,11 @@ class MainActivity : AppCompatActivity() {
         // check to see if the GeotriggerMonitor failed to start
         future.addDoneListener {
             if (geotriggerMonitor.status == GeotriggerMonitorStatus.FAILED_TO_START) {
-                Log.e(
-                    TAG,
-                    "GeotriggerMonitor failed to load: " +
-                        geotriggerMonitor.warning.message +
-                        geotriggerMonitor.warning.additionalMessage
-                )
+                val message = "GeotriggerMonitor failed to load: " +
+                    geotriggerMonitor.warning.message +
+                    geotriggerMonitor.warning.additionalMessage
+                Log.e(TAG, message)
+                Toast.makeText(this@MainActivity, message, Toast.LENGTH_SHORT).show()
             }
         }
 
