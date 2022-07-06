6a085378fe7cec564e697aaf5fa3b5919abd6c68
Trevor Draeseke <trev8939@esri.com>
2022-07-06 17:21:17 -0400

    lower parts threshold

--- tools/CI/README_Metadata_StyleCheck/entry.py
+++ tools/CI/README_Metadata_StyleCheck/entry.py
@@ -89,7 +89,7 @@ def main():
 
         path_parts = os.path.normpath(f).split(os.path.sep)
         print(path_parts)
-        if len(path_parts) < 7:
+        if len(path_parts) < 1:
             # A file not in samples folder, omit.
             # E.g. might be in the root folder or other unrelated folders.
             continue
