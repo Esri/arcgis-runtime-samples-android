de4b896b063a575db4ce4a4af26ed189de23c021
TADraeseke <tdraeseke@esri.com>
2022-08-09 15:04:26 +0100

    Update metadata_style_checker.py

--- tools/CI/README_Metadata_StyleCheck/metadata_style_checker.py
+++ tools/CI/README_Metadata_StyleCheck/metadata_style_checker.py
@@ -89,7 +89,7 @@ def parse_provision_from(offline_data_string: str) -> typing.List[str]:
 
     # extract any guids - these are AGOL items
     regex = re.compile('[0-9a-f]{8}[0-9a-f]{4}[1-5][0-9a-f]{3}[89ab][0-9a-f]{3}[0-9a-f]{12}', re.I)
-    matches = re.findall("]((.*)).")
+    matches = re.findall("]((.*)).", offline_data_string)
 
     return list(dict.fromkeys(matches))
 
